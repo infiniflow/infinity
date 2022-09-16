3d41f7e9e9b32725c66322e473d8a35876af5935
jinhai <haijin.chn@gmail.com>
2022-09-16 20:58:07 +0800

    Add function part7: Add abs function
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- src/function/scalar/abs.cpp
+++ src/function/scalar/abs.cpp
@@ -7,12 +7,19 @@
 
 namespace infinity {
 
-struct AbsFunction {
+struct AbsFunctionInt {
     template <typename TA, typename TB>
     static inline TA Execute(TB value) {
         if(value == std::numeric_limits<TB>::min()) {
             ExecutorError("Overflow on abs(" + std::to_string(value) + ").");
         }
+        return value < 0 ? -value : value;
+    }
+};
+
+struct AbsFunctionFloat {
+    template <typename TA, typename TB>
+    static inline TA Execute(TB value) {
         return value < 0 ? -value : value;
     }
 };
@@ -26,8 +33,43 @@ RegisterAbsFunction() {
             "abs",
             { LogicalType(LogicalTypeId::kTinyInt) },
             { LogicalType(LogicalTypeId::kTinyInt) },
-            &ScalarFunction::UnaryFunction<int8_t, int8_t, AbsFunction>);
+            &ScalarFunction::UnaryFunction<int8_t, int8_t, AbsFunctionInt>);
     function_set.AddFunction(abs_int8);
+
+    ScalarFunction abs_int16(
+            "abs",
+            { LogicalType(LogicalTypeId::kSmallInt) },
+            { LogicalType(LogicalTypeId::kSmallInt) },
+            &ScalarFunction::UnaryFunction<int16_t, int16_t, AbsFunctionInt>);
+    function_set.AddFunction(abs_int16);
+
+    ScalarFunction abs_int32(
+            "abs",
+            { LogicalType(LogicalTypeId::kInteger) },
+            { LogicalType(LogicalTypeId::kInteger) },
+            &ScalarFunction::UnaryFunction<int32_t, int32_t, AbsFunctionInt>);
+    function_set.AddFunction(abs_int32);
+
+    ScalarFunction abs_int64(
+            "abs",
+            { LogicalType(LogicalTypeId::kBigInt) },
+            { LogicalType(LogicalTypeId::kBigInt) },
+            &ScalarFunction::UnaryFunction<int64_t, int64_t, AbsFunctionInt>);
+    function_set.AddFunction(abs_int64);
+
+    ScalarFunction abs_float(
+            "abs",
+            { LogicalType(LogicalTypeId::kFloat) },
+            { LogicalType(LogicalTypeId::kFloat) },
+            &ScalarFunction::UnaryFunction<float, float, AbsFunctionFloat>);
+    function_set.AddFunction(abs_float);
+
+    ScalarFunction abs_double(
+            "abs",
+            { LogicalType(LogicalTypeId::kDouble) },
+            { LogicalType(LogicalTypeId::kDouble) },
+            &ScalarFunction::UnaryFunction<float, float, AbsFunctionFloat>);
+    function_set.AddFunction(abs_double);
 
     return function_set;
 }
