d4525dd10b54b4509f6a94260d39a136351a6f01
jinhai <haijin.chn@gmail.com>
2022-11-17 00:04:11 +0800

    Heterogeneous type, implementation part1.
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- src/common/types/heterogenous/mixed_type.cpp
+++ src/common/types/heterogenous/mixed_type.cpp
@@ -3,3 +3,86 @@
 //
 
 #include "mixed_type.h"
+
+namespace infinity {
+MixedType
+MixedType::MakeInteger(i64 input) {
+    MixedType value(MixedValueType::kInteger);
+    IntegerMixedType* integer_mixed_ptr = (IntegerMixedType *)(&value);
+    integer_mixed_ptr->value = input;
+    return value;
+}
+
+MixedType
+MixedType::MakeFloat(f64 input) {
+    MixedType value(MixedValueType::kFloat);
+    FloatMixedType* float_mixed_ptr = (FloatMixedType *)(&value);
+    float_mixed_ptr->value = input;
+    return value;
+}
+
+MixedType
+MixedType::MakeString(const String& str) {
+    size_t str_len = str.size();
+    if(str_len <= MixedType::SHORT_STR_LIMIT) {
+        // Short str
+        MixedType value(MixedValueType::kShortStr);
+        ShortStrMixedType* short_mixed_ptr = (ShortStrMixedType*)(&value);
+        memcpy(short_mixed_ptr->ptr, str.c_str(), str_len);
+        if(str_len < SHORT_STR_LIMIT) {
+            memset(short_mixed_ptr->ptr + str_len, 0, MixedType::SHORT_STR_LIMIT - str_len);
+        }
+        return value;
+    } else {
+        // Long str
+    }
+}
+
+MixedType
+MixedType::MakeObject() {
+    MixedType value(MixedValueType::kObject);
+    return value;
+}
+
+MixedType
+MixedType::MakeArray(ptr_t ptr, i32 count) {
+
+}
+
+MixedType
+MixedType::MakeNull() {
+    MixedType value(MixedValueType::kNull);
+    return value;
+}
+
+MixedType
+MixedType::MakeMissing() {
+    MixedType value(MixedValueType::kMissing);
+    return value;
+}
+
+// Non-static member method
+MixedType::MixedType(const MixedType& other) {
+
+}
+
+MixedType::MixedType(MixedType&& other) noexcept {
+
+}
+
+MixedType&
+MixedType::operator=(const MixedType& other) {
+
+}
+
+MixedType&
+MixedType::operator=(MixedType&& other) noexcept {
+
+}
+
+void
+MixedType::Reset() {
+
+}
+
+}
--- src/common/types/heterogenous/mixed_type.h
+++ src/common/types/heterogenous/mixed_type.h
@@ -30,6 +30,26 @@ enum class MixedValueType : i8 {
     kDummy,
 };
 
+class MixedType;
+class IntegerMixedType;
+class FloatMixedType;
+class LongStrMixedType;
+class ShortStrMixedType;
+class ObjectMixedType;
+class ArrayMixedType;
+class NullMixedType;
+class MissingMixedType;
+
+class NestedIntegerMixedType;
+class NestedFloatMixedType;
+class NestedLongStrMixedType;
+class NestedShortStrMixedType;
+class NestedObjectMixedType;
+class NestedArrayMixedType;
+class NestedNullMixedType;
+class NestedMissingMixedType;
+
+
 struct BaseMixedType {
     BaseMixedType() = default;
 
@@ -41,9 +61,55 @@ struct BaseMixedType {
 
 // Not nested data type
 struct __attribute__((packed)) MixedType : public BaseMixedType {
+
+public:
+    char_t ptr[15];
+
+public:
+    static constexpr i64 SHORT_STR_LIMIT = 15;
+    static constexpr i64 SHORT_NESTED_STR_LIMIT = 13;
+    static constexpr i64 LONG_STR_HEADER = 5;
+    static constexpr i64 LONG_NESTED_STR_HEADER = 3;
+public:
+    static MixedType
+    MakeInteger(i64 value);
+
+    static MixedType
+    MakeFloat(f64 value);
+
+    static MixedType
+    MakeString(const String& str);
+
+    static MixedType
+    MakeObject();
+
+    static MixedType
+    MakeArray(ptr_t ptr, i32 count);
+
+    static MixedType
+    MakeNull();
+
+    static MixedType
+    MakeMissing();
+
+public:
     MixedType() = default;
 
-    char_t ptr[15];
+    explicit inline
+    MixedType(MixedValueType data_type) : BaseMixedType(data_type) {
+    }
+
+    inline
+    ~MixedType() {
+    }
+
+    MixedType(const MixedType& other);
+    MixedType(MixedType&& other) noexcept;
+    MixedType& operator=(const MixedType& other);
+    MixedType& operator=(MixedType&& other) noexcept;
+
+    void
+    Reset();
 };
 
 
@@ -69,7 +135,7 @@ struct __attribute__((packed)) FloatMixedType : public BaseMixedType {
 struct __attribute__((packed)) LongStrMixedType : public BaseMixedType {
     LongStrMixedType() : BaseMixedType(MixedValueType::kLongStr) {}
 
-    char_t header[5] {}; // 5 bytes is used to store the header of the string to shortcut some computation.
+    char_t header[MixedType::LONG_STR_HEADER] {}; // 5 bytes is used to store the header of the string to shortcut some computation.
     i16 length {0};  // Max string length will be 65535
     ptr_t ptr {nullptr};
 };
@@ -77,7 +143,7 @@ struct __attribute__((packed)) LongStrMixedType : public BaseMixedType {
 struct __attribute__((packed)) ShortStrMixedType : public BaseMixedType {
     ShortStrMixedType() : BaseMixedType(MixedValueType::kShortStr) {}
 
-    char_t ptr[15] {};  // not more than 15 bytes string will use short string type
+    char_t ptr[MixedType::SHORT_STR_LIMIT] {};  // not more than 15 bytes string will use short string type
 };
 
 struct __attribute__((packed)) ObjectMixedType : public BaseMixedType {
@@ -139,7 +205,9 @@ struct __attribute__((packed)) NestedLongStrMixedType : public BaseMixedType {
     NestedLongStrMixedType() : BaseMixedType(MixedValueType::kNestedLongStr) {}
     i16 array_index{};
 
-    char_t header[3] {}; // 3 bytes is used to store the header of the string to shortcut some computation.
+    // 3 bytes is used to store the header of the string to shortcut some computation.
+    char_t header[MixedType::LONG_NESTED_STR_HEADER] {};
+
     i16 length {0};   // Max string length will be 65535
     ptr_t ptr {nullptr};
 };
@@ -148,7 +216,8 @@ struct __attribute__((packed)) NestedShortStrMixedType : public BaseMixedType {
     NestedShortStrMixedType() : BaseMixedType(MixedValueType::kNestedShortStr) {}
     i16 array_index{};
 
-    char_t ptr[13] {}; // not more than 13 bytes string will use short string type
+    // not more than 13 bytes string will use short string type
+    char_t ptr[MixedType::SHORT_NESTED_STR_LIMIT] {};
 };
 
 struct __attribute__((packed)) NestedObjectMixedType : public BaseMixedType {
@@ -185,6 +254,21 @@ struct __attribute__((packed)) NestedMissingMixedType : public BaseMixedType {
     i8 _dummy1{};
     i32 _dummy2{};
     i64 _dummy3{};
+};
+
+struct MixedArray {
+public:
+    ptr_t parent_ptr;
+    MixedType array[0];
+private:
+    static ptr_t
+    Make(i32 count);
+
+    static void
+    Set(i32 index, const MixedType& mixed_value);
+
+    static MixedType&
+    Get(i32 index);
 };
 
 }
