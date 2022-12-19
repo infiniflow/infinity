a54dfbdf6231c8ca816da17805838206a5719992
jinhai <haijin.chn@gmail.com>
2022-12-20 00:07:24 +0800

    Add cast function, part6:
    1. Add tinyint to small/integer/bigint/hugeint test cases.
    
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- test/function/cast/integer_cast.cpp
+++ test/function/cast/integer_cast.cpp
@@ -50,8 +50,7 @@ TEST_F(IntegerCastTest, tiny_integer_cast1) {
         auto tiny2small_ptr = BindIntegerCast<TinyIntT>(tinyint_data_type, smallint_data_type);
         EXPECT_NE(tiny2small_ptr.function, nullptr);
 
-        DataType smallint_type(LogicalType::kSmallInt);
-        ColumnVector col_smallint(smallint_type, ColumnVectorType::kFlat);
+        ColumnVector col_smallint(smallint_data_type, ColumnVectorType::kFlat);
         col_smallint.Initialize();
 
         CastParameters cast_parameters;
@@ -65,6 +64,105 @@ TEST_F(IntegerCastTest, tiny_integer_cast1) {
         }
     }
 
+    // cast tiny column vector to integer column vector
+    {
+        DataType integer_data_type(LogicalType::kInteger);
+        auto tiny2integer_ptr = BindIntegerCast<TinyIntT>(tinyint_data_type, integer_data_type);
+        EXPECT_NE(tiny2integer_ptr.function, nullptr);
+
+        ColumnVector col_int(integer_data_type, ColumnVectorType::kFlat);
+        col_int.Initialize();
+
+        CastParameters cast_parameters;
+        bool result = tiny2integer_ptr.function(col_tinyint, col_int, DEFAULT_VECTOR_SIZE, cast_parameters);
+        EXPECT_TRUE(result);
+        for(i64 i = 0; i < DEFAULT_VECTOR_SIZE; ++ i) {
+            Value vx = col_int.GetValue(i);
+            EXPECT_EQ(vx.type().type(), LogicalType::kInteger);
+            i8 check_value = static_cast<i8>(i);
+            EXPECT_EQ(vx.value_.integer, static_cast<IntegerT>(check_value));
+        }
+    }
+
+    // cast tiny column vector to big int column vector
+    {
+        DataType bigint_data_type(LogicalType::kBigInt);
+        auto tiny2bigint_ptr = BindIntegerCast<TinyIntT>(tinyint_data_type, bigint_data_type);
+        EXPECT_NE(tiny2bigint_ptr.function, nullptr);
+
+        ColumnVector col_bigint(bigint_data_type, ColumnVectorType::kFlat);
+        col_bigint.Initialize();
+
+        CastParameters cast_parameters;
+        bool result = tiny2bigint_ptr.function(col_tinyint, col_bigint, DEFAULT_VECTOR_SIZE, cast_parameters);
+        EXPECT_TRUE(result);
+        for(i64 i = 0; i < DEFAULT_VECTOR_SIZE; ++ i) {
+            Value vx = col_bigint.GetValue(i);
+            EXPECT_EQ(vx.type().type(), LogicalType::kBigInt);
+            i8 check_value = static_cast<i8>(i);
+            EXPECT_EQ(vx.value_.big_int, static_cast<BigIntT>(check_value));
+        }
+    }
+
+    // cast tiny column vector to huge int column vector
+    {
+        DataType hugeint_data_type(LogicalType::kHugeInt);
+        auto tiny2hugeint_ptr = BindIntegerCast<TinyIntT>(tinyint_data_type, hugeint_data_type);
+        EXPECT_NE(tiny2hugeint_ptr.function, nullptr);
+
+        ColumnVector col_hugeint(hugeint_data_type, ColumnVectorType::kFlat);
+        col_hugeint.Initialize();
+
+        CastParameters cast_parameters;
+        bool result = tiny2hugeint_ptr.function(col_tinyint, col_hugeint, DEFAULT_VECTOR_SIZE, cast_parameters);
+        EXPECT_TRUE(result);
+        for(i64 i = 0; i < DEFAULT_VECTOR_SIZE; ++ i) {
+            Value vx = col_hugeint.GetValue(i);
+            EXPECT_EQ(vx.type().type(), LogicalType::kHugeInt);
+            HugeIntT check_value((static_cast<i8>(i) < 0) * -1, static_cast<i8>(i));
+            EXPECT_EQ(vx.value_.huge_int, check_value);
+        }
+    }
+
+    // cast tiny column vector to big int column vector
+    {
+        DataType float_data_type(LogicalType::kFloat);
+        auto tiny2float_ptr = BindIntegerCast<TinyIntT>(tinyint_data_type, float_data_type);
+        EXPECT_NE(tiny2float_ptr.function, nullptr);
+
+        ColumnVector col_float(float_data_type, ColumnVectorType::kFlat);
+        col_float.Initialize();
+
+        CastParameters cast_parameters;
+        bool result = tiny2float_ptr.function(col_tinyint, col_float, DEFAULT_VECTOR_SIZE, cast_parameters);
+        EXPECT_TRUE(result);
+        for(i64 i = 0; i < DEFAULT_VECTOR_SIZE; ++ i) {
+            Value vx = col_float.GetValue(i);
+            EXPECT_EQ(vx.type().type(), LogicalType::kFloat);
+            i8 check_value = static_cast<i8>(i);
+            EXPECT_FLOAT_EQ(vx.value_.float32, static_cast<FloatT>(check_value));
+        }
+    }
+
+    // cast tiny column vector to big int column vector
+    {
+        DataType double_data_type(LogicalType::kDouble);
+        auto tiny2double_ptr = BindIntegerCast<TinyIntT>(tinyint_data_type, double_data_type);
+        EXPECT_NE(tiny2double_ptr.function, nullptr);
+
+        ColumnVector col_double(double_data_type, ColumnVectorType::kFlat);
+        col_double.Initialize();
+
+        CastParameters cast_parameters;
+        bool result = tiny2double_ptr.function(col_tinyint, col_double, DEFAULT_VECTOR_SIZE, cast_parameters);
+        EXPECT_TRUE(result);
+        for(i64 i = 0; i < DEFAULT_VECTOR_SIZE; ++ i) {
+            Value vx = col_double.GetValue(i);
+            EXPECT_EQ(vx.type().type(), LogicalType::kDouble);
+            i8 check_value = static_cast<i8>(i);
+            EXPECT_FLOAT_EQ(vx.value_.float64, static_cast<DoubleT>(check_value));
+        }
+    }
 //    {
 //        DataType source(LogicalType::kSmallInt);
 //        DataType target(LogicalType::kBigInt);
