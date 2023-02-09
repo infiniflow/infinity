5055411b1db36f7f3077cc71e1437529b8329d5d
jinhai <haijin.chn@gmail.com>
2023-02-09 17:33:48 +0800

    Implement aggregate function, part4: complete count function and unit tests
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- src/function/aggregate/count.cpp
+++ src/function/aggregate/count.cpp
@@ -20,12 +20,12 @@ public:
 
     void
     Update(ValueType *__restrict input, SizeT idx) {
-        this->count_ ++;
+        count_ ++;
     }
 
     inline void
     ConstantUpdate(ValueType *__restrict input, SizeT idx, SizeT count) {
-        this->count_ =+ count;
+        count_ += count;
     }
 
     ResultType
@@ -45,16 +45,237 @@ RegisterCountFunction(const UniquePtr<Catalog> &catalog_ptr) {
 
     SharedPtr<AggregateFunctionSet> function_set_ptr = MakeShared<AggregateFunctionSet>(func_name);
 
-    AggregateFunction count_boolean
-            = UnaryAggregate<CountState<BooleanT, BigIntT>, BooleanT, BigIntT>(func_name,
-                                                                               DataType(LogicalType::kBoolean),
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<BooleanT, BigIntT>, BooleanT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kBoolean),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<TinyIntT, BigIntT>, TinyIntT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kTinyInt),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<SmallIntT, BigIntT>, SmallIntT, BigIntT>(func_name,
+                                                                                     DataType(LogicalType::kSmallInt),
+                                                                                     DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<IntegerT, BigIntT>, IntegerT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kInteger),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<BigIntT, BigIntT>, BigIntT, BigIntT>(func_name,
+                                                                                 DataType(LogicalType::kBigInt),
+                                                                                 DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<HugeIntT, BigIntT>, HugeIntT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kHugeInt),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<FloatT, BigIntT>, FloatT, BigIntT>(func_name,
+                                                                               DataType(LogicalType::kFloat),
                                                                                DataType(LogicalType::kBigInt));
-    function_set_ptr->AddFunction(count_boolean);
-    AggregateFunction count_tinyint
-            = UnaryAggregate<CountState<TinyIntT, BigIntT>, TinyIntT, BigIntT>(func_name,
-                                                                               DataType(LogicalType::kTinyInt),
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<DoubleT, BigIntT>, DoubleT, BigIntT>(func_name,
+                                                                                 DataType(LogicalType::kDouble),
+                                                                                 DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<Decimal16T, BigIntT>, Decimal16T, BigIntT>(func_name,
+                                                                                       DataType(LogicalType::kDecimal16),
+                                                                                       DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<Decimal32T, BigIntT>, Decimal32T, BigIntT>(func_name,
+                                                                                       DataType(LogicalType::kDecimal32),
+                                                                                       DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<Decimal64T, BigIntT>, Decimal64T, BigIntT>(func_name,
+                                                                                       DataType(LogicalType::kDecimal64),
+                                                                                       DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<Decimal128T, BigIntT>, Decimal128T, BigIntT>(func_name,
+                                                                                         DataType(LogicalType::kDecimal128),
+                                                                                         DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<VarcharT, BigIntT>, VarcharT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kVarchar),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<CharT, BigIntT>, CharT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kChar),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<DateT, BigIntT>, DateT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kDate),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<TimeT, BigIntT>, TimeT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kTime),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<DateTimeT, BigIntT>, DateTimeT, BigIntT>(func_name,
+                                                                                     DataType(LogicalType::kDateTime),
+                                                                                     DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<TimestampT, BigIntT>, TimestampT, BigIntT>(func_name,
+                                                                                       DataType(LogicalType::kTimestamp),
+                                                                                       DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<TimestampTZT, BigIntT>, TimestampTZT, BigIntT>(func_name,
+                                                                                         DataType(LogicalType::kTimestampTZ),
+                                                                                         DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<ArrayT, BigIntT>, ArrayT, BigIntT>(func_name,
+                                                                               DataType(LogicalType::kArray),
                                                                                DataType(LogicalType::kBigInt));
-    function_set_ptr->AddFunction(count_tinyint);
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<TupleT, BigIntT>, TupleT, BigIntT>(func_name,
+                                                                               DataType(LogicalType::kTuple),
+                                                                               DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<PointT, BigIntT>, PointT, BigIntT>(func_name,
+                                                                               DataType(LogicalType::kPoint),
+                                                                               DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<LineT, BigIntT>, LineT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kLine),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<LineSegT, BigIntT>, LineSegT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kLineSeg),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<BoxT, BigIntT>, BoxT, BigIntT>(func_name,
+                                                                           DataType(LogicalType::kBox),
+                                                                           DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<PathT, BigIntT>, PathT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kPath),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<PolygonT, BigIntT>, PolygonT, BigIntT>(func_name,
+                                                                                   DataType(LogicalType::kPolygon),
+                                                                                   DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<CircleT, BigIntT>, CircleT, BigIntT>(func_name,
+                                                                                 DataType(LogicalType::kCircle),
+                                                                                 DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<BitmapT, BigIntT>, BitmapT, BigIntT>(func_name,
+                                                                                 DataType(LogicalType::kBitmap),
+                                                                                 DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<UuidT, BigIntT>, UuidT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kUuid),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<BlobT, BigIntT>, BlobT, BigIntT>(func_name,
+                                                                             DataType(LogicalType::kBlob),
+                                                                             DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<EmbeddingT, BigIntT>, EmbeddingT, BigIntT>(func_name,
+                                                                                       DataType(LogicalType::kEmbedding),
+                                                                                       DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
+    {
+        AggregateFunction count_function
+                = UnaryAggregate<CountState<MixedT, BigIntT>, MixedT, BigIntT>(func_name,
+                                                                               DataType(LogicalType::kMixed),
+                                                                               DataType(LogicalType::kBigInt));
+        function_set_ptr->AddFunction(count_function);
+    }
     catalog_ptr->AddFunctionSet(function_set_ptr);
 }
 
---
+++ test/unittest/function/aggregate/count_functions.cpp
@@ -0,0 +1,300 @@
+//
+// Created by jinhai on 23-1-5.
+//
+
+#include <gtest/gtest.h>
+#include "base_test.h"
+#include "common/column_vector/column_vector.h"
+#include "common/types/value.h"
+#include "main/logger.h"
+#include "main/stats/global_resource_usage.h"
+#include "main/infinity.h"
+#include "common/types/info/varchar_info.h"
+#include "storage/catalog.h"
+#include "function/aggregate/count.h"
+#include "function/aggregate_function_set.h"
+#include "expression/column_expression.h"
+
+class CountFunctionTest : public BaseTest {
+    void
+    SetUp() override {
+        infinity::GlobalResourceUsage::Init();
+        infinity::Infinity::instance().Init();
+    }
+
+    void
+    TearDown() override {
+        infinity::Infinity::instance().UnInit();
+        EXPECT_EQ(infinity::GlobalResourceUsage::GetObjectCount(), 0);
+        EXPECT_EQ(infinity::GlobalResourceUsage::GetRawMemoryCount(), 0);
+        infinity::GlobalResourceUsage::UnInit();
+    }
+};
+
+TEST_F(CountFunctionTest, avg_func) {
+    using namespace infinity;
+
+    UniquePtr<Catalog> catalog_ptr = MakeUnique<Catalog>();
+
+    RegisterCountFunction(catalog_ptr);
+
+    String op = "count";
+    SharedPtr<FunctionSet> function_set = catalog_ptr->GetFunctionSetByName(op);
+    EXPECT_EQ(function_set->type_, FunctionType::kAggregate);
+    SharedPtr<AggregateFunctionSet> aggregate_function_set = std::static_pointer_cast<AggregateFunctionSet>(function_set);
+    {
+        DataType data_type(LogicalType::kBoolean);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(Boolean)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeBool(i % 2 == 0));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+    {
+        DataType data_type(LogicalType::kTinyInt);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(TinyInt)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeTinyInt(i));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+    {
+        DataType data_type(LogicalType::kSmallInt);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(SmallInt)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeSmallInt(static_cast<SmallIntT>(i)));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+
+    {
+        DataType data_type(LogicalType::kInteger);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(Integer)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeInt(static_cast<IntegerT>(2 * i)));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+
+    {
+        DataType data_type(LogicalType::kBigInt);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(BigInt)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeBigInt(static_cast<BigIntT>(2 * i)));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+
+    {
+        DataType data_type(LogicalType::kHugeInt);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(HugeInt)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            HugeIntT input(0, i);
+            Value v = Value::MakeHugeInt(input);
+            data_block.AppendValue(0, v);
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+
+    {
+        DataType data_type(LogicalType::kFloat);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(Float)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeFloat(static_cast<FloatT>(2 * i)));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+
+    {
+        DataType data_type(LogicalType::kDouble);
+        SharedPtr<ColumnExpression> col_expr_ptr = MakeShared<ColumnExpression>(data_type,
+                                                                                "t1",
+                                                                                "c1",
+                                                                                0,
+                                                                                0);
+
+        AggregateFunction func = aggregate_function_set->GetMostMatchFunction(col_expr_ptr);
+        EXPECT_STREQ("COUNT(Double)->BigInt", func.ToString().c_str());
+
+        Vector<DataType> column_types;
+        column_types.emplace_back(data_type);
+
+        SizeT row_count = DEFAULT_VECTOR_SIZE;
+
+        DataBlock data_block;
+        data_block.Init(column_types);
+
+        for (SizeT i = 0; i < row_count; ++i) {
+            data_block.AppendValue(0, Value::MakeDouble(static_cast<DoubleT>(2 * i)));
+        }
+        data_block.Finalize();
+
+        func.init_func_(func.GetState());
+        func.update_func_(func.GetState(), data_block.column_vectors[0]);
+        BigIntT result;
+        func.finalize_func_(func.GetState(), (ptr_t)(&result));
+
+        EXPECT_FLOAT_EQ(result, row_count);
+    }
+}
