0af6e1f05a1a0cd8b2d8bac08390adeb351e2c0d
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2023-08-26 23:41:02 +0800

    Fix compile

--- benchmark/numericstore/test_numericstore.cpp
+++ benchmark/numericstore/test_numericstore.cpp
@@ -9,14 +9,16 @@ int main() {
     std::string data_path = "./numeric";
     NumericDB db(data_path, 1);
     SharedPtr<TableDef> table_def;
+
     Vector<SharedPtr<ColumnDef>> columns;
     columns.push_back(MakeShared<ColumnDef>(0,
                                             MakeShared<DataType>(DataType(LogicalType::kInteger)),
                                             "c1",
                                             HashSet<ConstraintType>())
                       );
-
-    table_def = MakeShared<TableDef>("test",columns);
+    SharedPtr<String> schema = MakeShared<String>("schema");
+    SharedPtr<String> table_name = MakeShared<String>("test");
+    table_def = MakeShared<TableDef>(schema, table_name, columns);
     db.Create(table_def);
 
 
