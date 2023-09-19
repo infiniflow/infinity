0b05623944c2972ad490985474a51f7a5a4d3aa8
Xwg <loloxwg@gmail.com>
2023-09-19 17:47:12 +0800

    Fix indentation of fragment

--- src/executor/explain_fragment.cpp
+++ src/executor/explain_fragment.cpp
@@ -8,8 +8,8 @@
 namespace infinity {
 
 void
-ExplainFragment::Explain(PlanFragment* fragment_ptr,
-                         SharedPtr<Vector<SharedPtr<String>>>& result,
+ExplainFragment::Explain(PlanFragment *fragment_ptr,
+                         SharedPtr<Vector<SharedPtr<String>>> &result,
                          i64 intent_size) {
     {
         String fragment_header;
@@ -23,25 +23,26 @@ ExplainFragment::Explain(PlanFragment* fragment_ptr,
         result->emplace_back(MakeShared<String>(fragment_header));
     }
 
-    intent_size += 2;
-
-    if(fragment_ptr->GetSinkNode()) {
-        ExplainPhysicalPlan::Explain(fragment_ptr->GetSinkNode(), result, intent_size);
+    if (fragment_ptr->GetSinkNode()) {
+        ExplainPhysicalPlan::Explain(fragment_ptr->GetSinkNode(), result, true, intent_size);
     }
 
-    Vector<PhysicalOperator*>& fragment_operators = fragment_ptr->GetOperators();
-    for (auto & fragment_operator : fragment_operators) {
+    Vector<PhysicalOperator *> &fragment_operators = fragment_ptr->GetOperators();
+    for (auto &fragment_operator: fragment_operators) {
         ExplainPhysicalPlan::Explain(fragment_operator, result, true, intent_size);
     }
 
-    if(fragment_ptr->GetSourceNode()) {
-        ExplainPhysicalPlan::Explain(fragment_ptr->GetSourceNode(), result, intent_size);
+    if (fragment_ptr->GetSourceNode()) {
+        ExplainPhysicalPlan::Explain(fragment_ptr->GetSourceNode(), result, true, intent_size);
     }
 
+    intent_size += 2;
+
     // NOTE: recursive call this function to explain child fragment
-    if (!fragment_ptr->Children().empty()){
-        for (auto& child : fragment_ptr->Children()) {
-            ExplainFragment::Explain(child.get(), result);
+    if (!fragment_ptr->Children().empty()) {
+        for (auto &child: fragment_ptr->Children()) {
+            intent_size += 2;
+            ExplainFragment::Explain(child.get(), result, intent_size);
         }
     }
 }
