2fdeb2f7c5d50235328417824288d41345514d9a
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2023-10-03 15:39:16 +0800

    Tiny

--- src/executor/operator/physical_merge_knn.h
+++ src/executor/operator/physical_merge_knn.h
@@ -12,21 +12,22 @@ namespace infinity {
 
 class PhysicalMergeKnn final : public PhysicalOperator {
 public:
-    explicit PhysicalMergeKnn(u64 id,
-                              SharedPtr<PhysicalOperator> left,
-                              SharedPtr<Vector<String>> output_names,
-                              SharedPtr<Vector<SharedPtr<DataType>>> output_types,
-                              Vector<SharedPtr<BaseExpression>> knn_exprs,
-                              SharedPtr<BaseExpression> limit_expr,
-                              OrderType order_by_type,
-                              u64 knn_table_index)
-        : PhysicalOperator(PhysicalOperatorType::kMergeKnn, std::move(left), nullptr, id),
-          output_names_(std::move(output_names)),
-          output_types_(std::move(output_types)),
-          knn_expressions_(std::move(knn_exprs)),
-          limit_expression_(std::move(limit_expr)),
-          order_by_type_(order_by_type),
-          knn_table_index_(knn_table_index) {
+    explicit
+    PhysicalMergeKnn(u64 id,
+                     SharedPtr<PhysicalOperator> left,
+                     SharedPtr<Vector<String>> output_names,
+                     SharedPtr<Vector<SharedPtr<DataType>>> output_types,
+                     Vector<SharedPtr<BaseExpression>> knn_exprs,
+                     SharedPtr<BaseExpression> limit_expr,
+                     OrderType order_by_type,
+                     u64 knn_table_index)
+            : PhysicalOperator(PhysicalOperatorType::kMergeKnn, std::move(left), nullptr, id),
+              output_names_(std::move(output_names)),
+              output_types_(std::move(output_types)),
+              knn_expressions_(std::move(knn_exprs)),
+              limit_expression_(std::move(limit_expr)),
+              order_by_type_(order_by_type),
+              knn_table_index_(knn_table_index) {
     }
 
     ~PhysicalMergeKnn() override = default;
@@ -63,6 +64,7 @@ private:
     Vector<SharedPtr<BaseExpression>> knn_expressions_{};
     SharedPtr<BaseExpression> limit_expression_{};
     OrderType order_by_type_{OrderType::kAsc};
+
 };
 
-}// namespace infinity
+}
