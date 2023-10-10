c2ffa1ff4c33c740f5d4cef3e6ae312393eb24af
Zhichang Yu <yuzhichang@gmail.com>
2023-10-10 13:20:17 +0800

    Insert mulptile rows at once (#77)

--- src/executor/operator/physical_insert.cpp
+++ src/executor/operator/physical_insert.cpp
@@ -13,41 +13,35 @@ namespace infinity {
 void PhysicalInsert::Init() {}
 
 void PhysicalInsert::Execute(QueryContext *query_context, InputState *input_state, OutputState *output_state) {
-    SizeT column_count = value_list_.size();
+    SizeT row_count = value_list_.size();
+    SizeT column_count = value_list_[0].size();
     SizeT table_collection_column_count = table_collection_entry_->columns_.size();
     if (column_count != table_collection_column_count) {
         ExecutorError(fmt::format("Insert values count{} isn't matched with table column count{}.", column_count, table_collection_column_count));
     }
 
-    // Prepare the expression states
-    Vector<SharedPtr<ExpressionState>> expr_states;
-    expr_states.reserve(column_count);
-
     // Prepare the output block
     Vector<SharedPtr<DataType>> output_types;
     output_types.reserve(column_count);
-
-    for (auto &expr : value_list_) {
-        // expression state
-        expr_states.emplace_back(ExpressionState::CreateState(expr));
-        // for output block
+    for (auto &expr : value_list_[0]) {
         output_types.emplace_back(MakeShared<DataType>(expr->Type()));
     }
-
     SharedPtr<DataBlock> output_block = DataBlock::Make();
     output_block->Init(output_types);
+    SharedPtr<DataBlock> output_block_tmp = DataBlock::Make();
+    output_block_tmp->Init(output_types);
 
     ExpressionEvaluator evaluator;
     evaluator.Init(nullptr);
-    for (SizeT expr_idx = 0; expr_idx < column_count; ++expr_idx) {
-        Vector<SharedPtr<ColumnVector>> blocks_column;
-        blocks_column.emplace_back(output_block->column_vectors[expr_idx]);
-        evaluator.Execute(value_list_[expr_idx], expr_states[expr_idx], output_block->column_vectors[expr_idx]);
-        if (blocks_column[0].get() != output_block->column_vectors[expr_idx].get()) {
-            ExecutorError("Unexpected error");
+    // Each cell's expression of a column may differ. So we have to evaluate each cell instead of column here.
+    for (SizeT row_idx = 0; row_idx < row_count; ++row_idx) {
+        for (SizeT expr_idx = 0; expr_idx < column_count; ++expr_idx) {
+            const SharedPtr<BaseExpression> &expr = value_list_[row_idx][expr_idx];
+            SharedPtr<ExpressionState> expr_state = ExpressionState::CreateState(expr);
+            evaluator.Execute(expr, expr_state, output_block_tmp->column_vectors[expr_idx]);
         }
+        output_block->AppendWith(output_block_tmp);
     }
-
     output_block->Finalize();
 
     auto *txn = query_context->GetTxn();
