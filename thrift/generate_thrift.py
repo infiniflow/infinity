23d8d894f1c2ba54aa251270f5954ead63cd882d
Xwg <loloxwg@gmail.com>
2023-11-17 17:44:06 +0800

    Add Infinity-RPC client for Python

---
+++ thrift/generate_thrift.py
@@ -0,0 +1,14 @@
+import os
+
+
+def generate_thrift():
+    parent_dir = os.path.dirname(os.getcwd())
+    python_dir = parent_dir + "/python/infinity-thrift/remote"
+    thrift_file = "infinity.thrift"
+    os.system("thrift --version")
+    os.system(f"thrift --out {python_dir} --gen py {thrift_file}")
+    # os.system(f"thrift --out {cpp_dir} --gen cpp {thrift_file}")
+
+
+if __name__ == '__main__':
+    generate_thrift()
--- thrift/infinity.thrift
+++ thrift/infinity.thrift
@@ -1,10 +1,131 @@
 
-namespace py3 infinity_thrift
+namespace py infinity_thrift_rpc
+namespace cpp infinity_thrift_rpc
 
+
+enum LogicType{
+    Boolean,
+    TinyInt,
+    SmallInt,
+    Integer,
+    BigInt,
+    HugeInt,
+    Decimal,
+    Float,
+    Double,
+    Varchar,
+    Embedding,
+    Invalid
+}
+
+struct Option {}
 
 struct CreateDatabaseRequest {
-     1:  string db_name,
-     2:  i64 session_id,
+    1:  string db_name,
+    2:  i64 session_id,
+    3:  Option option,
+}
+
+struct DropDatabaseRequest {
+    1:  string db_name,
+    2:  i64 session_id,
+    3:  Option option,
+}
+
+struct CreateTableRequest {
+    1:  string db_name,
+    2:  string table_name,
+    3:  list<ColumnDef> column_defs = [],
+    6:  i64 session_id,
+    7:  Option option,
+}
+
+enum Constraint {
+    PrimaryKey,
+    NotNull,
+    Null,
+    Unique,
+}
+
+struct ColumnDef {
+    1:  i32 id,
+    2:  string name,
+    3:  DataType data_type,
+    4:  Constraint constraint = [],
+}
+
+struct DataType {
+    1:  LogicType logic_type,
+    2:  PhysicalType physical_type,
+}
+
+union PhysicalType {
+    1:  NumberType number_type,
+    2:  VarcharType varchar_type,
+    3:  EmbeddingType embedding_type,
+
+}
+
+struct NumberType {}
+
+struct VarcharType {}
+
+struct EmbeddingType {
+    1:  i32 dimension,
+    2:  ElementType element_type,
+}
+
+enum ElementType {
+    ElementBit,
+    ElementInt8,
+    ElementInt16,
+    ElementInt32,
+    ElementInt64,
+    ElementFloat32,
+    ElementFloat64,
+}
+
+struct DropTableRequest {
+    1:  string db_name,
+    2:  string table_name,
+    3:  i64 session_id,
+    4:  Option option,
+}
+
+struct InsertRequest {
+    1:  string db_name,
+    2:  string table_name,
+    3:  list<string> column_names = [],
+    4:  list<Field> fields = [],
+    5:  i64 session_id,
+}
+
+struct Field {
+    1: list<ParsedExpr> parse_exprs = [],
+}
+
+struct ImportRequest{
+    1:  string db_name,
+    2:  string table_name,
+    3:  string file_path,
+    4:  ImportOption import_option,
+    5:  i64 session_id,
+}
+
+struct ImportOption {
+    1:  string delimiter,
+    2:  string file_type,
+    3:  string table_name,
+    4:  string schema_name,
+    5:  bool copy_from,
+    6:  bool has_header,
+    7:  CopyFileType copy_file_type,
+}
+
+enum CopyFileType {
+    CSV,
+    JSON,
+    FVECS,
 }
 
 struct CommonResponse {
@@ -17,38 +138,32 @@ struct CommonRequest {
     1:  i64 session_id,
 }
 
-service InfinityService {
-    CommonResponse Connect(1:CommonRequest req),
-    CommonResponse CreateDatabase(1:CreateDatabaseRequest req)
-}
-
-
-
 union ParsedExprType {
-   1: ConstantExpr constant_expr,
-   2: ColumnExpr column_expr,
-   3: FunctionExpr function_expr,
+    1: ConstantExpr constant_expr,
+    2: ColumnExpr column_expr,
+    3: FunctionExpr function_expr,
+    4: KnnExpr knn_expr,
 }
 
 struct ParsedExpr {
-   1: ParsedExprType type ,
-   2: string alias,
+    1: ParsedExprType type ,
+    2: string alias,
 }
 
 enum LiteralType {
-    kBoolean,
-    kDouble,
-    kString,
-    kInt64,
-    kNull,
-    kIntegerArray,
-    kDoubleArray,
+    Boolean,
+    Double,
+    String,
+    Int64,
+    Null,
+    IntegerArray,
+    DoubleArray,
 }
 
 struct ConstantExpr {
     1: LiteralType literal_type,
     2: bool bool_value,
-    3: int64 i64_value,
+    3: i64 i64_value,
     4: double f64_value,
     5: string str_value,
 }
@@ -63,5 +178,168 @@ struct FunctionExpr {
     2: list<ParsedExpr> arguments =[],
 }
 
+struct BetweenExpr {
+    1: ParsedExpr value,
+    2: ParsedExpr upper_bound,
+    3: ParsedExpr lower_bound,
+}
 
+struct KnnExpr {
+    1: ParsedExpr column_expr,
+    2: list<string> embedding_data,
+    3: i64 dimension, 
+    4: KnnDistanceType distance_type,
+    5: EmbeddingType embedding_type,
+}
 
+enum KnnDistanceType{
+    L2,
+    Cosine,
+    InnerProduct,
+    Hamming,
+}
+
+struct SelectRequest {
+    1:  string db_name,
+    2:  string table_name,
+    3:  list<ParsedExpr> select_list = [],
+    4:  ParsedExpr where_expr,
+    5:  list<ParsedExpr> group_by_list = [],
+    6:  ParsedExpr having_expr,
+    7:  ParsedExpr limit_expr,
+    8:  ParsedExpr offset_expr,
+    9:  i64 session_id,
+}
+
+struct SelectResponse {
+    1: bool success,
+    2: string error_msg,
+    3: list<ColumnDef> column_defs = [],
+
+}
+
+struct ColumnField {
+    1: ColumnType column_type,
+    2: binary column_vector,
+}
+
+enum ColumnType {
+    ColumnBool,
+    ColumnInt8,
+    ColumnInt16,
+    ColumnInt32,
+    ColumnInt64,
+    ColumnFloat32,
+    ColumnFloat64,
+    ColumnVerchar,
+    ColumnEmbedding,
+    ColumnInvalid,
+}
+
+struct ListDatabaseRequest {
+    1: i64 session_id,
+}
+
+struct ListDatabaseResponse {
+    1: bool success,
+    2: string error_msg,
+    3: list<string> db_names = [],
+}
+
+struct ListTableRequest {
+    1: string db_name,
+    2: i64 session_id,
+}
+
+struct ListTableResponse {
+    1: bool success,
+    2: string error_msg,
+    3: list<string> table_names = [],
+}
+
+struct DescribeDatabaseRequest {
+    1: string db_name,
+    2: i64 session_id,
+}
+
+struct DescribeDatabaseResponse {
+    1: bool success,
+    2: string error_msg,
+    3: i64 num_segments,
+    4: i64 num_rows,
+    5: i64 num_blocks,
+}
+
+struct DescribeTableRequest {
+    1: string db_name,
+    2: string table_name,
+    3: i64 session_id,
+}
+
+struct DescribeTableResponse {
+    1: bool success,
+    2: string error_msg,
+    3: i64 num_segments,
+    4: i64 num_rows,
+    5: i64 num_blocks,
+}
+
+struct GetTableRequest {
+    1: string db_name,
+    2: string table_name,
+    3: i64 session_id,
+}
+
+struct CreateIndexRequest {
+    1: string db_name,
+    2: string table_name,
+    3: string index_name,
+    4: list<string> column_names = [],
+    5: string method_type,
+    6: list<InitParameter> index_para_list = [],
+    7: i64 session_id,
+    8: Option option,
+}
+
+struct InitParameter {
+    1: string para_name,
+    2: string para_value,
+}
+
+struct DropIndexRequest {
+    1: string db_name,
+    2: string table_name,
+    3: string index_name,
+    4: i64 session_id,
+}
+
+struct GetDatabaseRequest {
+    1: string db_name,
+    2: i64 session_id,
+}
+
+// Service
+service InfinityService {
+    CommonResponse Connect(),
+    CommonResponse Disconnect(1:CommonRequest request),
+
+    CommonResponse CreateDatabase(1:CreateDatabaseRequest request),
+    CommonResponse DropDatabase(1:DropDatabaseRequest request),
+    CommonResponse CreateTable(1:CreateTableRequest request),
+    CommonResponse DropTable(1:DropTableRequest request),
+    CommonResponse Insert(1:InsertRequest request),
+    CommonResponse Import(1:ImportRequest request),
+    SelectResponse Select(1:SelectRequest request),
+
+    ListDatabaseResponse ListDatabase(1:ListDatabaseRequest request),
+    ListTableResponse ListTable(1:ListTableRequest request),
+
+    DescribeDatabaseResponse DescribeDatabase(1:DescribeDatabaseRequest request),
+    DescribeTableResponse DescribeTable(1:DescribeTableRequest request),
+
+    CommonResponse GetDatabase(1:GetDatabaseRequest request),
+    CommonResponse GetTable(1:GetTableRequest request),
+
+    CommonResponse CreateIndex(1:CreateIndexRequest request),
+    CommonResponse DropIndex(1:DropIndexRequest request),
+}
