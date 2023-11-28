ce54521a87304846fe75f3ef0f084dba84115904
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2023-11-28 17:57:40 +0800

    Make create irs index work

--- src/storage/meta/entry/irs_index_entry.cpp
+++ src/storage/meta/entry/irs_index_entry.cpp
@@ -19,7 +19,11 @@ import base_entry;
 import third_party;
 import infinity_exception;
 import table_index_entry;
+import table_index_meta;
 import buffer_manager;
+import iresearch_datastore;
+import index_base;
+import logger;
 
 module irs_index_entry;
 
@@ -32,20 +36,41 @@ IrsIndexEntry::IrsIndexEntry(TableIndexEntry *table_index_entry, SharedPtr<Strin
 }
 
 Json IrsIndexEntry::Serialize(const IrsIndexEntry *index_def_entry, TxnTimeStamp max_commit_ts) {
-    Error<StorageException>("Not implemented");
-    Json irs_index_entry_json;
+    Json json;
+    json["txn_id"] = index_def_entry->txn_id_.load();
+    json["begin_ts"] = index_def_entry->begin_ts_;
+    json["commit_ts"] = index_def_entry->commit_ts_.load();
+    json["index_dir"] = *index_def_entry->index_dir_;
 
-    return irs_index_entry_json;
+    return json;
 }
 
 SharedPtr<IrsIndexEntry> IrsIndexEntry::Deserialize(const Json &index_def_entry_json, TableIndexEntry *table_index_entry, BufferManager *buffer_mgr) {
-    Error<StorageException>("Not implemented");
-    return nullptr;
+    u64 txn_id = index_def_entry_json["txn_id"];
+    TxnTimeStamp begin_ts = index_def_entry_json["begin_ts"];
+    TxnTimeStamp commit_ts = index_def_entry_json["commit_ts"];
+    auto index_dir = MakeShared<String>(index_def_entry_json["index_dir"]);
+    auto irs_index_entry = MakeShared<IrsIndexEntry>(table_index_entry, index_dir, txn_id, begin_ts);
+    irs_index_entry->commit_ts_.store(commit_ts);
+
+    return irs_index_entry;
 }
 
 SharedPtr<String> IrsIndexEntry::DetermineIndexDir(const String &parent_dir, const String &index_name) {
     Error<StorageException>("Not implemented");
     return nullptr;
+}
+
+SharedPtr<IrsIndexEntry> IrsIndexEntry::NewIrsIndexEntry(HashMap<u64, SharedPtr<IndexBase>> index_info_map,
+                                                         TableIndexEntry *table_index_entry,
+                                                         u64 txn_id,
+                                                         SharedPtr<String> index_dir,
+                                                         TxnTimeStamp begin_ts) {
+    auto irs_index_entry = MakeShared<IrsIndexEntry>(table_index_entry, index_dir, txn_id, begin_ts);
+    irs_index_entry->irs_index_ =
+        MakeShared<IRSDataStore>(*(TableIndexMeta::GetTableCollectionEntry(table_index_entry->table_index_meta_)->table_collection_name_),
+                                 *(index_dir));
+    return irs_index_entry;
 }
 
 } // namespace infinity
