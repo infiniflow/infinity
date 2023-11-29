80f130a4b35489579bbbda06fb3814cd7af9ca65
jinhai <haijin.chn@gmail.com>
2023-11-29 15:22:47 +0800

    Remove thrift compiler and test
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- third_party/thrift/build_cmake/cmake/DefineOptions.cmake
+++ third_party/thrift/build_cmake/cmake/DefineOptions.cmake
@@ -23,7 +23,7 @@ include(CMakeDependentOption)
 set(THRIFT_COMPILER "" CACHE FILEPATH "External Thrift compiler to use during build")
 
 # Additional components
-option(BUILD_COMPILER "Build Thrift compiler" ON)
+option(BUILD_COMPILER "Build Thrift compiler" OFF)
 
 if(BUILD_COMPILER OR EXISTS ${THRIFT_COMPILER})
     set(HAVE_COMPILER ON)
