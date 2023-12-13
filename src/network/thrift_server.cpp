935e220aa8ba27821ec12ec5047f353444c25e0f
Xwg <loloxwg@gmail.com>
2023-12-13 12:30:47 +0800

    Add server profiler

--- src/network/thrift_server.cpp
+++ src/network/thrift_server.cpp
@@ -30,7 +30,6 @@
 #include "infinity_thrift/InfinityService.h"
 #include "infinity_thrift/infinity_types.h"
 
-#include <expr/knn_expr.h>
 
 import infinity;
 import stl;
@@ -312,17 +311,17 @@ public:
 
     void Select(infinity_thrift_rpc::SelectResponse &response, const infinity_thrift_rpc::SelectRequest &request) override {
         ++count_;
-        auto start1 = std::chrono::high_resolution_clock::now();
+        auto start1 = std::chrono::steady_clock::now();
 
         auto infinity = GetInfinityBySessionID(request.session_id);
         auto database = infinity->GetDatabase(request.db_name);
         auto table = database->GetTable(request.table_name);
 
-        auto end1 = std::chrono::high_resolution_clock::now();
+        auto end1 = std::chrono::steady_clock::now();
 
         phase_1_duration_ += end1 - start1;
 
-        auto start2 = std::chrono::high_resolution_clock::now();
+        auto start2 = std::chrono::steady_clock::now();
 
         // select list
         if (request.__isset.select_list == false) {
@@ -381,19 +380,18 @@ public:
             limit = GetParsedExprFromProto(request.limit_expr);
         }
 
-        auto end2 = std::chrono::high_resolution_clock::now();
+        auto end2 = std::chrono::steady_clock::now();
         phase_2_duration_ += end2 - start2;
 
-        auto start3 = std::chrono::high_resolution_clock::now();
+        auto start3 = std::chrono::steady_clock::now();
 
         const QueryResult result = table->Search(search_expr, filter, output_columns);
 
-        auto end3 = std::chrono::high_resolution_clock::now();
+        auto end3 = std::chrono::steady_clock::now();
 
         phase_3_duration_ += end3 - start3;
 
-
-        auto start4 = std::chrono::high_resolution_clock::now();
+        auto start4 = std::chrono::steady_clock::now();
 
         if (result.IsOk()) {
             auto data_block_count = result.result_table_->DataBlockCount();
@@ -443,19 +441,25 @@ public:
             LOG_ERROR(Format("THRIFT ERROR: {}", result.ErrorStr()));
         }
 
-        auto end4 = std::chrono::high_resolution_clock::now();
+        auto end4 = std::chrono::steady_clock::now();
         phase_4_duration_ += end4 - start4;
 
         if (count_ % 10000 == 0) {
             LOG_ERROR(Format("Phase 1: {} Phase 2: {} Phase 3: {} Phase 4: {}  seconds",
-                            phase_1_duration_.count(),
-                            phase_2_duration_.count(),
-                            phase_3_duration_.count(),
-                            phase_4_duration_.count()));
+                             phase_1_duration_.count(),
+                             phase_2_duration_.count(),
+                             phase_3_duration_.count(),
+                             phase_4_duration_.count()));
             phase_1_duration_ = std::chrono::duration<double>();
             phase_2_duration_ = std::chrono::duration<double>();
             phase_3_duration_ = std::chrono::duration<double>();
             phase_4_duration_ = std::chrono::duration<double>();
+        } else if (count_ % 1000 == 0) {
+            LOG_ERROR(Format("Phase 1: {} Phase 2: {} Phase 3: {} Phase 4: {}  seconds",
+                             phase_1_duration_.count(),
+                             phase_2_duration_.count(),
+                             phase_3_duration_.count(),
+                             phase_4_duration_.count()));
         }
     }
 
