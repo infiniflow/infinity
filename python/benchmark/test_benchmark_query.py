280ed7ef97d1eb802407113320a69876f0e6440a
Xwg <loloxwg@gmail.com>
2023-12-14 14:42:19 +0800

    Add new fvecs_read_all 3

--- python/benchmark/test_benchmark_query.py
+++ python/benchmark/test_benchmark_query.py
@@ -53,20 +53,98 @@ def fvecs_read_all(filename):
     return vectors
 
 
+def read_groundtruth(filename):
+    vectors = []
+    with open(filename, 'rb') as f:
+        while True:
+            try:
+                dims = struct.unpack('i', f.read(4))[0]
+                vec = struct.unpack('{}i'.format(dims), f.read(4 * dims))
+                assert dims == len(vec)
+                vectors.append(list(vec))
+            except struct.error:
+                break
+
+    print("len(vectors): ", len(vectors))
+
+    gt_count = len(vectors)
+    gt_top_k = len(vectors[0])
+
+    ground_truth_sets_1 = [set() for _ in range(gt_count)]
+    ground_truth_sets_10 = [set() for _ in range(gt_count)]
+    ground_truth_sets_100 = [set() for _ in range(gt_count)]
+
+    for i in range(gt_count):
+        for j in range(gt_top_k):
+            x = vectors[i][j]
+            if j < 1:
+                ground_truth_sets_1[i].add(x)
+            if j < 10:
+                ground_truth_sets_10[i].add(x)
+            if j < 100:
+                ground_truth_sets_100[i].add(x)
+
+    return ground_truth_sets_1, ground_truth_sets_10, ground_truth_sets_100
+
+
+def calculate_recall(ground_truth_sets, result_sets):
+    recall = 0.0
+    for i in range(len(ground_truth_sets)):
+        if ground_truth_sets[i] in result_sets:
+            recall += 1
+    return recall / len(result_sets) * len(ground_truth_sets)
+
+
+def calculate_recall_all(ground_truth_sets_1, ground_truth_sets_10, ground_truth_sets_100, query_results):
+    correct_1 = 0
+    correct_10 = 0
+    correct_100 = 0
+
+    for query_idx in range(len(query_results)):
+        result = query_results[query_idx]
+        ground_truth_1 = ground_truth_sets_1[query_idx]
+        ground_truth_10 = ground_truth_sets_10[query_idx]
+        ground_truth_100 = ground_truth_sets_100[query_idx]
+
+        for i in range(len(result)):
+            if i < 1 and result[i] in ground_truth_1:
+                correct_1 += 1
+            if i < 10 and result[i] in ground_truth_10:
+                correct_10 += 1
+            if i < 100 and result[i] in ground_truth_100:
+                correct_100 += 1
+
+    recall_1 = correct_1 // len(query_results) * 1
+    recall_10 = correct_10 // len(query_results) * 10
+    recall_100 = correct_100 // len(query_results) * 100
+    return recall_1, recall_10, recall_100
+
+
 def test_read_all():
-    sift_query_path = os.getcwd() + "/sift_1m/sift/query.fvecs"
+    sift_query_path = os.getcwd() + "/sift_1m/sift/l2_groundtruth.ivecs"
     vectors = fvecs_read_all(sift_query_path)
     print(len(vectors))
     print(pl.DataFrame(vectors))
 
 
 def test_read():
-    sift_query_path = os.getcwd() + "/sift_1m/sift/query.fvecs"
+    sift_query_path = os.getcwd() + "/sift_1m/sift/l2_groundtruth.ivecs"
     for idx, query_vec in enumerate(fvecs_read(sift_query_path)):
         print(pl.DataFrame([query_vec]))
         if idx == 10:
             assert idx == 10
             break
+
+
+def test_read_groundtruth():
+    read_groundtruth_path = os.getcwd() + "/sift_1m/sift/l2_groundtruth.ivecs"
+    ground_truth_sets_1, ground_truth_sets_10, ground_truth_sets_100 = read_groundtruth(read_groundtruth_path)
+    print(len(ground_truth_sets_1))
+    print(len(ground_truth_sets_10))
+    print(len(ground_truth_sets_100))
+    print(len(ground_truth_sets_1[0]))
+    print(len(ground_truth_sets_10[0]))
+    print(len(ground_truth_sets_100[0]))
 
 
 @trace_unhandled_exceptions
@@ -138,7 +216,7 @@ class TestQueryBenchmark:
 
     def test_query(self):
         thread_num = 1
-        total_times = 10000
+        total_times = 1
 
         print(">>> Query Benchmark Start <<<")
         print(f"Thread Num: {thread_num}, Times: {total_times}")
@@ -152,16 +230,35 @@ class TestQueryBenchmark:
         conn = ThriftInfinityClient(REMOTE_HOST)
         table = RemoteTable(conn, "default", "knn_benchmark")
         querys = fvecs_read_all(sift_query_path)
+        query_results = [[] for _ in range(len(querys))]
         for idx, query_vec in enumerate(querys):
             query_builder = InfinityThriftQueryBuilder(table)
             query_builder.output(["_row_id_"])
             query_builder.knn('col1', query_vec, 'float', 'l2', 100)
             res = query_builder.to_df()
-            print(res)
+            res_list = res["ROW_ID"].to_list()
+            # print(len(res_list))
+
+            for i in range(len(res_list)):
+                query_results[idx].append(res_list[i][1])
+
+            # print(query_results)
+
             if idx == total_times:
                 assert idx == total_times
                 break
+
         end = time.time()
+
+        read_groundtruth_path = os.getcwd() + "/sift_1m/sift/l2_groundtruth.ivecs"
+        ground_truth_sets_1, ground_truth_sets_10, ground_truth_sets_100 = read_groundtruth(read_groundtruth_path)
+
+        recall_1, recall_10, recall_100 = calculate_recall_all(ground_truth_sets_1, ground_truth_sets_10,
+                                                               ground_truth_sets_100, query_results)
+        print("recall_1: ", recall_1)
+        print("recall_10: ", recall_10)
+        print("recall_100: ", recall_100)
+
         dur = end - start
         print(">>> Query Benchmark End <<<")
         qps = total_times / dur
