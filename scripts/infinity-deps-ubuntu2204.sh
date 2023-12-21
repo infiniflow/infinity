e06c81600e3750ff633ea311847df5dd2c5fd29c
yangzq50 <58433399+yangzq50@users.noreply.github.com>
2023-12-21 10:13:20 +0800

    Build environment script for ubuntu 22.04 and 23.10 (#337)
    * add scripts/infinity-deps-ubuntu2310.sh
    
    * add scripts/infinity-deps-ubuntu2204.sh

---
+++ scripts/infinity-deps-ubuntu2204.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+echo
+echo "running script $0"
+echo "current working directory : $PWD"
+
+echo
+echo 'step [1/9] : sudo apt update && sudo apt install -y git wget unzip software-properties-common'
+echo
+sudo apt update && sudo apt install -y git wget unzip software-properties-common
+
+echo
+echo 'step [2/9] : download cmake-3.28.1'
+echo 'command: wget https://cmake.org/files/v3.28/cmake-3.28.1-linux-x86_64.tar.gz'
+echo
+wget https://cmake.org/files/v3.28/cmake-3.28.1-linux-x86_64.tar.gz
+
+echo
+echo 'step [3/9] : install cmake-3.28.1 into /usr/local/bin and /usr/local/share'
+echo 'command: tar zxvf cmake-3.28.1-linux-x86_64.tar.gz'
+echo 'command: cp -rf cmake-3.28.1-linux-x86_64/bin/* /usr/local/bin && cp -rf cmake-3.28.1-linux-x86_64/share/* /usr/local/share && rm -rf cmake-3.28.1-linux-x86_64'
+echo
+tar zxvf cmake-3.28.1-linux-x86_64.tar.gz
+cp -rf cmake-3.28.1-linux-x86_64/bin/* /usr/local/bin && cp -rf cmake-3.28.1-linux-x86_64/share/* /usr/local/share && rm -rf cmake-3.28.1-linux-x86_64
+
+echo
+echo 'step [4/9] : download ninja-1.11.1'
+echo 'command: wget https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-linux.zip'
+echo
+wget https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-linux.zip
+
+echo
+echo 'step [5/9] : install ninja-1.11.1 into /usr/local/bin'
+echo 'command: unzip ninja-linux.zip && cp ninja /usr/local/bin && rm ninja ninja-linux.zip'
+echo
+unzip ninja-linux.zip && cp ninja /usr/local/bin && rm ninja ninja-linux.zip
+
+echo
+echo 'step [6/9] : add apt source for llvm-17'
+echo 'command: echo -e "deb https://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main" > /etc/apt/sources.list.d/llvm17.list'
+echo 'command: wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc'
+echo
+echo -e "deb https://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main" > /etc/apt/sources.list.d/llvm17.list
+wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+
+echo
+echo 'step [7/9] : add apt source for g++-13 gcc-13 libstdc++-13-dev libboost1.81-dev'
+echo 'command: sudo add-apt-repository -y -P ppa:ubuntu-toolchain-r/test'
+echo 'command: sudo add-apt-repository -y -P ppa:mhier/libboost-latest'
+echo
+sudo add-apt-repository -y -P ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y -P ppa:mhier/libboost-latest
+
+echo
+echo "step [8/9] : sudo apt update && sudo apt install -y g++-13 clang-17 clang-tools-17 flex libboost1.81-dev liblz4-dev libevent-dev liburing-dev libthrift-dev"
+echo
+sudo apt update && sudo apt install -y g++-13 clang-17 clang-tools-17 flex libboost1.81-dev liblz4-dev libevent-dev liburing-dev libthrift-dev
+
+echo
+echo 'step [9/9] : create a symbolic link for clang-scan-deps-17'
+echo 'command: ln -s /usr/lib/llvm-17/bin/clang-scan-deps /usr/bin/clang-scan-deps'
+echo
+ln -s /usr/lib/llvm-17/bin/clang-scan-deps /usr/bin/clang-scan-deps
+
+echo
+echo "script $0 finished successfully!"
+echo
