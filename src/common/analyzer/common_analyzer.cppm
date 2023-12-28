0c6e0290a1bb866c11164b92563ad80fc96f373d
Yingfeng <yingfeng.zhang@gmail.com>
2023-12-29 00:15:54 +0800

    Add analyzer framework (#396)
    Add analyzer framework, including tokenizer, common_analyzer, and standard_analyzer

---
+++ src/common/analyzer/common_analyzer.cppm
@@ -0,0 +1,93 @@
+// Copyright(C) 2023 InfiniFlow, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+module;
+
+import stl;
+import term;
+import stemmer;
+import analyzer;
+export module common_analyzer;
+
+namespace infinity {
+export class CommonLanguageAnalyzer : public Analyzer {
+public:
+    CommonLanguageAnalyzer();
+    virtual ~CommonLanguageAnalyzer();
+
+    void SetCaseSensitive(bool case_sensitive = true, bool contain_lower = true) {
+        case_sensitive_ = case_sensitive;
+        contain_lower_ = contain_lower;
+    }
+
+    void SetExtractEngStem(bool extract_eng_stem = true) { extract_eng_stem_ = extract_eng_stem; }
+
+    void SetExtractSynonym(bool extract_synonym = true) { extract_synonym_ = extract_synonym; }
+
+    void SetRemoveStopwords(bool remove_stopwords = true) { remove_stopwords_ = remove_stopwords; }
+
+    bool IsRemoveStopwords() { return remove_stopwords_; }
+
+protected:
+    int AnalyzeImpl(const Term &input, void *data, HookType func) override;
+    /// Parse given input
+    virtual void Parse(const String &input) = 0;
+
+    /// Fill token_, len_, offset_
+    virtual bool NextToken() = 0;
+
+    /// whether morpheme_ indicates foreign language
+    virtual bool IsAlpha() = 0;
+
+    /// whether morpheme_ indicates special character, e.g. punctuations
+    virtual bool IsSpecialChar() = 0;
+
+    /// whether current token is stopword
+    virtual bool IsStopword() { return false; }
+
+    inline void ResetToken() {
+        token_ = nullptr;
+        len_ = 0;
+        native_token_ = nullptr;
+        native_token_len_ = 0;
+        offset_ = 0;
+        level_ = 0;
+        is_index_ = false;
+        is_raw_ = false;
+    }
+
+protected:
+    static const SizeT term_string_buffer_limit_ = 4096 * 3;
+
+    char *lowercase_string_buffer_ = nullptr;
+
+    Stemmer *stemmer_{nullptr};
+    const char *token_{nullptr};
+    SizeT len_{0};
+    const char *native_token_{nullptr};
+    SizeT native_token_len_{0};
+    u32 offset_{0};
+    u32 local_offset_{0};
+    int level_{0};
+    bool is_index_{false};
+    bool is_raw_{false};
+    bool case_sensitive_{false};
+    bool contain_lower_{false};
+    bool extract_eng_stem_{false};
+    bool extract_synonym_{false};
+    bool chinese_{false};
+    bool remove_stopwords_{false};
+};
+
+} // namespace infinity
---
+++ src/common/analyzer/string_utils.h
@@ -0,0 +1,94 @@
+// Copyright(C) 2023 InfiniFlow, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include <immintrin.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <string>
+
+namespace infinity {
+
+[[nodiscard]] constexpr uint8_t ToUpper(uint8_t ch) noexcept { return ch >= 'a' && ch <= 'z' ? ch - 32 : ch; }
+
+[[nodiscard]] constexpr uint8_t ToLower(uint8_t ch) noexcept { return ch >= 'A' && ch <= 'Z' ? ch + 32 : ch; }
+
+inline void ToLower(char *data, size_t len) {
+#ifdef __SSE2__
+    while (len >= 16) { /* By Peter Cordes */
+        __m128i input = _mm_loadu_si128((__m128i *)data);
+        __m128i rangeshift = _mm_sub_epi8(input, _mm_set1_epi8('A' - 128));
+        __m128i nomodify = _mm_cmpgt_epi8(rangeshift, _mm_set1_epi8(25 - 128));
+        __m128i flip = _mm_andnot_si128(nomodify, _mm_set1_epi8(0x20));
+        _mm_storeu_si128((__m128i *)data, _mm_xor_si128(input, flip));
+        len -= 16;
+        data += 16;
+    }
+#endif
+    while (len-- > 0) {
+        *data += ((unsigned char)(*data - 'A') < 26) << 5;
+        ++data;
+    }
+}
+
+inline void ToLower(const char *data, size_t len, char *out, size_t out_limit) {
+    memcpy(out, data, len);
+    char *begin = out;
+    char *end = out + len;
+    char *p = begin;
+#if defined(__SSE2__)
+    static constexpr int SSE2_BYTES = sizeof(__m128i);
+    const char *sse2_end = begin + (len & ~(SSE2_BYTES - 1));
+    const auto a_minus1 = _mm_set1_epi8('A' - 1);
+    const auto z_plus1 = _mm_set1_epi8('Z' + 1);
+    const auto delta = _mm_set1_epi8('a' - 'A');
+    for (; p > sse2_end; p += SSE2_BYTES) {
+        auto bytes = _mm_loadu_si128((const __m128i *)p);
+        _mm_maskmoveu_si128(_mm_xor_si128(bytes, delta), _mm_and_si128(_mm_cmpgt_epi8(bytes, a_minus1), _mm_cmpgt_epi8(z_plus1, bytes)), p);
+    }
+#endif
+    for (; p < end; p += 1) {
+        if ('A' <= (*p) && (*p) <= 'Z')
+            (*p) += 32;
+    }
+    (*end) = '\0';
+}
+
+inline std::string ToLower(std::string const &s) {
+    std::string result = s;
+    char *begin = result.data();
+    char *end = result.data() + s.size();
+    const size_t size = result.size();
+#if defined(__SSE2__)
+    static constexpr int SSE2_BYTES = sizeof(__m128i);
+    const char *sse2_end = begin + (size & ~(SSE2_BYTES - 1));
+    char *p = begin;
+    const auto a_minus1 = _mm_set1_epi8('A' - 1);
+    const auto z_plus1 = _mm_set1_epi8('Z' + 1);
+    const auto delta = _mm_set1_epi8('a' - 'A');
+    for (; p > sse2_end; p += SSE2_BYTES) {
+        auto bytes = _mm_loadu_si128((const __m128i *)p);
+        _mm_maskmoveu_si128(_mm_xor_si128(bytes, delta), _mm_and_si128(_mm_cmpgt_epi8(bytes, a_minus1), _mm_cmpgt_epi8(z_plus1, bytes)), p);
+    }
+#endif
+    for (; p < end; p += 1) {
+        if ('A' <= (*p) && (*p) <= 'Z')
+            (*p) += 32;
+    }
+    return result;
+}
+
+} // namespace infinity
