2c155cb39b9140a82748c76fed0deda480544683
Xwg <loloxwg@gmail.com>
2024-01-16 20:10:13 +0800

    Add KNN class and test in python module
    The commit introduces a new class TestKnn in the test_knn.py file, which includes the implementation of test cases for KNN functionality. A data file for testing (tmp_20240116.csv) is also included. This commit enhances testing coverage and helps validate the accuracy of the KNN functionality.

---
+++ python/test/test_knn.py
@@ -0,0 +1,82 @@
+# Copyright(C) 2023 InfiniFlow, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import infinity
+from infinity.common import REMOTE_HOST
+
+
+class TestKnn:
+
+    def test_version(self):
+        print(infinity.__version__)
+
+    def test_knn(self):
+        infinity_obj = infinity.connect(REMOTE_HOST)
+        # assert infinity_obj
+        #
+        # infinity
+        #
+        db_obj = infinity_obj.get_database("default")
+
+        db_obj.drop_table("fix_tmp_20240116", if_exists=True)
+        table_obj = db_obj.create_table("fix_tmp_20240116", {
+            "variant_id": "varchar",
+            "gender_vector": "vector,4,float",
+            "color_vector": "vector,4,float",
+            "category_vector": "vector,4,float",
+            "tag_vector": "vector,4,float",
+            "other_vector": "vector,4,float",
+            "query_is_recommend": "varchar",
+            "query_gender": "varchar",
+            "query_color": "varchar",
+            "query_price": "float"
+        }, None)
+
+        # TODO FIXME
+        # res = table_obj.insert([{"variant_id": "123",
+        #                          "gender_vector": [1.0] * 4,
+        #                          "color_vector": [2.0] * 4,
+        #                          "category_vector": [3.0] * 4,
+        #                          "tag_vector": [4.0] * 4,
+        #                          "other_vector": [5.0] * 4,
+        #                          "query_is_recommend": "ok",
+        #                          "query_color": "red",
+        #                          "query_price": 1.0
+        #                          }])
+        # assert res.success
+
+        test_csv_dir = "/tmp/infinity/test_data/tmp_20240116.csv"
+        print("import:", test_csv_dir, " start!")
+        table_obj.import_data(test_csv_dir, None)
+
+        # true
+        # search
+        # table_obj = db_obj.get_table("fix_tmp_20240116")
+        # res = table_obj.output(["variant_id"]).to_df()
+
+        # true
+        # res = table_obj.output(["variant_id", "query_price"]).knn('gender_vector', [1.0] * 768, "float", "ip",
+        #                                                           10).to_pl()
+        # print(res)
+
+        # true
+        res = table_obj.output(["variant_id", "_row_id"]).knn(
+            "gender_vector", [1.0] * 4, "float", "ip", 10).to_pl()
+        print(res)
+
+        # FIXME
+        # res = table_obj.output(["variant_id", "query_is_recommend", "query_gender", "query_color", "query_price"]).knn(
+        #     "gender_vector", [1.0] * 4, "float", "ip", 3).to_pl()
+
+        # print(res)
