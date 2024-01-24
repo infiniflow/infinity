cd7f90e4a0da2cb1f1f62ba2c1507de9308cc029
Yingfeng <yingfeng.zhang@gmail.com>
2024-01-24 12:06:40 +0800

    Add lock free vector with multiple producer for real time indexing (#470)
    Rename previous LockFreeVector to SPLockFreeVector which means lfvector for single producer(lock is required if multiple writer is used)
    
    Add MPLockFreeVector which allows for multiple producers
    
    SPLockFreeVector has higher performance which will be used for near real time indexing.

--- src/unit_test/storage/invertedindex/common/lock_free_vector.cpp
+++ src/unit_test/storage/invertedindex/common/lock_free_vector.cpp
@@ -26,56 +26,56 @@ class LockFreeVectorTest : public BaseTest {};
 TEST_F(LockFreeVectorTest, test1) {
     using namespace infinity;
     MemoryPool memory_pool;
-    LockFreeVector<u32> vec(&memory_pool);
+    SPLockFreeVector<u32> vec(&memory_pool);
     vec.PushBack(1);
     vec.PushBack(2);
     vec.PushBack(3);
     vec.PushBack(4);
     vec.PushBack(5);
-    EXPECT_EQ(vec.Size(), 5);
+    EXPECT_EQ(vec.Size(), (unsigned)5);
     for (u32 i = 0; i < vec.Size(); ++i) {
         EXPECT_EQ(vec[i], i + 1);
     }
 }
 
-void PushTest(infinity::LockFreeVector<infinity::u32> *vec, int number_of_pushes, bool lock) {
-    for (int i = 0; i < number_of_pushes; i++) {
+void PushTest(infinity::SPLockFreeVector<infinity::u32> *vec, unsigned number_of_pushes, bool lock) {
+    for (unsigned i = 0; i < number_of_pushes; i++) {
         vec->PushBack(i, lock);
     }
 }
 
-void ReadTest(infinity::LockFreeVector<infinity::u32> *vec) {
-    for (int i = 0; i < vec->Size(); i++) {
-        auto v = vec->At(i);
+void ReadTest(infinity::SPLockFreeVector<infinity::u32> *vec) {
+    for (unsigned i = 0; i < vec->Size(); i++) {
+        vec->At(i);
     }
 }
 
 TEST_F(LockFreeVectorTest, test2) {
     using namespace infinity;
     MemoryPool memory_pool;
-    LockFreeVector<u32> vec(&memory_pool);
-    int number_of_threads = 4;
-    int number_of_pushes = 100;
+    SPLockFreeVector<u32> vec(&memory_pool);
+    unsigned number_of_threads = 4;
+    unsigned number_of_pushes = 100;
 
     Thread push_thread_list[number_of_threads];
-    for (int i = 0; i < number_of_threads; i++) {
+    for (unsigned i = 0; i < number_of_threads; i++) {
         push_thread_list[i] = Thread(PushTest, &vec, number_of_pushes, true);
     }
-    for (int i = 0; i < number_of_threads; i++) {
+    for (unsigned i = 0; i < number_of_threads; i++) {
         push_thread_list[i].join();
     }
     EXPECT_EQ(vec.Size(), (number_of_pushes * number_of_threads));
 
     Vector<u32> stl_vec;
-    for (int i = 0; i < vec.Size(); i++) {
+    for (unsigned i = 0; i < vec.Size(); i++) {
         stl_vec.push_back(vec[i]);
     }
 
     std::sort(stl_vec.begin(), stl_vec.end());
 
     // Test that there are number_of_threads copies of each value in the vector
-    for (int i = 0; i < stl_vec.size() - number_of_threads; i += number_of_threads) {
-        for (int j = i + 1; j < i + number_of_threads; j++) {
+    for (unsigned i = 0; i < stl_vec.size() - number_of_threads; i += number_of_threads) {
+        for (unsigned j = i + 1; j < i + number_of_threads; j++) {
             EXPECT_EQ(stl_vec[j], stl_vec[i]);
         }
     }
@@ -84,17 +84,53 @@ TEST_F(LockFreeVectorTest, test2) {
 TEST_F(LockFreeVectorTest, test3) {
     using namespace infinity;
     MemoryPool memory_pool;
-    LockFreeVector<u32> vec(&memory_pool);
-    int number_of_threads = 16;
-    int number_of_pushes = 100;
+    SPLockFreeVector<u32> vec(&memory_pool);
+    unsigned number_of_threads = 16;
+    unsigned number_of_pushes = 100;
 
     Thread push_thread = Thread(PushTest, &vec, number_of_pushes, false);
     Thread read_thread_list[number_of_threads];
-    for (int i = 0; i < number_of_threads; i++) {
+    for (unsigned i = 0; i < number_of_threads; i++) {
         read_thread_list[i] = Thread(ReadTest, &vec);
     }
     push_thread.join();
-    for (int i = 0; i < number_of_threads; i++) {
+    for (unsigned i = 0; i < number_of_threads; i++) {
         read_thread_list[i].join();
+    }
+}
+
+void PushTest2(infinity::MPLockFreeVector<infinity::u32> *vec, unsigned number_of_pushes) {
+    for (unsigned i = 0; i < number_of_pushes; i++) {
+        vec->PushBack(i);
+    }
+}
+TEST_F(LockFreeVectorTest, test4) {
+    using namespace infinity;
+    MemoryPool memory_pool;
+    MPLockFreeVector<u32> vec(&memory_pool);
+    unsigned number_of_threads = 4;
+    unsigned number_of_pushes = 100;
+
+    Thread push_thread_list[number_of_threads];
+    for (unsigned i = 0; i < number_of_threads; i++) {
+        push_thread_list[i] = Thread(PushTest2, &vec, number_of_pushes);
+    }
+    for (unsigned i = 0; i < number_of_threads; i++) {
+        push_thread_list[i].join();
+    }
+    EXPECT_EQ(vec.Size(), (number_of_pushes * number_of_threads));
+
+    Vector<u32> stl_vec;
+    for (unsigned i = 0; i < vec.Size(); i++) {
+        stl_vec.push_back(vec[i]);
+    }
+
+    std::sort(stl_vec.begin(), stl_vec.end());
+
+    // Test that there are number_of_threads copies of each value in the vector
+    for (unsigned i = 0; i < stl_vec.size() - number_of_threads; i += number_of_threads) {
+        for (unsigned j = i + 1; j < i + number_of_threads; j++) {
+            EXPECT_EQ(stl_vec[j], stl_vec[i]);
+        }
     }
 }
