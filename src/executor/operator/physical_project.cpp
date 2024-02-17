1ca85c2ec2007ad6f7d15edc70ee544de0202d4e
Jin Hai <haijin.chn@gmail.com>
2024-02-17 22:54:47 +0800

    Support select expression without from clause (#579)
    ### What problem does this PR solve?
    
    Select EXPR without from clause will introduce crashing.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    
    ---------
    
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- src/executor/operator/physical_project.cpp
+++ src/executor/operator/physical_project.cpp
@@ -47,19 +47,14 @@ void PhysicalProject::Init() {
 }
 
 bool PhysicalProject::Execute(QueryContext *, OperatorState *operator_state) {
-    OperatorState* prev_op_state = operator_state->prev_op_state_;
     auto *project_operator_state = static_cast<ProjectionOperatorState *>(operator_state);
-
-    SizeT input_block_count = prev_op_state->data_block_array_.size();
-    for(SizeT block_idx = 0; block_idx < input_block_count; ++ block_idx) {
-        DataBlock* input_data_block = prev_op_state->data_block_array_[block_idx].get();
-
+    if(project_operator_state->empty_source_) {
         project_operator_state->data_block_array_.emplace_back(DataBlock::MakeUniquePtr());
         DataBlock* output_data_block = project_operator_state->data_block_array_.back().get();
         output_data_block->Init(*GetOutputTypes());
 
         ExpressionEvaluator evaluator;
-        evaluator.Init(input_data_block);
+        evaluator.Init(nullptr);
 
         SizeT expression_count = expressions_.size();
 
@@ -78,19 +73,53 @@ bool PhysicalProject::Execute(QueryContext *, OperatorState *operator_state) {
             evaluator.Execute(expressions_[expr_idx], expr_states[expr_idx], output_data_block->column_vectors[expr_idx]);
         }
         output_data_block->Finalize();
-    }
-
-
-//    if (prev_op_state->Complete() && !prev_op_state->data_block_->Finalized()) {
-//        project_operator_state->data_block_->Finalize();
-//        project_operator_state->SetComplete();
-//        return ;
-//    }
-
-    prev_op_state->data_block_array_.clear();
-    if (prev_op_state->Complete()) {
         project_operator_state->SetComplete();
+    } else {
+        OperatorState* prev_op_state = operator_state->prev_op_state_;
+
+        SizeT input_block_count = prev_op_state->data_block_array_.size();
+        for(SizeT block_idx = 0; block_idx < input_block_count; ++ block_idx) {
+            DataBlock* input_data_block = prev_op_state->data_block_array_[block_idx].get();
+
+            project_operator_state->data_block_array_.emplace_back(DataBlock::MakeUniquePtr());
+            DataBlock* output_data_block = project_operator_state->data_block_array_.back().get();
+            output_data_block->Init(*GetOutputTypes());
+
+            ExpressionEvaluator evaluator;
+            evaluator.Init(input_data_block);
+
+            SizeT expression_count = expressions_.size();
+
+            // Prepare the expression states
+            Vector<SharedPtr<ExpressionState>> expr_states;
+            expr_states.reserve(expression_count);
+
+            for (const auto &expr : expressions_) {
+                // expression state
+                expr_states.emplace_back(ExpressionState::CreateState(expr));
+            }
+
+            for (SizeT expr_idx = 0; expr_idx < expression_count; ++expr_idx) {
+                //        Vector<SharedPtr<ColumnVector>> blocks_column;
+                //        blocks_column.emplace_back(output_data_block->column_vectors[expr_idx]);
+                evaluator.Execute(expressions_[expr_idx], expr_states[expr_idx], output_data_block->column_vectors[expr_idx]);
+            }
+            output_data_block->Finalize();
+        }
+
+
+        //    if (prev_op_state->Complete() && !prev_op_state->data_block_->Finalized()) {
+        //        project_operator_state->data_block_->Finalize();
+        //        project_operator_state->SetComplete();
+        //        return ;
+        //    }
+
+        prev_op_state->data_block_array_.clear();
+        if (prev_op_state->Complete()) {
+            project_operator_state->SetComplete();
+        }
     }
+
     return true;
 }
 
