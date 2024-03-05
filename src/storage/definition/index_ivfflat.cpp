81da91edd0997696f855d1dfadd7e8b389586623
Jin Hai <haijin.chn@gmail.com>
2024-03-05 16:47:43 +0800

    Create full text index on non-varchar column, no error raised (#704)
    ### What problem does this PR solve?
    
    Create full text index on non-varchar column will trigger error
    response.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    
    ---------
    
    Signed-off-by: jinhai <haijin.chn@gmail.com>

--- src/storage/definition/index_ivfflat.cpp
+++ src/storage/definition/index_ivfflat.cpp
@@ -103,7 +103,7 @@ void IndexIVFFlat::ValidateColumnDataType(const SharedPtr<BaseTableRef> &base_ta
         RecoverableError(Status::ColumnNotExist(column_name));
     } else if (auto &data_type = column_types_vector[column_id]; data_type->type() != LogicalType::kEmbedding) {
         RecoverableError(Status::InvalidIndexDefinition(
-            fmt::format("Invalid parameter for IVFFlat index: column name: {}, data type not supported: {}.", column_name, data_type->ToString())));
+            fmt::format("Attempt to create IVFFLAT index on column: {}, data type: {}.", column_name, data_type->ToString())));
     }
 }
 
