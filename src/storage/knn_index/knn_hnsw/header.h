bc5541514376b16dccd12ae457674430d6cbb240
ChebyshevTST <wenjianqiu5@gmail.com>
2024-03-06 11:10:26 -0500

    fixed header.h

--- src/storage/knn_index/knn_hnsw/header.h
+++ src/storage/knn_index/knn_hnsw/header.h
@@ -25,14 +25,14 @@ static __int64 xgetbv(unsigned int x) {
 #include <x86intrin.h>
 #include <cpuid.h>
 #include <stdint.h>
-static void cpuid(int32_t cpuInfo[4], int32_t eax, int32_t ecx) {
-    __cpuid_count(eax, ecx, cpuInfo[0], cpuInfo[1], cpuInfo[2], cpuInfo[3]);
-}
-static uint64_t xgetbv(unsigned int index) {
-    uint32_t eax, edx;
-    __asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
-    return ((uint64_t)edx << 32) | eax;
-}
+// static void cpuid(int32_t cpuInfo[4], int32_t eax, int32_t ecx) {
+    // __cpuid_count(eax, ecx, cpuInfo[0], cpuInfo[1], cpuInfo[2], cpuInfo[3]);
+// }
+// static uint64_t xgetbv(unsigned int index) {
+    // uint32_t eax, edx;
+    // __asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
+    // return ((uint64_t)edx << 32) | eax;
+// }
 #endif
 
 #if defined(USE_AVX512)
@@ -50,59 +50,59 @@ static uint64_t xgetbv(unsigned int index) {
 // Adapted from https://github.com/Mysticial/FeatureDetector
 #define _XCR_XFEATURE_ENABLED_MASK  0
 
-static bool AVXCapable() {
-    int cpuInfo[4];
+// static bool AVXCapable() {
+//     int cpuInfo[4];
 
-    // CPU support
-    cpuid(cpuInfo, 0, 0);
-    int nIds = cpuInfo[0];
+//     // CPU support
+//     cpuid(cpuInfo, 0, 0);
+//     int nIds = cpuInfo[0];
 
-    bool HW_AVX = false;
-    if (nIds >= 0x00000001) {
-        cpuid(cpuInfo, 0x00000001, 0);
-        HW_AVX = (cpuInfo[2] & ((int)1 << 28)) != 0;
-    }
+//     bool HW_AVX = false;
+//     if (nIds >= 0x00000001) {
+//         cpuid(cpuInfo, 0x00000001, 0);
+//         HW_AVX = (cpuInfo[2] & ((int)1 << 28)) != 0;
+//     }
 
-    // OS support
-    cpuid(cpuInfo, 1, 0);
+//     // OS support
+//     cpuid(cpuInfo, 1, 0);
 
-    bool osUsesXSAVE_XRSTORE = (cpuInfo[2] & (1 << 27)) != 0;
-    bool cpuAVXSuport = (cpuInfo[2] & (1 << 28)) != 0;
+//     bool osUsesXSAVE_XRSTORE = (cpuInfo[2] & (1 << 27)) != 0;
+//     bool cpuAVXSuport = (cpuInfo[2] & (1 << 28)) != 0;
 
-    bool avxSupported = false;
-    if (osUsesXSAVE_XRSTORE && cpuAVXSuport) {
-        uint64_t xcrFeatureMask = xgetbv(_XCR_XFEATURE_ENABLED_MASK);
-        avxSupported = (xcrFeatureMask & 0x6) == 0x6;
-    }
-    return HW_AVX && avxSupported;
-}
+//     bool avxSupported = false;
+//     if (osUsesXSAVE_XRSTORE && cpuAVXSuport) {
+//         uint64_t xcrFeatureMask = xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+//         avxSupported = (xcrFeatureMask & 0x6) == 0x6;
+//     }
+//     return HW_AVX && avxSupported;
+// }
 
-static bool AVX512Capable() {
-    if (!AVXCapable()) return false;
+// static bool AVX512Capable() {
+//     if (!AVXCapable()) return false;
 
-    int cpuInfo[4];
+//     int cpuInfo[4];
 
-    // CPU support
-    cpuid(cpuInfo, 0, 0);
-    int nIds = cpuInfo[0];
+//     // CPU support
+//     cpuid(cpuInfo, 0, 0);
+//     int nIds = cpuInfo[0];
 
-    bool HW_AVX512F = false;
-    if (nIds >= 0x00000007) {  //  AVX512 Foundation
-        cpuid(cpuInfo, 0x00000007, 0);
-        HW_AVX512F = (cpuInfo[1] & ((int)1 << 16)) != 0;
-    }
+//     bool HW_AVX512F = false;
+//     if (nIds >= 0x00000007) {  //  AVX512 Foundation
+//         cpuid(cpuInfo, 0x00000007, 0);
+//         HW_AVX512F = (cpuInfo[1] & ((int)1 << 16)) != 0;
+//     }
 
-    // OS support
-    cpuid(cpuInfo, 1, 0);
+//     // OS support
+//     cpuid(cpuInfo, 1, 0);
 
-    bool osUsesXSAVE_XRSTORE = (cpuInfo[2] & (1 << 27)) != 0;
-    bool cpuAVXSuport = (cpuInfo[2] & (1 << 28)) != 0;
+//     bool osUsesXSAVE_XRSTORE = (cpuInfo[2] & (1 << 27)) != 0;
+//     bool cpuAVXSuport = (cpuInfo[2] & (1 << 28)) != 0;
 
-    bool avx512Supported = false;
-    if (osUsesXSAVE_XRSTORE && cpuAVXSuport) {
-        uint64_t xcrFeatureMask = xgetbv(_XCR_XFEATURE_ENABLED_MASK);
-        avx512Supported = (xcrFeatureMask & 0xe6) == 0xe6;
-    }
-    return HW_AVX512F && avx512Supported;
-}
+//     bool avx512Supported = false;
+//     if (osUsesXSAVE_XRSTORE && cpuAVXSuport) {
+//         uint64_t xcrFeatureMask = xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+//         avx512Supported = (xcrFeatureMask & 0xe6) == 0xe6;
+//     }
+//     return HW_AVX512F && avx512Supported;
+// }
 #endif
