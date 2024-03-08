31b680bcda7ba7e0d983c1703d69e08ead65f40a
Thomas134 <1265552377@qq.com>
2024-03-08 09:53:49 -0500

    avx2 benchmark change

--- src/storage/knn_index/knn_hnsw/header.h
+++ src/storage/knn_index/knn_hnsw/header.h
@@ -15,12 +15,12 @@
 #ifdef _MSC_VER
 #include <intrin.h>
 #include <stdexcept>
-void cpuid(int32_t out[4], int32_t eax, int32_t ecx) {
-    __cpuidex(out, eax, ecx);
-}
-static __int64 xgetbv(unsigned int x) {
-    return _xgetbv(x);
-}
+// void cpuid(int32_t out[4], int32_t eax, int32_t ecx) {
+//     __cpuidex(out, eax, ecx);
+// }
+// static __int64 xgetbv(unsigned int x) {
+//     return _xgetbv(x);
+// }
 #else
 #include <x86intrin.h>
 #include <cpuid.h>
