f82632b15de082acf573b62dd86619efeb7f6c44
Jin Hai <haijin.chn@gmail.com>
2024-03-28 11:34:52 +0800

    Open pytest log
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- tools/run_pysdk_test.py
+++ tools/run_pysdk_test.py
@@ -12,8 +12,8 @@ def python_sdk_test(python_test_dir: str, pytest_mark: str):
     # run test
     print(f"start pysdk test with {pytest_mark}")
     process = subprocess.Popen(
-        # ["python", "-m", "pytest", "--tb=line", '-s', '-x', '-m', pytest_mark, f'{python_test_dir}/test'],
-        ["python", "-m", "pytest", "--tb=line", '-x', '-m', pytest_mark, f'{python_test_dir}/test'],
+        ["python", "-m", "pytest", "--tb=line", '-s', '-x', '-m', pytest_mark, f'{python_test_dir}/test'],
+        # ["python", "-m", "pytest", "--tb=line", '-x', '-m', pytest_mark, f'{python_test_dir}/test'],
         stdout=sys.stdout,
         stderr=sys.stderr,
         universal_newlines=True,
