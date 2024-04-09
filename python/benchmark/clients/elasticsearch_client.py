e4f7ba8188b1f104066ca7ed55f0c9213cd88fa4
salieri <lomlieri@gmail.com>
2024-04-09 19:51:16 +0800

    add run.py and init clients for benmark

--- python/benchmark/clients/elasticsearch_client.py
+++ python/benchmark/clients/elasticsearch_client.py
@@ -2,32 +2,16 @@ import argparse
 
 from base_client import BaseClient
 
-from qdrant_client import QdrantClient as QC
-from qdrant_client import models
-from qdrant_client.models import VectorParams, Distance
 
-import json
-
-from base_client import BaseClient
-
-class QdrantClient(BaseClient):
+class ElasticsearchClient(BaseClient):
     def __init__(self,
                  mode: str,
                  options: argparse.Namespace,
                  drop_old: bool = True) -> None:
-        data = json.load(mode)
-        self.client = QC(data['connection_url'])
-        self.collection_name = data['name']
-        if data['distance'] == 'cosine':
-            self.distance = Distance.COSINE
-        elif data['distance'] == 'L2':
-            self.distance = Distance.EUCLID
-        hnsw_config = json.loads(data['vector_index'])
-        self.client.recreate_collection(collection_name=self.collection_name,
-                                        vectors_config=VectorParams(
-                                            size=data['vector_size'],
-                                            distance=self.distance,
-                                            hnsw_config=models.HnswConfigDiff(m=data['vector_index'])))
+        """
+        The mode configuration file is parsed to extract the needed parameters, which are then all stored for use by other functions.
+        """
+        pass
 
     def upload(self) -> dict:
         """
