9fccf2bd6c0937362839f4aa4a1f47f0046bd2af
Jin Hai <haijin.chn@gmail.com>
2024-04-18 21:59:32 +0800

    Update thrift to 0.20.0
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- third_party/thrift/build_cmake/cmake/StaticCodeAnalysis.cmake
+++
@@ -1,9 +0,0 @@
-find_package(ClangTools QUIET)
-if(clang-tidy_FOUND AND run-clang-tidy_FOUND AND NOT TARGET do_run_clang_tidy)
-    add_custom_target(
-      do_run_clang_tidy
-      COMMAND ClangTools::run-clang-tidy -clang-tidy-binary "$<TARGET_FILE:ClangTools::clang-tidy>" -p ${CMAKE_BINARY_DIR} "-quiet" > ./run-clang-tidy.txt
-      DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-      )
-endif()
