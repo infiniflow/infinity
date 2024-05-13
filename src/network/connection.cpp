5c2ac70736da1ee73fe032339f537537c0c1a7fc
yzq <58433399+yangzq50@users.noreply.github.com>
2024-05-13 18:42:30 +0800

    pg protocol oid

--- src/network/connection.cpp
+++ src/network/connection.cpp
@@ -299,6 +299,53 @@ void Connection::SendTableDescription(const SharedPtr<DataTable> &result_table) 
                 }
                 break;
             }
+            case LogicalType::kTensor: {
+                if (column_type->type_info()->type() != TypeInfoType::kEmbedding) {
+                    UnrecoverableError("Not tensor type");
+                }
+                EmbeddingInfo *embedding_info = static_cast<EmbeddingInfo *>(column_type->type_info().get());
+                switch (embedding_info->Type()) {
+                    case kElemBit: {
+                        object_id = 1025;
+                        object_width = 1;
+                        break;
+                    }
+                    case kElemInt8: {
+                        object_id = 1026;
+                        object_width = 1;
+                        break;
+                    }
+                    case kElemInt16: {
+                        object_id = 1027;
+                        object_width = 2;
+                        break;
+                    }
+                    case kElemInt32: {
+                        object_id = 1028;
+                        object_width = 4;
+                        break;
+                    }
+                    case kElemInt64: {
+                        object_id = 1029;
+                        object_width = 8;
+                        break;
+                    }
+                    case kElemFloat: {
+                        object_id = 1030;
+                        object_width = 4;
+                        break;
+                    }
+                    case kElemDouble: {
+                        object_id = 1031;
+                        object_width = 8;
+                        break;
+                    }
+                    case kElemInvalid: {
+                        UnrecoverableError("Invalid embedding data type");
+                    }
+                }
+                break;
+            }
             default: {
                 UnrecoverableError("Unexpected type");
             }
