e773a3b2a79773caa44209f628c01db7cdfda802
Jin Hai <haijin.chn@gmail.com>
2024-06-05 11:27:37 +0800

    Fix compile error and add one slt
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- src/parser/type/complex/embedding_type.h
+++ src/parser/type/complex/embedding_type.h
@@ -162,62 +162,9 @@ public:
         return std::string();
     }
 
-    static inline void Embedding2Json(const EmbeddingType &embedding, EmbeddingDataType type, size_t dimension, nlohmann::json& embedding_json) {
-        switch (type) {
-            case kElemBit: {
-                BitmapEmbedding2JsonInternal(embedding, dimension, embedding_json);
-                break;
-            }
-            case kElemInt8: {
-                Embedding2JsonInternal<int8_t>(embedding, dimension, embedding_json);
-                break;
-            }
-            case kElemInt16: {
-                Embedding2JsonInternal<int16_t>(embedding, dimension, embedding_json);
-                break;
-            }
-            case kElemInt32: {
-                Embedding2JsonInternal<int32_t>(embedding, dimension, embedding_json);
-                break;
-            }
-            case kElemInt64: {
-                Embedding2JsonInternal<int64_t>(embedding, dimension, embedding_json);
-                break;
-            }
-            case kElemFloat: {
-                Embedding2JsonInternal<float>(embedding, dimension, embedding_json);
-                break;
-            }
-            case kElemDouble: {
-                Embedding2JsonInternal<double>(embedding, dimension, embedding_json);
-                break;
-            }
-            default: {
-                ParserError("Unexpected embedding type");
-            }
-        }
-    }
+    static void Embedding2Json(const EmbeddingType &embedding, EmbeddingDataType type, size_t dimension, nlohmann::json& embedding_json);
 
 private:
-    template <typename T>
-    static inline void Embedding2JsonInternal(const EmbeddingType &embedding, size_t dimension, nlohmann::json& embedding_json) {
-        for (size_t i = 0; i < dimension; ++i) {
-            embedding_json.push_back(((T *)(embedding.ptr))[i]);
-        }
-    }
-
-    static inline void BitmapEmbedding2JsonInternal(const EmbeddingType &embedding, size_t dimension, nlohmann::json& embedding_json) {
-        ParserAssert(dimension % 8 == 0, "Binary embedding dimension should be the times of 8.");
-        auto *array = reinterpret_cast<const uint8_t *>(embedding.ptr);
-        for (size_t i = 0; i < dimension / 8; ++i) {
-            const uint8_t byte = array[i];
-            for (size_t j = 0; j < 8; ++j) {
-                int8_t elem = (byte & (1 << j)) ? 1 : 0;
-                embedding_json.push_back(elem);
-            }
-        }
-    }
-
     template <typename T>
     static inline std::string Embedding2StringInternal(const EmbeddingType &embedding, size_t dimension) {
         std::stringstream ss;
