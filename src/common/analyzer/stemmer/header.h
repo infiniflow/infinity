1b600b470de0b15ad5576a027140f99d9f31965e
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2024-06-12 19:58:43 +0800

    update stemmer

--- src/common/analyzer/stemmer/header.h
+++ src/common/analyzer/stemmer/header.h
@@ -1,16 +1,3 @@
-// Copyright(C) 2023 InfiniFlow, Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 #include <limits.h>
 
@@ -18,7 +5,9 @@
 
 #define MAXINT INT_MAX
 #define MININT INT_MIN
+
 #define HEAD 2 * sizeof(int)
+
 #define SIZE(p) ((int *)(p))[-1]
 #define SET_SIZE(p, n) ((int *)(p))[-1] = n
 #define CAPACITY(p) ((int *)(p))[-2]
