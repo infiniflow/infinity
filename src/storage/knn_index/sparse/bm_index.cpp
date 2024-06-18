9d1ed7c154317c346d14b8130e29b17f1de899bb
shenyushi <shenyushi99@qq.com>
2024-06-18 10:43:20 +0800

    Add: insert api for bmp index

--- src/storage/knn_index/sparse/bm_index.cpp
+++ src/storage/knn_index/sparse/bm_index.cpp
@@ -75,6 +75,96 @@ PostingList PostingList::ReadAdv(char *&p) {
     return res;
 }
 
+void BMIvt::AddBlock(i32 block_id, const Vector<Vector<Pair<i32, f32>>> &tail_terms) {
+    HashMap<i32, f32> max_scores;
+    for (const auto &terms : tail_terms) {
+        for (const auto &[term_id, score] : terms) {
+            max_scores[term_id] = std::max(max_scores[term_id], score);
+        }
+    }
+    for (const auto &[term_id, score] : max_scores) {
+        postings_[term_id].max_scores_.emplace_back(block_id, score);
+    }
+}
+
+void BMIvt::Optimize(Vector<Vector<f32>> ivt_scores) {
+    for (i32 term_id = 0; term_id < (i32)ivt_scores.size(); ++term_id) {
+        auto &posting = postings_[term_id];
+        auto &term_scores = ivt_scores[term_id];
+        std::sort(term_scores.begin(), term_scores.end(), std::greater<>());
+        for (i32 i = 0; i < 3; ++i) {
+            i32 k = std::pow(10, i + 1);
+            if (k < (i32)term_scores.size()) {
+                posting.kth_score_[i] = term_scores[k];
+            }
+        }
+    }
+}
+
+i8 TailFwd::AddDoc(const SparseVecRef<f32, i32> &doc) {
+    Vector<Pair<i32, f32>> doc_terms;
+    for (i32 i = 0; i < doc.nnz_; ++i) {
+        doc_terms.emplace_back(doc.indices_[i], doc.data_[i]);
+    }
+    tail_terms_.emplace_back(std::move(doc_terms));
+    return tail_terms_.size();
+}
+
+Vector<Pair<i32, Vector<Pair<i8, f32>>>> TailFwd::ToBlockFwd() const {
+    Vector<Tuple<i32, i8, f32>> term_pairs;
+    i8 block_size = tail_terms_.size();
+    for (i8 block_offset = 0; block_offset < block_size; ++block_offset) {
+        for (const auto &[term_id, score] : tail_terms_[block_offset]) {
+            term_pairs.emplace_back(term_id, block_offset, score);
+        }
+    }
+    std::sort(term_pairs.begin(), term_pairs.end(), [](const auto &a, const auto &b) { return std::get<0>(a) < std::get<0>(b); });
+
+    Vector<Pair<i32, Vector<Pair<i8, f32>>>> res;
+    i32 last_term_id = -1;
+    Vector<Pair<i8, f32>> inners;
+    for (const auto &[term_id, block_offset, score] : term_pairs) {
+        if (term_id != last_term_id) {
+            if (last_term_id != -1) {
+                Vector<Pair<i8, f32>> inners1;
+                std::swap(inners1, inners);
+                res.emplace_back(last_term_id, std::move(inners1));
+            }
+            last_term_id = term_id;
+        }
+        inners.emplace_back(block_offset, score);
+    }
+    if (!inners.empty()) {
+        res.emplace_back(last_term_id, std::move(inners));
+    }
+    return res;
+}
+
+Optional<TailFwd> BlockFwd::AddDoc(const SparseVecRef<f32, i32> &doc) {
+    i8 tail_size = tail_fwd_.AddDoc(doc);
+    if (tail_size < block_size_) {
+        return None;
+    }
+    TailFwd tail_fwd1;
+    std::swap(tail_fwd1, tail_fwd_);
+
+    Vector<Pair<i32, Vector<Pair<i8, f32>>>> block_terms = tail_fwd1.ToBlockFwd();
+    block_terms_.emplace_back(std::move(block_terms));
+    return tail_fwd1;
+}
+
+Vector<Vector<f32>> BlockFwd::GetIvtScores(i32 term_num) const {
+    Vector<Vector<f32>> res(term_num);
+    for (const auto &block_terms : block_terms_) {
+        for (const auto &[term_id, inners] : block_terms) {
+            for (const auto &[block_offset, block_score] : inners) {
+                res[term_id].emplace_back(block_score);
+            }
+        }
+    }
+    return res;
+}
+
 Vector<f32> BlockFwd::GetScores(i32 block_id, const SparseVecRef<f32, i32> &query) const {
     const auto &block_terms = block_terms_[block_id];
     Vector<f32> res(block_size_, 0.0);
@@ -232,6 +322,21 @@ BlockFwd BMIndexBuilder::Fwd2BlockFwd() && {
     return BlockFwd(block_size_, std::move(res));
 }
 
+void BMIndex::AddDoc(const SparseVecRef<f32, i32> &doc) {
+    Optional<TailFwd> tail_fwd = block_fwd_.AddDoc(doc);
+    if (!tail_fwd.has_value()) {
+        return;
+    }
+    i32 block_id = block_fwd_.block_num() - 1;
+    const auto &tail_terms = tail_fwd->GetTailTerms();
+    bm_ivt_.AddBlock(block_id, tail_terms);
+}
+
+void BMIndex::Optimize() {
+    Vector<Vector<f32>> ivt_scores = block_fwd_.GetIvtScores(term_num_);
+    bm_ivt_.Optimize(std::move(ivt_scores));
+}
+
 Pair<Vector<i32>, Vector<f32>> BMIndex::SearchKnn(const SparseVecRef<f32, i32> &query, i32 topk, f32 alpha, f32 beta) const {
     SparseVecEle<f32, i32> keeped_query;
     if (beta < 1.0) {
@@ -255,13 +360,14 @@ Pair<Vector<i32>, Vector<f32>> BMIndex::SearchKnn(const SparseVecRef<f32, i32> &
     const SparseVecRef<f32, i32> &query_ref =
         beta < 1.0 ? SparseVecRef<f32, i32>(keeped_query.nnz_, keeped_query.indices_.get(), keeped_query.data_.get()) : query;
 
+    const auto &postings = bm_ivt_.GetPostings();
     f32 threshold = 0.0;
     i32 block_num = block_fwd_.block_num();
     Vector<f32> upper_bounds(block_num, 0.0);
     for (i32 i = 0; i < query_ref.nnz_; ++i) {
         i32 query_term = query_ref.indices_[i];
         f32 query_score = query_ref.data_[i];
-        const auto &posting = postings_[query_term];
+        const auto &posting = postings[query_term];
         threshold = std::max(threshold, query_score * posting.kth(topk));
         for (const auto &[block_id, score] : posting.max_scores_) {
             upper_bounds[block_id] += score * query_score;
@@ -324,7 +430,8 @@ BMIndex BMIndex::Load(FileHandler &file_handler) {
 SizeT BMIndex::GetSizeInBytes() const {
     SizeT size = 0;
     size += sizeof(SizeT);
-    for (const auto &posting : postings_) {
+    const auto &postings = bm_ivt_.GetPostings();
+    for (const auto &posting : postings) {
         size += posting.GetSizeInBytes();
     }
     size += block_fwd_.GetSizeInBytes();
@@ -332,9 +439,10 @@ SizeT BMIndex::GetSizeInBytes() const {
 }
 
 void BMIndex::WriteAdv(char *&p) const {
-    SizeT posting_size = postings_.size();
+    const auto &postings = bm_ivt_.GetPostings();
+    SizeT posting_size = postings.size();
     WriteBufAdv<SizeT>(p, posting_size);
-    for (const auto &posting : postings_) {
+    for (const auto &posting : postings) {
         posting.WriteAdv(p);
     }
     block_fwd_.WriteAdv(p);
--- src/storage/knn_index/sparse/bm_index.cppm
+++ src/storage/knn_index/sparse/bm_index.cppm
@@ -25,9 +25,7 @@ namespace infinity {
 i32 ceil_div(i32 a, i32 b) { return (a + b - 1) / b; }
 
 struct PostingList {
-    Vector<Pair<i32, f32>> max_scores_;
-    Array<f32, 3> kth_score_; // [0] for 10th, [1] for 100th, [2] for 1000th
-
+public:
     f32 kth(i32 topk) const;
 
     SizeT GetSizeInBytes() const;
@@ -35,20 +33,58 @@ struct PostingList {
     void WriteAdv(char *&p) const;
 
     static PostingList ReadAdv(char *&p);
+
+public:
+    Vector<Pair<i32, f32>> max_scores_;
+    Array<f32, 3> kth_score_; // [0] for 10th, [1] for 100th, [2] for 1000th
+};
+
+struct BMIvt {
+public:
+    BMIvt() = default;
+
+    BMIvt(Vector<PostingList> postings) : postings_(std::move(postings)) {}
+
+    void AddBlock(i32 block_id, const Vector<Vector<Pair<i32, f32>>> &tail_terms);
+
+    void Optimize(Vector<Vector<f32>> ivt_scores);
+
+    const Vector<PostingList> &GetPostings() const { return postings_; }
+
+private:
+    Vector<PostingList> postings_;
 };
 
 export class BMIndex;
 
+struct TailFwd {
+public:
+    i8 AddDoc(const SparseVecRef<f32, i32> &doc);
+
+    const Vector<Vector<Pair<i32, f32>>> &GetTailTerms() const { return tail_terms_; }
+
+    Vector<Pair<i32, Vector<Pair<i8, f32>>>> ToBlockFwd() const;
+
+private:
+    Vector<Vector<Pair<i32, f32>>> tail_terms_;
+};
+
 struct BlockFwd {
 public:
-    BlockFwd(i32 block_size, Vector<Vector<Pair<i32, Vector<Pair<i8, f32>>>>> block_terms)
+    BlockFwd(i8 block_size) : block_size_(block_size) {}
+
+    BlockFwd(i8 block_size, Vector<Vector<Pair<i32, Vector<Pair<i8, f32>>>>> block_terms)
         : block_size_(block_size), block_terms_(std::move(block_terms)) {}
+
+    Optional<TailFwd> AddDoc(const SparseVecRef<f32, i32> &doc);
+
+    Vector<Vector<f32>> GetIvtScores(i32 term_num) const;
 
     Vector<f32> GetScores(i32 block_id, const SparseVecRef<f32, i32> &query) const;
 
     void Prefetch(i32 block_id) const;
 
-    i32 block_size() const { return block_size_; }
+    i8 block_size() const { return block_size_; }
 
     i32 block_num() const { return block_terms_.size(); }
 
@@ -59,8 +95,9 @@ public:
     static BlockFwd ReadAdv(char *&p);
 
 private:
-    i32 block_size_;
+    i8 block_size_;
     Vector<Vector<Pair<i32, Vector<Pair<i8, f32>>>>> block_terms_;
+    TailFwd tail_fwd_;
 };
 
 export class BMIndexBuilder {
@@ -84,7 +121,14 @@ export class BMIndex {
 public:
     friend class BMIndexBuilder;
 
-    BMIndex(Vector<PostingList> postings, BlockFwd block_fwd) : postings_(std::move(postings)), block_fwd_(std::move(block_fwd)) {}
+    BMIndex(i32 term_num, i8 block_size) : term_num_(term_num), block_fwd_(block_size) {}
+
+    BMIndex(Vector<PostingList> postings, BlockFwd block_fwd)
+        : term_num_(postings.size()), bm_ivt_(std::move(postings)), block_fwd_(std::move(block_fwd)) {}
+
+    void AddDoc(const SparseVecRef<f32, i32> &doc);
+
+    void Optimize();
 
     Pair<Vector<i32>, Vector<f32>> SearchKnn(const SparseVecRef<f32, i32> &query, i32 topk, f32 alpha, f32 beta) const;
 
@@ -100,7 +144,8 @@ private:
     static BMIndex ReadAdv(char *&p);
 
 private:
-    Vector<PostingList> postings_;
+    i32 term_num_;
+    BMIvt bm_ivt_;
     BlockFwd block_fwd_;
 };
 
