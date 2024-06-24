9fded40e3d450aeae5b3631e932c6ff3098bc006
Yingfeng <yingfeng.zhang@gmail.com>
2024-06-24 19:10:18 +0800

    Support traditional Chinese for full text search (#1378)
    ### What problem does this PR solve?
    
    Using opencc for converting from tradtional Chinese to simplified
    Chinese
    (Just introducing older version of OpenCC for much less dependencies)
    
    Issue:#1376
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- src/common/analyzer/analyzer_pool.cpp
+++ src/common/analyzer/analyzer_pool.cpp
@@ -24,6 +24,7 @@ import config;
 import infinity_context;
 import analyzer;
 import chinese_analyzer;
+import traditional_chinese_analyzer;
 import japanese_analyzer;
 import standard_analyzer;
 import ngram_analyzer;
@@ -69,6 +70,39 @@ Tuple<UniquePtr<Analyzer>, Status> AnalyzerPool::GetAnalyzer(const std::string_v
                 cut_grain = CutGrain::kFine;
             }
             UniquePtr<ChineseAnalyzer> analyzer = MakeUnique<ChineseAnalyzer>(*reinterpret_cast<ChineseAnalyzer *>(prototype));
+            analyzer->SetCutGrain(cut_grain);
+            return {std::move(analyzer), Status::OK()};
+        }
+        case Str2Int(TRADITIONALCHINESE.data()): {
+            // chinese-{coarse|fine}
+            Analyzer *prototype = cache_[TRADITIONALCHINESE].get();
+            if (prototype == nullptr) {
+                String path;
+                Config *config = InfinityContext::instance().config();
+                if (config == nullptr) {
+                    // InfinityContext has not been initialized.
+                    path = "/var/infinity/resource";
+                } else {
+                    path = config->ResourcePath();
+                }
+                UniquePtr<TraditionalChineseAnalyzer> analyzer = MakeUnique<TraditionalChineseAnalyzer>(std::move(path));
+                Status load_status = analyzer->Load();
+                if (!load_status.ok()) {
+                    return {nullptr, load_status};
+                }
+                prototype = analyzer.get();
+                cache_[TRADITIONALCHINESE] = std::move(analyzer);
+            }
+            CutGrain cut_grain = CutGrain::kCoarse;
+            const char *str = name.data();
+            while (*str != '\0' && *str != '-') {
+                str++;
+            }
+            if (strcmp(str, "-fine") == 0) {
+                cut_grain = CutGrain::kFine;
+            }
+            UniquePtr<TraditionalChineseAnalyzer> analyzer =
+                MakeUnique<TraditionalChineseAnalyzer>(*reinterpret_cast<TraditionalChineseAnalyzer *>(prototype));
             analyzer->SetCutGrain(cut_grain);
             return {std::move(analyzer), Status::OK()};
         }
