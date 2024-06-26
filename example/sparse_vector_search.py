eafca96c865e227147b35225cf0edcb643a6db43
Jin Hai <haijin.chn@gmail.com>
2024-06-26 23:26:26 +0800

    Remove redundant code  (#1396)
    ### What problem does this PR solve?
    
    Some third_party library isn't necessary any more.
    
    ### Type of change
    
    - [x] Refactoring
    
    ---------
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

---
+++ example/sparse_vector_search.py
@@ -0,0 +1,67 @@
+# Copyright(C) 2023 InfiniFlow, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+This example is to connect local infinity instance, create table, insert data, search the data
+'''
+
+import infinity
+
+try:
+    # open a local directory to store the data
+    infinity_instance = infinity.connect("/var/infinity")
+
+    # connect to server with 127.0.0.1
+    # infinity_instance = infinity.connect(infinity.common.LOCAL_HOST)
+
+    # 'default_db' is the default database
+    db_instance = infinity_instance.get_database("default_db")
+
+    # Drop my_table if it already exists
+    db_instance.drop_table("my_table", infinity.common.ConflictType.Ignore)
+
+    # Create a table named "my_table"
+    table_instance = db_instance.create_table("my_table", {
+        "num": {"type": "integer"},
+        "body": {"type": "varchar"},
+        "vec": {"type": "sparse,100,float,int"}
+    })
+
+    # Insert 3 rows of data into the 'my_table'
+    table_instance.insert(
+        [
+            {
+                "num": 1,
+                "body": r"unnecessary and harmful",
+                "vec": {"indices": [10, 20, 30], "values": [1.1, 2.2, 3.3]}
+            },
+            {
+                "num": 2,
+                "body": r"Office for Harmful Blooms",
+                "vec": {"indices": [40, 50, 60], "values": [4.4, 5.5, 6.6]}
+            },
+            {
+                "num": 3,
+                "body": r"A Bloom filter is a space-efficient probabilistic data structure, conceived by Burton Howard Bloom in 1970, that is used to test whether an element is a member of a set.",
+                "vec":  {"indices": [70, 80, 90], "values": [7.7, 8.8, 9.9]}
+            },
+        ]
+    )
+
+    result = table_instance.output(["num", "vec", "_similarity"]).match_sparse("vec", {"indices": [0, 20, 80], "values": [1.0, 2.0, 3.0]}, "ip", 3).to_pl()
+    print(result)
+    infinity_instance.disconnect()
+
+except Exception as e:
+    print(str(e))
