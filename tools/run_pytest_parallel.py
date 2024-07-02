889e024d37fec7a913bcd8421709a749ada1cb80
Ma-cat <1054638297@qq.com>
2024-07-02 15:03:49 +0800

    fix parallel test script and additional drop database during testing

--- tools/run_pytest_parallel.py
+++ tools/run_pytest_parallel.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import sys
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 commands = [
@@ -10,13 +11,22 @@ commands = [
 
 def run_command(command):
     start_time = time.time()
-    subprocess.run(command, shell=True)
+    result = subprocess.run(command, shell=True)
     end_time = time.time()
     elapsed_time = end_time - start_time
+    if result.returncode != 0:
+        raise RuntimeError(f"Command '{command}' failed with return code {result.returncode}")
     return command, elapsed_time
 
-with ProcessPoolExecutor() as executor:
-    futures = [executor.submit(run_command, cmd) for cmd in commands]
-    for future in as_completed(futures):
-        command, elapsed_time = future.result()
-        print(f"Command '{command}' executed in {elapsed_time:.2f} seconds")
+if __name__ == "__main__":
+    print("Note: this script must be run under root directory of the project.")
+    try:
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(run_command, cmd) for cmd in commands]
+            for future in as_completed(futures):
+                command, elapsed_time = future.result()
+                print(f"Command '{command}' executed in {elapsed_time:.2f} seconds")
+    except RuntimeError as e:
+        print(e)
+        sys.exit(-1)
+
