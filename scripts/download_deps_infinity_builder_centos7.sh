f2ecac800fdf1d2951b9824036aee6d1dd7d3998
Zhichang Yu <yuzhichang@gmail.com>
2024-07-02 18:46:44 +0800

    Use gcc 13 and clang 18 inside builder image

--- scripts/download_deps_infinity_builder_centos7.sh
+++ scripts/download_deps_infinity_builder_centos7.sh
@@ -16,10 +16,9 @@ https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz
 https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz
 https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-linux-x86_64.tar.gz
 https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
-https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz
+https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/llvm-project-18.1.8.src.tar.xz
 https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.bz2
 https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz
-https://github.com/axboe/liburing/archive/refs/tags/liburing-2.5.tar.gz
 https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz
 https://github.com/lz4/lz4/releases/download/v1.9.4/lz4-1.9.4.tar.gz
 https://github.com/madler/zlib/releases/download/v1.3.1/zlib-1.3.1.tar.gz
