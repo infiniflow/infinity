8eb3434684d673351a97f1598d13bcff49ff2c46
Ma-cat <1054638297@qq.com>
2024-07-02 12:40:12 +0800

    delete pytest.mark.skip

--- python/test/cases/test_infinity.py
+++ python/test/cases/test_infinity.py
@@ -19,6 +19,5 @@ class TestInfinity:
     def test_get_infinity(self):
         self.test_infinity_obj._test_get_database()
 
-    @pytest.mark.skip
     def test_list_infinity(self):
         self.test_infinity_obj._test_list_database()
