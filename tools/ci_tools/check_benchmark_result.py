c39f5d1c92623a3dd235e742176b0cdec86e8ea5
Jin Hai <haijin.chn@gmail.com>
2024-07-07 11:30:49 +0800

    Update benchmark baseline
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- tools/ci_tools/check_benchmark_result.py
+++ tools/ci_tools/check_benchmark_result.py
@@ -7,7 +7,7 @@ import sys
 
 
 def main():
-    benchmark_bars = {"sift_1": 1.84197, "sift_8": 0.286523}
+    benchmark_bars = {"sift_1": 1.84197, "sift_8": 0.309965}
     benchmark_id = sys.argv[1]
     standard = benchmark_bars[benchmark_id]
     file_path = sys.argv[2]
