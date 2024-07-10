43c2e8875901b26149d4f3a97301ea35ff76eab2
Jin Hai <haijin.chn@gmail.com>
2024-07-10 23:53:58 +0800

    Update version info (#1463)
    ### What problem does this PR solve?
    
    Ready for 0.2.1.dev5
    
    ### Type of change
    
    - [x] Documentation Update
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- python/infinity/remote_thrift/client.py
+++ python/infinity/remote_thrift/client.py
@@ -55,7 +55,7 @@ class ThriftInfinityClient:
         # version: 0.2.0.dev7, client_version: 6
         # version: 0.2.0.dev8, client_version: 7
         # version: 0.2.0, client_version: 8
-        # version: 0.2.1.dev4, client_version: 9
+        # version: 0.2.1.dev5, client_version: 9
         res = self.client.Connect(ConnectRequest(client_version=9))
         if res.error_code != 0:
             raise InfinityException(res.error_code, res.error_msg)
