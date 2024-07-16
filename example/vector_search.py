7dbf24538fa75bb957cd950c9c306331a331cb97
Jin Hai <haijin.chn@gmail.com>
2024-07-16 14:20:33 +0800

    Fix document error
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- example/vector_search.py
+++ example/vector_search.py
@@ -59,7 +59,7 @@ try:
         ]
     )
 
-    result = table_instance.output(["num", "vec", "_similarity"]).knn("vec", [3.0, 2.8, 2.7, 3.1], "float", "ip",
+    result = table_instance.output(["num", "vec", "_similarity"]).knn("vec", [3.0, 2.8, 2.7, 3.1], "float", "cosine",
                                                                       3).to_pl()
     print(result)
     infinity_instance.disconnect()
