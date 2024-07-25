77c14a4d2a5b012e1da183d78b21a1f9437b83f8
Zhiyuan Liang <132966438+Ami11111@users.noreply.github.com>
2024-07-25 20:02:55 +0800

    Add test cases for tensor in test_knn.py (#1528)
    ### What problem does this PR solve?
    Add test cases for invalid match tensor params, including data type,
    match type and extra options.
    
    ### Type of change
    - [x] Test cases
    
    ---------
    
    Co-authored-by: Jin Hai <haijin.chn@gmail.com>

--- python/test/internal/test_knn.py
+++ python/test/internal/test_knn.py
@@ -855,3 +855,241 @@ class TestKnn(TestSdk):
 
         res = db_obj.drop_table("test_with_multiple_fusion", ConflictType.Error)
         assert res.error_code == ErrorCode.OK
+
+    def _test_with_various_index_knn_distance_combination(self, check_data, index_column_name, knn_column_name,
+                               index_distance_type, knn_distance_type, index_type):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_with_index", ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_with_index", {
+            "variant_id": {"type": "varchar"},
+            "gender_vector": {"type": "vector,4,float"},
+            "color_vector": {"type": "vector,4,float"},
+            "category_vector": {"type": "vector,4,float"},
+            "tag_vector": {"type": "vector,4,float"},
+            "other_vector": {"type": "vector,4,float"},
+            "query_is_recommend": {"type": "varchar"},
+            "query_gender": {"type": "varchar"},
+            "query_color": {"type": "varchar"},
+            "query_price": {"type": "float"}
+        }, ConflictType.Error)
+        if not check_data:
+            copy_data("pysdk_test_knn.csv")
+        test_csv_dir = "/var/infinity/test_data/pysdk_test_knn.csv"
+        table_obj.import_data(test_csv_dir, None)
+        if index_type == index.IndexType.Hnsw:
+            if index_distance_type == "cosine":
+                with pytest.raises(InfinityException) as e:
+                    res = table_obj.create_index("my_index",
+                                                 [index.IndexInfo(index_column_name,
+                                                                  index.IndexType.Hnsw,
+                                                                  [
+                                                                      index.InitParameter(
+                                                                          "M", "16"),
+                                                                      index.InitParameter(
+                                                                          "ef_construction", "50"),
+                                                                      index.InitParameter(
+                                                                          "ef", "50"),
+                                                                      index.InitParameter(
+                                                                          "metric", index_distance_type)
+                                                                  ])], ConflictType.Error)
+                assert e.type == InfinityException
+                assert e.value.args[0] == ErrorCode.INVALID_INDEX_PARAM
+            else:
+                res = table_obj.create_index("my_index",
+                                             [index.IndexInfo(index_column_name,
+                                                              index.IndexType.Hnsw,
+                                                              [
+                                                                  index.InitParameter(
+                                                                      "M", "16"),
+                                                                  index.InitParameter(
+                                                                      "ef_construction", "50"),
+                                                                  index.InitParameter(
+                                                                      "ef", "50"),
+                                                                  index.InitParameter(
+                                                                      "metric", index_distance_type)
+                                                              ])], ConflictType.Error)
+                assert res.error_code == ErrorCode.OK
+                res = table_obj.output(["variant_id"]).knn(
+                    knn_column_name, [1] * 4, "float", knn_distance_type, 5).to_pl()
+                print(res)
+                res = table_obj.drop_index("my_index", ConflictType.Error)
+                assert res.error_code == ErrorCode.OK
+        elif index_type == index.IndexType.IVFFlat:
+            if index_distance_type == "cosine":
+                with pytest.raises(InfinityException) as e:
+                    res = table_obj.create_index("my_index",
+                                                 [index.IndexInfo(index_column_name,
+                                                                  index.IndexType.IVFFlat,
+                                                                  [index.InitParameter("centroids_count", "128"),
+                                                                   index.InitParameter("metric",
+                                                                                       index_distance_type)])],
+                                                 ConflictType.Error)
+                assert e.type == InfinityException
+                assert e.value.args[0] == ErrorCode.LACK_INDEX_PARAM
+            else:
+                res = table_obj.create_index("my_index",
+                                             [index.IndexInfo(index_column_name,
+                                                              index.IndexType.IVFFlat,
+                                                              [index.InitParameter("centroids_count", "128"),
+                                                               index.InitParameter("metric", index_distance_type)])], ConflictType.Error)
+                assert res.error_code == ErrorCode.OK
+                #for IVFFlat, index_distance_type has to match knn_distance_type?
+                res = table_obj.output(["variant_id"]).knn(
+                    knn_column_name, [1] * 4, "float", index_distance_type, 5).to_pl()
+                print(res)
+                res = table_obj.drop_index("my_index", ConflictType.Error)
+                assert res.error_code == ErrorCode.OK
+
+        res = db_obj.drop_table("test_with_index", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+    def _test_zero_dimension_vector(self):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_zero_dimension_vector",
+                          conflict_type=ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_zero_dimension_vector", {
+            "zero_vector": {"type": "vector,0,float"},
+        }, ConflictType.Error)
+
+        # try to insert and search a non-zero dim vector
+        with pytest.raises(InfinityException) as e:
+            table_obj.insert([{"zero_vector":[0.0]}])
+        assert e.type == InfinityException
+        assert e.value.args[0] == ErrorCode.DATA_TYPE_MISMATCH
+        with pytest.raises(InfinityException) as e:
+            res = table_obj.output(["_row_id"]).knn(
+                "zero_vector", [0.0], "float", "l2", 5).to_pl()
+        assert e.type == InfinityException
+        assert e.value.args[0] == ErrorCode.SYNTAX_ERROR
+
+        # try to insert and search a zero dim vector
+        with pytest.raises(IndexError) as e:
+            table_obj.insert([{"zero_vector":[]}])
+        try:
+            res = table_obj.output(["_row_id"]).knn(
+            "zero_vector", [], "float", "l2", 5).to_pl()
+        except:
+            print("Exception")
+
+        res = db_obj.drop_table("test_zero_dimension_vector", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+    def _test_big_dimension_vector(self, dim):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_big_dimension_vector",
+                          conflict_type=ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_big_dimension_vector", {
+            "big_vector": {"type": f"vector,{dim},float"},
+        }, ConflictType.Error)
+        table_obj.insert([{"big_vector": [1.0]*dim},
+                          {"big_vector": [2.0]*dim},
+                          {"big_vector": [3.0]*dim},
+                          {"big_vector": [4.0]*dim},
+                          {"big_vector": [5.0]*dim}])
+        res = table_obj.output(["_row_id"]).knn(
+            "big_vector", [0.0]*dim, "float", "l2", 5).to_pl()
+        print(res)
+
+        res = db_obj.drop_table("test_big_dimension_vector", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+    def _test_with_various_fulltext_match(self, check_data, fields_and_matching_text):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table(
+            "test_with_various_fulltext_match", ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_with_various_fulltext_match",
+                                        {"doctitle": {"type": "varchar"},
+                                         "docdate": {"type": "varchar"},
+                                         "body": {"type": "varchar"},
+                                         "num": {"type": "int"},
+                                         "vec": {"type": "vector, 4, float"}})
+        table_obj.create_index("my_index",
+                               [index.IndexInfo("body",
+                                                index.IndexType.FullText,
+                                                [index.InitParameter("ANALYZER", "standard")]),
+                                ], ConflictType.Error)
+
+        if not check_data:
+            generate_commas_enwiki(
+                "enwiki_99.csv", "enwiki_embedding_99_commas.csv", 1)
+            copy_data("enwiki_embedding_99_commas.csv")
+
+        test_csv_dir = common_values.TEST_TMP_DIR + "enwiki_embedding_99_commas.csv"
+        table_obj.import_data(test_csv_dir, import_options={"delimiter": ","})
+        res = (table_obj
+               .output(["*"])
+               .knn("vec", [3.0, 2.8, 2.7, 3.1], "float", "ip", 1)
+               .match(fields_and_matching_text[0], fields_and_matching_text[1], "topn=1")
+               .fusion('rrf')
+               .to_pl())
+        print(res)
+
+        res = table_obj.drop_index("my_index", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+        res = db_obj.drop_table(
+            "test_with_various_fulltext_match", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+    def _test_tensor_scan_with_invalid_data_type(self, check_data, data_type):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_tensor_scan", ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_tensor_scan",
+                                        {"title": {"type": "varchar"},
+                                            "num": {"type": "int"},
+                                            "t": {"type": "tensor, 4, float"},
+                                            "body": {"type": "varchar"}})
+        if not check_data:
+            copy_data("tensor_maxsim.csv")
+        test_csv_dir = common_values.TEST_TMP_DIR + "tensor_maxsim.csv"
+        table_obj.import_data(test_csv_dir, import_options={"delimiter": ","})
+        with pytest.raises(Exception):
+            res = (table_obj
+                    .output(["*", "_row_id", "_score"])
+                    .match_tensor('t', [[0.0, -10.0, 0.0, 0.7], [9.2, 45.6, -55.8, 3.5]], data_type, 'maxsim', 'topn=2')
+                    .to_pl())
+
+        res = db_obj.drop_table("test_tensor_scan", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+    def _test_tensor_scan_with_invalid_method_type(self, check_data, method_type):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_tensor_scan", ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_tensor_scan",
+                                        {"title": {"type": "varchar"},
+                                            "num": {"type": "int"},
+                                            "t": {"type": "tensor, 4, float"},
+                                            "body": {"type": "varchar"}})
+        if not check_data:
+            copy_data("tensor_maxsim.csv")
+        test_csv_dir = common_values.TEST_TMP_DIR + "tensor_maxsim.csv"
+        table_obj.import_data(test_csv_dir, import_options={"delimiter": ","})
+        with pytest.raises(Exception):
+            res = (table_obj
+                    .output(["*", "_row_id", "_score"])
+                    .match_tensor('t', [[0.0, -10.0, 0.0, 0.7], [9.2, 45.6, -55.8, 3.5]], 'float', method_type, 'topn=2')
+                    .to_pl())
+
+        res = db_obj.drop_table("test_tensor_scan", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
+
+    def _test_tensor_scan_with_invalid_extra_option(self, check_data, extra_option):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_tensor_scan", ConflictType.Ignore)
+        table_obj = db_obj.create_table("test_tensor_scan",
+                                        {"title": {"type": "varchar"},
+                                            "num": {"type": "int"},
+                                            "t": {"type": "tensor, 4, float"},
+                                            "body": {"type": "varchar"}})
+        if not check_data:
+            copy_data("tensor_maxsim.csv")
+        test_csv_dir = common_values.TEST_TMP_DIR + "tensor_maxsim.csv"
+        table_obj.import_data(test_csv_dir, import_options={"delimiter": ","})
+        with pytest.raises(Exception):
+            res = (table_obj
+                    .output(["*", "_row_id", "_score"])
+                    .match_tensor('t', [[0.0, -10.0, 0.0, 0.7], [9.2, 45.6, -55.8, 3.5]], 'float', 'maxsim', extra_option)
+                    .to_pl())
+
+        res = db_obj.drop_table("test_tensor_scan", ConflictType.Error)
+        assert res.error_code == ErrorCode.OK
