1467326b9cef963d3a214960324b2a16abbe71af
Jin Hai <haijin.chn@gmail.com>
2024-08-05 13:30:56 +0800

    Update version info of 0.3.0.dev3 (#1584)
    ### What problem does this PR solve?
    
    Release 0.3.0.dev3 server and client
    
    ### Type of change
    
    - [x] Other (please describe): Update version info before release
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- benchmark/remote_infinity/remote_query_benchmark.cpp
+++ benchmark/remote_infinity/remote_query_benchmark.cpp
@@ -51,7 +51,7 @@ struct InfinityClient {
         transport->open();
         CommonResponse response;
         ConnectRequest request;
-        request.__set_client_version(11); // 0.3.0.dev1
+        request.__set_client_version(12); // 0.3.0.dev3
         client->Connect(response, request);
         session_id = response.session_id;
     }
--- python/infinity/remote_thrift/client.py
+++ python/infinity/remote_thrift/client.py
@@ -58,7 +58,8 @@ class ThriftInfinityClient:
         # version: 0.2.1.dev5, client_version: 9
         # version: 0.2.1, client_version: 10
         # version: 0.3.0.dev1, client_version: 11
-        res = self.client.Connect(ConnectRequest(client_version=11))
+        # version: 0.3.0.dev3, client_version: 12
+        res = self.client.Connect(ConnectRequest(client_version=12))
         if res.error_code != 0:
             raise InfinityException(res.error_code, res.error_msg)
         self.session_id = res.session_id
