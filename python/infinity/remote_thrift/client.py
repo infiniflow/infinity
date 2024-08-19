ba7954b925a627931246c3efbed7a2d8d3e639ef
Jin Hai <haijin.chn@gmail.com>
2024-08-19 16:52:42 +0800

    Update version info to 0.3.0.dev6
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- python/infinity/remote_thrift/client.py
+++ python/infinity/remote_thrift/client.py
@@ -61,7 +61,8 @@ class ThriftInfinityClient:
         # version: 0.3.0.dev3, client_version: 12
         # version: 0.3.0.dev4, client_version: 13
         # version: 0.3.0.dev5, client_version: 14
-        res = self.client.Connect(ConnectRequest(client_version=14))
+        # version: 0.3.0.dev6, client_version: 15
+        res = self.client.Connect(ConnectRequest(client_version=15)) # 0.3.0.dev6
         if res.error_code != 0:
             raise InfinityException(res.error_code, res.error_msg)
         self.session_id = res.session_id
