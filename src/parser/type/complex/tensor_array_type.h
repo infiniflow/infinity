3a2d9e4824282b4b9dfe8b2e688993e2efb9bd4c
yzq <58433399+yangzq50@users.noreply.github.com>
2024-08-19 13:30:54 +0800

    update tensor, tensorarray

--- src/parser/type/complex/tensor_array_type.h
+++ src/parser/type/complex/tensor_array_type.h
@@ -22,8 +22,8 @@ namespace infinity {
 #pragma pack(1)
 
 struct TensorArrayType {
-    uint32_t tensor_num_ = 0;
-    uint32_t file_offset_ = 0;
+    uint64_t tensor_num_ : 16 = 0;
+    uint64_t file_offset_ : 48 = 0;
 
     [[nodiscard]] static std::string
     TensorArray2String(const std::vector<std::pair<std::span<const char>, size_t>> &raw_data, EmbeddingDataType type, size_t embedding_dimension);
