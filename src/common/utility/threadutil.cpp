76dccd6e184d818c69bbb61e37a40e09f10f5f7e
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2024-08-28 00:26:56 +0800

    remove log

--- src/common/utility/threadutil.cpp
+++ src/common/utility/threadutil.cpp
@@ -67,7 +67,7 @@ inline int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t *
         if (CPU_ISSET(core, cpu_set))
             break;
     }
-    printf("binding to core %d\n", core);
+    // printf("binding to core %d\n", core);
     thread_affinity_policy_data_t policy = {core};
     mach_thread = pthread_mach_thread_np(thread);
     thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
