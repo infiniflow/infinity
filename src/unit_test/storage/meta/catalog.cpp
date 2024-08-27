2e21da12d29705f4b0efbf1cf3bb2a7c6dc641dc
Ami11111 <691325878@qq.com>
2024-08-27 09:36:41 +0800

    add test cases

--- src/unit_test/storage/meta/catalog.cpp
+++ src/unit_test/storage/meta/catalog.cpp
@@ -428,3 +428,82 @@ TEST_P(CatalogTest, get_table_index_info_test) {
 
     txn_mgr->CommitTxn(txn1);
 }
+
+TEST_P(CatalogTest, remove_index_test) {
+    using namespace infinity;
+
+    TxnManager *txn_mgr = infinity::InfinityContext::instance().storage()->txn_manager();
+    Catalog *catalog = infinity::InfinityContext::instance().storage()->catalog();
+
+    // start txn1
+    auto *txn1 = txn_mgr->BeginTxn(MakeUnique<String>("create index"));
+
+    //create table
+    {
+        Vector<SharedPtr<ColumnDef>> columns;
+        {
+            std::set<ConstraintType> constraints;
+            constraints.insert(ConstraintType::kNotNull);
+            i64 column_id = 0;
+            auto embeddingInfo = MakeShared<EmbeddingInfo>(EmbeddingDataType::kElemFloat, 128);
+            auto column_def_ptr =
+                MakeShared<ColumnDef>(column_id, MakeShared<DataType>(LogicalType::kEmbedding, embeddingInfo), "col1", constraints);
+            columns.emplace_back(column_def_ptr);
+        }
+        auto tbl1_def = MakeUnique<TableDef>(MakeShared<String>("default_db"), MakeShared<String>("tbl1"), columns);
+        auto [table_entry, status] = catalog->CreateTable("default_db", txn1->TxnID(), txn1->BeginTS(), std::move(tbl1_def), ConflictType::kError, txn_mgr);
+        EXPECT_TRUE(status.ok());
+    }
+
+    // CreateIndex
+    {
+        Vector<String> columns1{"col1"};
+        Vector<InitParameter *> parameters1;
+        parameters1.emplace_back(new InitParameter("metric", "l2"));
+        parameters1.emplace_back(new InitParameter("encode", "plain"));
+        parameters1.emplace_back(new InitParameter("m", "16"));
+        parameters1.emplace_back(new InitParameter("ef_construction", "200"));
+
+        SharedPtr<String> index_name = MakeShared<String>("hnsw_index");
+        auto index_base_hnsw = IndexHnsw::Make(index_name, "hnsw_index_test_hnsw", columns1, parameters1);
+        for (auto *init_parameter : parameters1) {
+            delete init_parameter;
+        }
+
+        const String &db_name = "default_db";
+        const String &table_name = "tbl1";
+        ConflictType conflict_type = ConflictType::kError;
+        auto [table_entry, table_status] = catalog->GetTableByName(db_name, table_name, txn1->TxnID(), txn1->BeginTS());
+        EXPECT_EQ(table_status.ok(), true);
+        auto [index_entry, index_status] = catalog->CreateIndex(table_entry, index_base_hnsw, conflict_type, txn1->TxnID(), txn1->BeginTS(), txn_mgr);
+        EXPECT_EQ(index_status.ok(), true);
+    }
+
+    //remove index
+    {
+        auto [index_entry, index_status] = catalog->GetIndexByName("default_db", "tbl1", "hnsw_index", txn1->TxnID(), txn1->BeginTS());
+        EXPECT_TRUE(index_status.ok());
+        auto status = catalog->RemoveIndexEntry(index_entry, txn1->TxnID());
+        EXPECT_TRUE(status.ok());
+    }
+
+    //drop table
+    {
+        auto [table_entry, status] = catalog->DropTableByName("default_db", "tbl1", ConflictType::kError, txn1->TxnID(), txn1->BeginTS(), txn_mgr);
+        EXPECT_TRUE(status.ok());
+    }
+
+    txn_mgr->CommitTxn(txn1);
+}
+
+TEST_P(CatalogTest, roll_back_test) {
+    using namespace infinity;
+
+    TxnManager *txn_mgr = infinity::InfinityContext::instance().storage()->txn_manager();
+    Catalog *catalog = infinity::InfinityContext::instance().storage()->catalog();
+
+    // start txn1
+    auto *txn1 = txn_mgr->BeginTxn(MakeUnique<String>("create index"));
+
+    txn_mgr->CommitTxn(txn1);
+}
