464bebed3c63b5b61a19d5a971eb050409067479
Zhichang Yu <yuzhichang@gmail.com>
2024-08-27 17:14:45 +0800

    Don't read file when replaying catalog and delta checkpoint

--- src/storage/wal/wal_entry.cpp
+++ src/storage/wal/wal_entry.cpp
@@ -923,6 +923,7 @@ String WalCmdOptimize::ToString() const {
     for (auto &param_ptr : params_) {
         ss << param_ptr->ToString() << " | ";
     }
+    ss << std::endl;
     return ss.str();
 }
 
@@ -940,6 +941,7 @@ String WalCmdDumpIndex::ToString() const {
     for (auto &chunk_id : deprecate_ids_) {
         ss << chunk_id << " | ";
     }
+    ss << std::endl;
     return ss.str();
 }
 
--- src/storage/wal/wal_manager.cpp
+++ src/storage/wal/wal_manager.cpp
@@ -580,7 +580,7 @@ i64 WalManager::ReplayWalFile() {
         last_commit_ts = replay_entries[replay_count]->commit_ts_;
         last_txn_id = replay_entries[replay_count]->txn_id_;
 
-        LOG_INFO(replay_entries[replay_count]->ToString());
+        LOG_DEBUG(replay_entries[replay_count]->ToString());
         ReplayWalEntry(*replay_entries[replay_count]);
     }
 
