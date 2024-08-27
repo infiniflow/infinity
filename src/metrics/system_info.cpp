d34e5d92300180d53cc1db94c1d15d6d8b7900c9
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2024-08-27 12:59:24 +0800

    Enable system info for Mac

--- src/metrics/system_info.cpp
+++ src/metrics/system_info.cpp
@@ -14,6 +14,26 @@
 
 module;
 
+#if defined(__APPLE__)
+
+#include <cerrno>
+#include <libproc.h>
+#include <mach/host_info.h>
+#include <mach/kern_return.h>
+#include <mach/mach_host.h>
+#include <mach/mach_init.h>
+#include <mach/mach_port.h>
+#include <mach/mach_traps.h>
+#include <mach/shared_region.h>
+#include <mach/task.h>
+#include <mach/thread_act.h>
+#include <mach/thread_info.h>
+#include <mach/vm_map.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
+#endif
+
 #include <cstdio>
 #include <dirent.h>
 #include <stdlib.h>
@@ -34,28 +54,182 @@ namespace infinity {
 
 namespace {
 
+#if defined(__APPLE__)
+
+#define OK 0
+#define MSEC 1000L
+#define USEC MSEC * 1000L
+
+#define TICK2MSEC(s) ((u64)(s) * ((u64)MSEC / (double)ticks))
+
+#define NSEC2MSEC(s) ((u64)(s) / ((u64)1000000L))
+#define NSEC2USEC(s) ((u64)(s) / ((u64)1000L))
+
+#define tval2msec(tval) ((tval.seconds * MSEC) + (tval.microseconds / 1000))
+
+#define tv2msec(tv) (((u64)tv.tv_sec * MSEC) + (((u64)tv.tv_usec) / 1000))
+
+u64 get_sysctl_value(const char *name) {
+    size_t len = 0;
+    if (sysctlbyname(name, nullptr, &len, nullptr, 0) != 0) {
+        LOG_ERROR(fmt::format("get_sysctl_value {} ", name));
+        return 0;
+    }
+    if (len == sizeof(u64)) {
+        u64 ret = 0;
+        if (sysctlbyname(name, &ret, &len, nullptr, 0) != 0) {
+            LOG_ERROR(fmt::format("get_sysctl_value {} : failed to fetch value", name));
+            return 0;
+        }
+        return ret;
+    }
+
+    if (len == sizeof(uint32_t)) {
+        uint32_t ret;
+        if (sysctlbyname("vm.pagesize", &ret, &len, nullptr, 0) != 0) {
+            LOG_ERROR(fmt::format("get_sysctl_value {} : failed to fetch value", name));
+            return 0;
+        }
+        return ret;
+    }
+    LOG_ERROR(fmt::format("get_sysctl_value {} : Unexpected variable size", name));
+    return 0;
+}
+
+vm_statistics64 get_vmstat() {
+    mach_port_t mach_port = mach_host_self();
+    vm_statistics64 vmstat;
+    mach_msg_type_number_t count = HOST_VM_INFO64_COUNT;
+    const auto status = host_statistics64(mach_port, HOST_VM_INFO64, (host_info_t)&vmstat, &count);
+
+    if (status != KERN_SUCCESS) {
+        LOG_ERROR(fmt::format("get_pinfo(): sysctl"));
+    }
+    return vmstat;
+}
+
+struct proc_time_t {
+    u64 start_time, user, sys, total;
+};
+
+int get_proc_times(u64 pid, proc_time_t *time) {
+    unsigned int count;
+    time_value_t utime = {0, 0}, stime = {0, 0};
+    task_basic_info_data_t ti;
+    task_thread_times_info_data_t tti;
+    task_port_t task, self;
+    kern_return_t status;
+
+    struct proc_taskinfo pti;
+    int sz = proc_pidinfo(pid, PROC_PIDTASKINFO, 0, &pti, sizeof(pti));
+
+    if (sz == sizeof(pti)) {
+        time->user = NSEC2MSEC(pti.pti_total_user);
+        time->sys = NSEC2MSEC(pti.pti_total_system);
+        time->total = time->user + time->sys;
+        return OK;
+    }
+
+    self = mach_task_self();
+    status = task_for_pid(self, pid, &task);
+    if (status != KERN_SUCCESS) {
+        return errno;
+    }
+
+    count = TASK_BASIC_INFO_COUNT;
+    status = task_info(task, TASK_BASIC_INFO, (task_info_t)&ti, &count);
+    if (status != KERN_SUCCESS) {
+        if (task != self) {
+            mach_port_deallocate(self, task);
+        }
+        return errno;
+    }
+
+    count = TASK_THREAD_TIMES_INFO_COUNT;
+    status = task_info(task, TASK_THREAD_TIMES_INFO, (task_info_t)&tti, &count);
+    if (status != KERN_SUCCESS) {
+        if (task != self) {
+            mach_port_deallocate(self, task);
+        }
+        return errno;
+    }
+
+    time_value_add(&utime, &ti.user_time);
+    time_value_add(&stime, &ti.system_time);
+    time_value_add(&utime, &tti.user_time);
+    time_value_add(&stime, &tti.system_time);
+
+    time->user = tval2msec(utime);
+    time->sys = tval2msec(stime);
+    time->total = time->user + time->sys;
+
+    return OK;
+}
 
 u64 cpu_total_cost() {
-    // different mode cpu cost
-    u64 user_time{0};
-    u64 nice_time{0};
-    u64 system_time{0};
-    u64 idle_time{0};
+    mach_port_t mach_port = mach_task_self();
 
-    std::ifstream file("/proc/stat");
-    if (!file) {
+    kern_return_t status;
+    mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+    host_cpu_load_info_data_t cpuload;
+
+    status = host_statistics(mach_port, HOST_CPU_LOAD_INFO, (host_info_t)&cpuload, &count);
+
+    if (status != KERN_SUCCESS) {
+        LOG_ERROR(fmt::format("get_cpu(): host_statistics"));
         return 0;
     }
 
-    String line;
-    if (std::getline(file, line)) {
-        std::istringstream iss(line);
-        String name;
-        iss >> name >> user_time >> nice_time >> system_time >> idle_time;
-    }
-    return (user_time + nice_time + system_time + idle_time);
+    int ticks = sysconf(_SC_CLK_TCK);
+    u64 user = TICK2MSEC(cpuload.cpu_ticks[CPU_STATE_USER]);
+    u64 sys = TICK2MSEC(cpuload.cpu_ticks[CPU_STATE_SYSTEM]);
+    u64 idle = TICK2MSEC(cpuload.cpu_ticks[CPU_STATE_IDLE]);
+    u64 nice = TICK2MSEC(cpuload.cpu_ticks[CPU_STATE_NICE]);
+    u64 total = user + nice + sys + idle;
+
+    return total;
 }
 
+u64 cpu_cost_of_process(pid_t pid) {
+    // get specific pid cpu use time
+    proc_time_t proctime;
+    int st = get_proc_times(pid, &proctime);
+    if (st != OK) {
+        return 0;
+    }
+
+    return proctime.user + proctime.sys;
+}
+
+i64 get_memory() {
+    u64 total_memory = get_sysctl_value("hw.memsize");
+    u64 page_size = get_sysctl_value("vm.pagesize");
+    const auto vmstat = get_vmstat();
+
+    u64 free = vmstat.free_count * page_size;
+    u64 used = total_memory - free;
+
+    u64 kern = vmstat.inactive_count * page_size;
+    u64 actual_free = free + kern;
+    u64 actual_used = used - kern;
+    return (i64)(actual_free + actual_used) / 1024;
+}
+
+int get_open_fd_count(pid_t pid) {
+    int rv = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, nullptr, 0);
+    if (rv < 0) {
+        return -1;
+    }
+
+    UniquePtr<char[]> buffer(new char[static_cast<size_t>(rv)]);
+    rv = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, buffer.get(), rv);
+    if (rv < 0) {
+        return -1;
+    }
+    return static_cast<int>(static_cast<unsigned long>(rv) / PROC_PIDLISTFD_SIZE);
+}
+
+#elif defined(linux)
 
 const char* get_items(const char* buffer, u32 item) {
     // read from buffer by offset
@@ -76,6 +250,27 @@ const char* get_items(const char* buffer, u32 item) {
     }
 
     return p;
+}
+
+u64 cpu_total_cost() {
+    // different mode cpu cost
+    u64 user_time{0};
+    u64 nice_time{0};
+    u64 system_time{0};
+    u64 idle_time{0};
+
+    std::ifstream file("/proc/stat");
+    if (!file) {
+        return 0;
+    }
+
+    String line;
+    if (std::getline(file, line)) {
+        std::istringstream iss(line);
+        String name;
+        iss >> name >> user_time >> nice_time >> system_time >> idle_time;
+    }
+    return (user_time + nice_time + system_time + idle_time);
 }
 
 u64 cpu_cost_of_process(pid_t pid) {
@@ -105,12 +300,15 @@ u64 cpu_cost_of_process(pid_t pid) {
 
     return (utime + stime + cutime + cstime);
 }
-
+#endif
 }
 
 i64 SystemInfo::MemoryUsage() {
     i64 vm_rss_in_kb = 0;
     try {
+#if defined(__APPLE__)
+        vm_rss_in_kb = get_memory();
+#elif defined(linux)
         FILE* file = fopen("/proc/self/status", "r");
         DeferFn defer_fn([&] {
             fclose(file);
@@ -131,6 +329,7 @@ i64 SystemInfo::MemoryUsage() {
                 iss >> vm_rss_in_kb >> kb;
             }
         }
+#endif
     } catch (std::exception& e) {
         Status status = Status::FailToGetSysInfo(fmt::format("Can't get VmRSS: {}", e.what()));
         RecoverableError(status);
@@ -162,8 +361,12 @@ f64 SystemInfo::CPUUsage() {
 }
 
 i64 SystemInfo::OpenFileCount() {
-    String dir_path;
     int count = 0;
+#if defined(__APPLE__)
+    pid_t current_pid = getpid();
+    count = get_open_fd_count(current_pid);
+#elif defined(linux)
+    String dir_path;
     DIR *dir;
     struct dirent *entry;
 
@@ -184,6 +387,7 @@ i64 SystemInfo::OpenFileCount() {
     }
 
     closedir(dir);
+#endif
     return count;
 }
 
