d543c2c36dfc733a8771c2688965a65c6dc4808e
yzq <58433399+yangzq50@users.noreply.github.com>
2024-08-28 18:34:51 +0800

    fix http sparse vector
    fix http default

--- python/infinity/common.py
+++ python/infinity/common.py
@@ -34,11 +34,19 @@ class SparseVector:
     def __post_init__(self):
         assert (self.values is None) or (len(self.indices) == len(self.values))
 
-    def to_dict(self):
+    def to_dict_old(self):
         d = {"indices": self.indices}
         if self.values is not None:
             d["values"] = self.values
         return d
+
+    def to_dict(self):
+        if self.values is None:
+            raise ValueError("SparseVector.values is None")
+        result = {}
+        for i, v in zip(self.indices, self.values):
+            result[str(i)] = v
+        return result
 
     @staticmethod
     def from_dict(d):
