18e4b239d403cb580f61c32a1e10a10c499b51e7
shenyushi <shenyushi99@qq.com>
2024-08-28 15:43:07 +0800

    Fix: unit test.

--- src/unit_test/storage/wal/wal_entry.cpp
+++ src/unit_test/storage/wal/wal_entry.cpp
@@ -281,15 +281,15 @@ TEST_F(WalEntryTest, ReadWriteVFS) {
     pm.SaveLocalPath(paths[0], obj_addr1);
 
     AddrSerializer addr_serializer;
-    SizeT size = addr_serializer.GetSizeInBytes(&pm, paths);
+    SizeT size = addr_serializer.Initialize(&pm, paths);
     auto buffer = MakeUnique<char[]>(size);
     char *ptr = buffer.get();
-    addr_serializer.WriteBufAdv(&pm, ptr, paths);
+    addr_serializer.WriteBufAdv(ptr);
     SizeT write_size = ptr - buffer.get();
     ASSERT_EQ(write_size, size);
 
     char *ptr1 = buffer.get();
-    Vector<String> paths1 = addr_serializer.ReadBufAdv(&pm, ptr1);
+    Vector<String> paths1 = addr_serializer.ReadBufAdv(ptr1);
     SizeT read_size = ptr1 - buffer.get();
     ASSERT_EQ(read_size, size);
     ASSERT_EQ(paths1, paths);
