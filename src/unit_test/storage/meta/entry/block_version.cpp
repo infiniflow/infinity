156e6cc50dd8284dedd220a4af21af69d44a5859
Zhiyuan Liang <132966438+Ami11111@users.noreply.github.com>
2024-09-02 19:58:40 +0800

    Add block_version test cases for unit test (#1778)
    ### What problem does this PR solve?
    
    Add block_version test cases for unit test
    
    ### Type of change
    
    - [x] Test cases

--- src/unit_test/storage/meta/entry/block_version.cpp
+++ src/unit_test/storage/meta/entry/block_version.cpp
@@ -28,6 +28,9 @@ import local_file_system;
 import file_system_type;
 import buffer_manager;
 import version_file_worker;
+import column_vector;
+import data_type;
+import logical_type;
 
 using namespace infinity;
 
@@ -142,4 +145,33 @@ TEST_P(BlockVersionTest, SaveAndLoad2) {
             ASSERT_EQ(block_version1, *block_version);
         }
     }
+}
+
+TEST_P(BlockVersionTest, delete_test) {
+    BlockVersion block_version(8192);
+    block_version.Delete(2, 30);
+    EXPECT_THROW(block_version.Delete(2, 30), UnrecoverableException);
+}
+
+TEST_P(BlockVersionTest, check_delete_test) {
+    BlockVersion block_version(8192);
+    block_version.Delete(2, 30);
+    EXPECT_TRUE(block_version.CheckDelete(2, 30));
+    EXPECT_TRUE(block_version.CheckDelete(2, 40));
+    EXPECT_FALSE(block_version.CheckDelete(2, 29));
+    EXPECT_FALSE(block_version.CheckDelete(3, 30));
+    EXPECT_FALSE(block_version.CheckDelete(8193, 30));
+}
+
+TEST_P(BlockVersionTest, get_delete_ts_test) {
+    BlockVersion block_version(8192);
+    block_version.Delete(2, 30);
+    block_version.Delete(5, 40);
+    auto res = MakeShared<ColumnVector>(MakeShared<DataType>(LogicalType::kTinyInt));
+    res->Initialize();
+    block_version.GetDeleteTS(2, 4, *res);
+    EXPECT_EQ(res->ToString(0), "30");
+    EXPECT_EQ(res->ToString(1), "0");
+    EXPECT_EQ(res->ToString(2), "0");
+    EXPECT_EQ(res->ToString(3), "40");
 }
