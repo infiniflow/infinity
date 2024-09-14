cc3776e7522972d547b2ba0e75715dc12791cff3
Zhichang Yu <yuzhichang@gmail.com>
2024-09-14 21:59:08 +0800

    Fix BlockEntry::DetermineDir

--- src/storage/meta/entry/block_entry.cpp
+++ src/storage/meta/entry/block_entry.cpp
@@ -577,7 +577,7 @@ SharedPtr<String> BlockEntry::DetermineDir(const String &parent_dir, BlockID blo
     LocalFileSystem fs;
     SharedPtr<String> relative_dir = MakeShared<String>(fmt::format("{}/blk_{}", parent_dir, block_id));
     String full_dir = Path(InfinityContext::instance().config()->DataDir()) / *relative_dir;
-    if (InfinityContext::instance()..persistence_manager() == nullptr)
+    if (InfinityContext::instance().persistence_manager() == nullptr)
         fs.CreateDirectoryNoExp(full_dir);
     return relative_dir;
 }
