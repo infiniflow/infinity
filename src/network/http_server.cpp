91ad4cd04291be5fb6a137bdf62c9bc237f1d7cc
Jin Hai <haijin.chn@gmail.com>
2024-09-16 20:23:43 +0800

    Fix can't shutdown http server
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- src/network/http_server.cpp
+++ src/network/http_server.cpp
@@ -3190,9 +3190,9 @@ void HTTPServer::Start(const String& ip_address, u16 port) {
 
     fmt::print("HTTP server listen on {}: {}\n", ip_address, port);
 
-    server_->run();
-
     started_ = true;
+
+    server_->run();
 }
 
 void HTTPServer::Shutdown() {
