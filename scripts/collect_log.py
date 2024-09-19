80fb1158fb4bb07a977e70dd4b207b9605652b6b
shenyushi <shenyushi99@qq.com>
2024-09-19 15:27:33 +0800

    Fix.

--- scripts/collect_log.py
+++ scripts/collect_log.py
@@ -23,16 +23,6 @@ log_path = args.log_path
 stdout_path = args.stdout_path
 output_dir = args.output_dir
 
-if not os.path.isfile(stdout_path):
-    print("Error: stdout file not found")
-    with open(stdout_path, "w") as f:
-        f.write("")
-
-if not os.path.isfile(log_path):
-    print("Error: /var/infinity/log/infinity.log not found")
-    with open(log_path, "w") as f:
-        f.write("")
-
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
@@ -40,5 +30,12 @@ if not os.path.isdir(output_dir):
 random_name = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
 print(f"Random log file name: {random_name}")
 
-shutil.copy(stdout_path, f"{output_dir}/{random_name}_.log")
-shutil.copy(log_path, f"{output_dir}/{random_name}.log")
+if not os.path.isfile(stdout_path):
+    print("Error: stdout file not found")
+else:
+    shutil.copy(stdout_path, f"{output_dir}/{random_name}_.log")
+
+if not os.path.isfile(log_path):
+    print("Error: /var/infinity/log/infinity.log not found")
+else:
+    shutil.copy(log_path, f"{output_dir}/{random_name}.log")
