5f4ef5cb402b800fdecb987019eae79b50eb54ff
shenyushi <shenyushi99@qq.com>
2024-09-23 10:51:25 +0800

    collect executable.

--- scripts/collect_log.py
+++ scripts/collect_log.py
@@ -12,15 +12,22 @@ parser.add_argument(
     "--stdout_path", type=str, required=True, help="Path to the output debug log file"
 )
 parser.add_argument(
+    "--executable_path",
+    type=str,
+    required=True,
+    help="Path to the executable file",
+)
+parser.add_argument(
     "--output_dir",
     type=str,
-    default="/var/infinity/log",
+    required=True,
     help="Path to the output directory",
 )
 args = parser.parse_args()
 
 log_path = args.log_path
 stdout_path = args.stdout_path
+executable_path = args.executable_path
 output_dir = args.output_dir
 
 if not os.path.isdir(output_dir):
@@ -39,3 +46,8 @@ if not os.path.isfile(log_path):
     print("Error: /var/infinity/log/infinity.log not found")
 else:
     shutil.copy(log_path, f"{output_dir}/{random_name}.log")
+
+if not os.path.isfile(executable_path):
+    print("Error: Executable file not found")
+else:
+    shutil.copy(executable_path, f"{output_dir}/{random_name}.exe")
