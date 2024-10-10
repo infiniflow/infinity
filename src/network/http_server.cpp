5e9a3cc1dafdb370615631b3a2b1586fcded026f
vsian <vsian_v@163.com>
2024-10-10 15:21:56 +0800

    unify http admin calls using infinity::admin* functions and update doc

--- src/network/http_server.cpp
+++ src/network/http_server.cpp
@@ -3473,7 +3473,7 @@ public:
     }
 };
 
-class ShowCurrentNodeHandler final : public HttpRequestHandler {
+class AdminShowCurrentNodeHandler final : public HttpRequestHandler {
 public:
     SharedPtr<OutgoingResponse> handle(const SharedPtr<IncomingRequest> &request) final {
 
@@ -3488,7 +3488,7 @@ public:
         if (result.IsOk()) {
             json_response["error_code"] = 0;
             DataBlock *data_block = result.result_table_->GetDataBlockById(0).get();
-            Value value = data_block->GetValue(1, 0);
+            Value value = data_block->GetValue(1, 1);
             const String &variable_value = value.ToString();
             json_response["role"] = variable_value;
 
@@ -3502,7 +3502,7 @@ public:
     }
 };
 
-class ShowNodeByNameHandler final : public HttpRequestHandler {
+class AdminShowNodeByNameHandler final : public HttpRequestHandler {
 public:
     SharedPtr<OutgoingResponse> handle(const SharedPtr<IncomingRequest> &request) final {
         auto infinity = Infinity::RemoteConnect();
@@ -3512,24 +3512,27 @@ public:
         HTTPStatus http_status;
 
         String node_name = request->getPathVariable("node_name");
-        infinity::Status status;
-        SharedPtr<NodeInfo> nodeinfo;
-        std::tie(status, nodeinfo) = InfinityContext::instance().cluster_manager()->GetNodeInfoPtrByName(node_name);
-        if (status.ok()) {
-            http_status = HTTPStatus::CODE_200;
+        QueryResult result = infinity->AdminShowNode(node_name);
+
+        if (result.IsOk()) {
+            DataBlock *data_block = result.result_table_->GetDataBlockById(0).get();
+            Value value = data_block->GetValue(1, 1);
+            const String &node_role = value.ToString();
+
             json_response["error_code"] = ErrorCode::kOk;
-            json_response["node_role"] = ToString(nodeinfo->node_role_);
+            json_response["role"] = node_role;
+            http_status = HTTPStatus::CODE_200;
         } else {
+            json_response["error_code"] = result.ErrorCode();
+            json_response["error_msg"] = result.ErrorMsg();
             http_status = HTTPStatus::CODE_500;
-            json_response["error_code"] = status.code();
-            json_response["error_msg"] = status.message();
         }
 
         return ResponseFactory::createResponse(http_status, json_response.dump());
     }
 };
 
-class ListAllNodesHandler final : public HttpRequestHandler {
+class AdminListAllNodesHandler final : public HttpRequestHandler {
 public:
     SharedPtr<OutgoingResponse> handle(const SharedPtr<IncomingRequest> &request) final {
         auto infinity = Infinity::RemoteConnect();
@@ -3539,15 +3542,26 @@ public:
         nlohmann::json json_response;
         nlohmann::json nodes_json;
 
-        Vector<SharedPtr<NodeInfo>> nodes = InfinityContext::instance().cluster_manager()->ListNodes();
-        for (const auto &ptr : nodes) {
-            nodes_json.push_back({ptr->node_name_, ToString(ptr->node_role_)});
+        QueryResult result = infinity->AdminShowNodes();
+        if (result.IsOk()) {
+            SizeT block_rows = result.result_table_->DataBlockCount();
+            for (SizeT block_id = 0; block_id < block_rows; ++block_id) {
+                DataBlock *data_block = result.result_table_->GetDataBlockById(block_id).get();
+                auto row_count = data_block->row_count();
+                for (int row = 0; row < row_count; ++row) {
+                    String node_name = data_block->GetValue(0, row).ToString();
+                    String node_role = data_block->GetValue(1, row).ToString();
+                    nodes_json.push_back({node_name, node_role});
+                }
+            }
+            http_status = HTTPStatus::CODE_200;
+            json_response["error_code"] = ErrorCode::kOk;
+            json_response["nodes"] = nodes_json;
+        } else {
+            json_response["error_code"] = result.ErrorCode();
+            json_response["error_message"] = result.ErrorMsg();
+            http_status = HTTPStatus::CODE_500;
         }
-
-        http_status = HTTPStatus::CODE_200;
-        json_response["error_code"] = ErrorCode::kOk;
-        json_response["nodes"] = nodes_json;
-
         return ResponseFactory::createResponse(http_status, json_response.dump());
     }
 };
@@ -3566,8 +3580,7 @@ public:
         nlohmann::json http_body_json;
         try {
             http_body_json = nlohmann::json::parse(data_body);
-        }
-        catch (nlohmann::json::exception &e) {
+        } catch (nlohmann::json::exception &e) {
             http_status = HTTPStatus::CODE_500;
             json_response["error_code"] = ErrorCode::kInvalidJsonFormat;
             json_response["error_message"] = e.what();
@@ -3582,32 +3595,32 @@ public:
         }
 
         String role = http_body_json["role"];
+        QueryResult result;
         ToLower(role);
-        if (role == "uninitialized") {
-            status = InfinityContext::instance().ChangeRole(NodeRole::kUnInitialized);
-        } else if (role == "admin") {
-            status = InfinityContext::instance().ChangeRole(NodeRole::kAdmin);
+        if (role == "admin") {
+            result = infinity->AdminSetAdmin();
         } else if (role == "standalone") {
-            status = InfinityContext::instance().ChangeRole(NodeRole::kStandalone);
+            result = infinity->AdminSetStandalone();
         } else if (role == "leader") {
-            status = InfinityContext::instance().ChangeRole(NodeRole::kLeader, http_body_json["name"]);
+            result = infinity->AdminSetLeader(http_body_json["name"]);
         } else if (role == "follower") {
-            String node_name = http_body_json["name"];
-            status = InfinityContext::instance().ChangeRole(NodeRole::kFollower, http_body_json["name"], http_body_json["address"], http_body_json["port"]);
+            result = infinity->AdminSetFollower(http_body_json["name"], http_body_json["address"]);
         } else if (role == "learner") {
-            String node_name = http_body_json["name"];
-            status = InfinityContext::instance().ChangeRole(NodeRole::kLearner, http_body_json["name"], http_body_json["address"], http_body_json["port"]);
+            result = infinity->AdminSetLearner(http_body_json["name"], http_body_json["address"]);
         } else {
-            status = infinity::Status::InvalidNodeRole("invalid node role");
+            http_status = HTTPStatus::CODE_500;
+            json_response["error_code"] = ErrorCode::kInvalidNodeRole;
+            json_response["error_code"] = "invalid node role";
+            return ResponseFactory::createResponse(http_status, json_response.dump());
         }
 
-        if (status.ok()) {
+        if (result.IsOk()) {
             http_status = HTTPStatus::CODE_200;
             json_response["error_code"] = ErrorCode::kOk;
         } else {
             http_status = HTTPStatus::CODE_500;
-            json_response["error_code"] = status.code();
-            json_response["error_code"] = status.message();
+            json_response["error_code"] = result.ErrorCode();
+            json_response["error_code"] = result.ErrorMsg();
         }
 
         return ResponseFactory::createResponse(http_status, json_response.dump());
@@ -3626,7 +3639,8 @@ public:
         auto result = infinity->AdminShowVariables();
         if (result.IsOk()) {
             json_response["error_code"] = 0;
-            DataBlock *data_block = result.result_table_->GetDataBlockById(0).get(); // Assume the variables output data only included in one data block
+            DataBlock *data_block =
+                result.result_table_->GetDataBlockById(0).get(); // Assume the variables output data only included in one data block
             auto row_count = data_block->row_count();
             for (int row = 0; row < row_count; ++row) {
                 // variable name
@@ -3806,9 +3820,9 @@ void HTTPServer::Start(const String &ip_address, u16 port) {
     router->route("GET", "/admin/variables", MakeShared<AdminShowNodeVariablesHandler>());
     router->route("GET", "/admin/configs", MakeShared<AdminShowNodeConfigsHandler>());
     router->route("GET", "/admin/variables/{variable_name}", MakeShared<AdminShowNodeVariableHandler>());
-    router->route("GET", "/admin/node/current", MakeShared<ShowCurrentNodeHandler>());
-    router->route("GET", "/admin/node/{node_name}", MakeShared<ShowNodeByNameHandler>());
-    router->route("GET", "/admin/nodes", MakeShared<ListAllNodesHandler>());
+    router->route("GET", "/admin/node/current", MakeShared<AdminShowCurrentNodeHandler>());
+    router->route("GET", "/admin/node/{node_name}", MakeShared<AdminShowNodeByNameHandler>());
+    router->route("GET", "/admin/nodes", MakeShared<AdminListAllNodesHandler>());
 
     SharedPtr<HttpConnectionProvider> connection_provider = HttpConnectionProvider::createShared({ip_address, port, WebAddress::IP_4});
     SharedPtr<HttpConnectionHandler> connection_handler = HttpConnectionHandler::createShared(router);
