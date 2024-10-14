04dd952d35430f61feae3b16ad3838cbc7a3e953
balibabu <cike8899@users.noreply.github.com>
2024-10-14 15:35:32 +0800

    feat: Hide unnecessary NavItem #1841 (#2037)
    ### What problem does this PR solve?
    
    feat: Hide unnecessary NavItem #1841
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- gui/app/(dashboard)/layout.tsx
+++ gui/app/(dashboard)/layout.tsx
@@ -1,25 +1,6 @@
-import {
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  PanelLeft,
-  Settings,
-  ShoppingCart,
-  Users2
-} from 'lucide-react';
+import { Home, Settings } from 'lucide-react';
 import Link from 'next/link';
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb';
-import { Button } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import {
   Tooltip,
   TooltipContent,
@@ -28,11 +9,8 @@ import {
 import { Analytics } from '@vercel/analytics/react';
 import { NavItem } from './nav-item';
 import Providers from './providers';
-import { SearchInput } from './search';
-import { User } from './user';
 import DatabaseIcon from '/public/database.svg';
 import Logo from '/public/logo.svg';
-import TableIcon from '/public/table.svg';
 
 export default function DashboardLayout({
   children
@@ -44,12 +22,12 @@ export default function DashboardLayout({
       <main className="flex min-h-screen w-full flex-col bg-muted/40">
         <DesktopNav />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+          {/* <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileNav />
             <DashboardBreadcrumb />
             <SearchInput />
             <User />
-          </header>
+          </header> */}
           <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
             {children}
           </main>
@@ -76,11 +54,11 @@ function DesktopNav() {
           <Home className="h-5 w-5" />
         </NavItem>
 
-        <NavItem href="/databases" label="Databases">
+        <NavItem href="/database" label="Databases">
           <DatabaseIcon className="h-5 w-5" />
         </NavItem>
 
-        <NavItem href="/tables" label="Tables">
+        {/* <NavItem href="/tables" label="Tables">
           <TableIcon className="h-5 w-5" />
         </NavItem>
 
@@ -90,7 +68,7 @@ function DesktopNav() {
 
         <NavItem href="#" label="Analytics">
           <LineChart className="h-5 w-5" />
-        </NavItem>
+        </NavItem> */}
       </nav>
       <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
         <Tooltip>
@@ -110,85 +88,85 @@ function DesktopNav() {
   );
 }
 
-function MobileNav() {
-  return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button size="icon" variant="outline" className="sm:hidden">
-          <PanelLeft className="h-5 w-5" />
-          <span className="sr-only">Toggle Menu</span>
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left" className="sm:max-w-xs">
-        <nav className="grid gap-6 text-lg font-medium">
-          <Link
-            href="#"
-            className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-          >
-            <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-            <span className="sr-only">Vercel</span>
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <Home className="h-5 w-5" />
-            Dashboard
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <ShoppingCart className="h-5 w-5" />
-            Orders
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-foreground"
-          >
-            <Package className="h-5 w-5" />
-            Products
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <Users2 className="h-5 w-5" />
-            Customers
-          </Link>
-          <Link
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <LineChart className="h-5 w-5" />
-            Settings
-          </Link>
-        </nav>
-      </SheetContent>
-    </Sheet>
-  );
-}
+// function MobileNav() {
+//   return (
+//     <Sheet>
+//       <SheetTrigger asChild>
+//         <Button size="icon" variant="outline" className="sm:hidden">
+//           <PanelLeft className="h-5 w-5" />
+//           <span className="sr-only">Toggle Menu</span>
+//         </Button>
+//       </SheetTrigger>
+//       <SheetContent side="left" className="sm:max-w-xs">
+//         <nav className="grid gap-6 text-lg font-medium">
+//           <Link
+//             href="#"
+//             className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+//           >
+//             <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+//             <span className="sr-only">Vercel</span>
+//           </Link>
+//           <Link
+//             href="#"
+//             className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+//           >
+//             <Home className="h-5 w-5" />
+//             Dashboard
+//           </Link>
+//           <Link
+//             href="#"
+//             className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+//           >
+//             <ShoppingCart className="h-5 w-5" />
+//             Orders
+//           </Link>
+//           <Link
+//             href="#"
+//             className="flex items-center gap-4 px-2.5 text-foreground"
+//           >
+//             <Package className="h-5 w-5" />
+//             Products
+//           </Link>
+//           <Link
+//             href="#"
+//             className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+//           >
+//             <Users2 className="h-5 w-5" />
+//             Customers
+//           </Link>
+//           <Link
+//             href="#"
+//             className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+//           >
+//             <LineChart className="h-5 w-5" />
+//             Settings
+//           </Link>
+//         </nav>
+//       </SheetContent>
+//     </Sheet>
+//   );
+// }
 
-function DashboardBreadcrumb() {
-  return (
-    <Breadcrumb className="hidden md:flex">
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="#">Dashboard</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="#">Products</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>All Products</BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
-  );
-}
+// function DashboardBreadcrumb() {
+//   return (
+//     <Breadcrumb className="hidden md:flex">
+//       <BreadcrumbList>
+//         <BreadcrumbItem>
+//           <BreadcrumbLink asChild>
+//             <Link href="#">Dashboard</Link>
+//           </BreadcrumbLink>
+//         </BreadcrumbItem>
+//         <BreadcrumbSeparator />
+//         <BreadcrumbItem>
+//           <BreadcrumbLink asChild>
+//             <Link href="#">Products</Link>
+//           </BreadcrumbLink>
+//         </BreadcrumbItem>
+//         <BreadcrumbSeparator />
+//         <BreadcrumbItem>
+//           <BreadcrumbPage>All Products</BreadcrumbPage>
+//         </BreadcrumbItem>
+//       </BreadcrumbList>
+//     </Breadcrumb>
+//   );
+// }
