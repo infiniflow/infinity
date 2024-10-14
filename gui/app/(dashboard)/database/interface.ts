61d536baffcf6acb08611ef6c1996e7fd11e5db6
balibabu <cike8899@users.noreply.github.com>
2024-10-14 11:51:04 +0800

    feat: Fixed the issue where the tree loading status displayed incorrectly when there was no table in the database #1841 (#2034)
    ### What problem does this PR solve?
    
    feat: Fixed the issue where the tree loading status displayed
    incorrectly when there was no table in the database #1841
    
    
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- gui/app/(dashboard)/database/interface.ts
+++ gui/app/(dashboard)/database/interface.ts
@@ -2,14 +2,6 @@ import { Leaf } from './constants';
 
 export type TreeParentId = string | number | null;
 
-export interface TreeNode {
-  name: string;
-  id: string | number;
-  children: Array<string | number>;
-  parent: TreeParentId;
-  isBranch?: boolean;
-}
-
 export interface DatabaseRouteParams {
   params: { databaseId: string; tableId: string };
   searchParams: { tab: Leaf };
