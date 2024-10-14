ea06ce95dbe440c65387454cdbcc96f9a8d5d172
Zhiyuan Liang <132966438+Ami11111@users.noreply.github.com>
2024-10-14 19:07:01 +0800

    Support char_length / character_length function (#2040)
    ### What problem does this PR solve?
    - Support char_length / character_length function
    - Add related test cases
    
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)
    - [x] Test cases

--- python/test_pysdk/test_select.py
+++ python/test_pysdk/test_select.py
@@ -804,3 +804,31 @@ class TestInfinity:
 
         res = db_obj.drop_table("test_sort"+suffix, ConflictType.Error)
         assert res.error_code == ErrorCode.OK
+
+    def test_select_varchar_length(self, suffix):
+        db_obj = self.infinity_obj.get_database("default_db")
+        db_obj.drop_table("test_select_varchar_length"+suffix, ConflictType.Ignore)
+        db_obj.create_table("test_select_varchar_length"+suffix,
+                            {"c1": {"type": "varchar", "constraints": ["primary key", "not null"]},
+                             "c2": {"type": "varchar", "constraints": ["not null"]}}, ConflictType.Error)
+        table_obj = db_obj.get_table("test_select_varchar_length"+suffix)
+        table_obj.insert(
+            [{"c1": 'a', "c2": 'a'}, {"c1": 'b', "c2": 'b'}, {"c1": 'c', "c2": 'c'}, {"c1": 'd', "c2": 'd'},
+             {"c1": 'abc', "c2": 'abc'}, {"c1": 'bbc', "c2": 'bbc'}, {"c1": 'cbc', "c2": 'cbc'}, {"c1": 'dbc', "c2": 'dbc'}])
+
+
+
+        res = table_obj.output(["*"]).filter("char_length(c1) = 1").to_df()
+        print(res)
+        pd.testing.assert_frame_equal(res, pd.DataFrame({'c1': ('a', 'b', 'c', 'd'),
+                                                         'c2': ('a', 'b', 'c', 'd')})
+                                      .astype({'c1': dtype('O'), 'c2': dtype('O')}))
+
+        res = table_obj.output(["*"]).filter("char_length(c1) = 3").to_df()
+        print(res)
+        pd.testing.assert_frame_equal(res, pd.DataFrame({'c1': ('abc', 'bbc', 'cbc', 'dbc'),
+                                                         'c2': ('abc', 'bbc', 'cbc', 'dbc')})
+                                      .astype({'c1': dtype('O'), 'c2': dtype('O')}))
+
+        res = db_obj.drop_table("test_select_varchar_length"+suffix)
+        assert res.error_code == ErrorCode.OK
