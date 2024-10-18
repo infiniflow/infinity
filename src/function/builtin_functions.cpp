0ea7ed029fa013eb43d797c567ab38e0cac09322
Zhiyuan Liang <132966438+Ami11111@users.noreply.github.com>
2024-10-18 21:09:34 +0800

    Support ltrim, rtrim, trim functions (#2064)
    ### What problem does this PR solve?
    Support ltrim, rtrim, trim functions
    
    ### Type of change
    
    - [x] New Feature (non-breaking change which adds functionality)

--- src/function/builtin_functions.cpp
+++ src/function/builtin_functions.cpp
@@ -51,6 +51,9 @@ import md5;
 import lower;
 import upper;
 import regex;
+import ltrim;
+import rtrim;
+import trim;
 import default_values;
 import special_function;
 import internal_types;
@@ -123,6 +126,9 @@ void BuiltinFunctions::RegisterScalarFunction() {
     RegisterLowerFunction(catalog_ptr_);
     RegisterUpperFunction(catalog_ptr_);
     RegisterRegexFunction(catalog_ptr_);
+    RegisterLtrimFunction(catalog_ptr_);
+    RegisterRtrimFunction(catalog_ptr_);
+    RegisterTrimFunction(catalog_ptr_);
 }
 
 void BuiltinFunctions::RegisterTableFunction() {}
---
+++ src/function/scalar/trim.cppm
@@ -0,0 +1,13 @@
+module;
+
+import stl;
+
+export module trim;
+
+namespace infinity {
+
+class Catalog;
+
+export void RegisterTrimFunction(const UniquePtr<Catalog> &catalog_ptr);
+
+}
