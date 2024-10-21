3152afbc684384aa9199a8d786f19cdcee95a9c5
Ami11111 <691325878@qq.com>
2024-10-21 15:28:11 +0800

    update

--- src/function/scalar/ceiling.cpp
+++ src/function/scalar/ceiling.cpp
@@ -17,7 +17,14 @@ import data_type;
 
 namespace infinity {
 
-struct CeilingFunction {
+struct CeilingFunctionInt {
+    template <typename SourceType, typename TargetType>
+    static inline void Run(SourceType value, TargetType &result) {
+        result = value;
+    }
+};
+
+struct CeilingFunctionFloat {
     template <typename SourceType, typename TargetType>
     static inline bool Run(SourceType value, TargetType &result) {
         result = ceil(static_cast<double>(value));
@@ -35,50 +42,50 @@ void RegisterCeilingFunction(const UniquePtr<Catalog> &catalog_ptr) {
 
     ScalarFunction ceiling_int8(func_name,
                             {DataType(LogicalType::kTinyInt)},
-                            DataType(LogicalType::kDouble),
-                            &ScalarFunction::UnaryFunctionWithFailure<TinyIntT, DoubleT, CeilingFunction>);
+                            DataType(LogicalType::kTinyInt),
+                            &ScalarFunction::UnaryFunction<TinyIntT, TinyIntT, CeilingFunctionInt>);
     function_set_ptr->AddFunction(ceiling_int8);
 
     ScalarFunction ceiling_int16(func_name,
                              {DataType(LogicalType::kSmallInt)},
-                             {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<SmallIntT, DoubleT, CeilingFunction>);
+                             {DataType(LogicalType::kSmallInt)},
+                             &ScalarFunction::UnaryFunction<SmallIntT, SmallIntT, CeilingFunctionInt>);
     function_set_ptr->AddFunction(ceiling_int16);
 
     ScalarFunction ceiling_int32(func_name,
                              {DataType(LogicalType::kInteger)},
-                             {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<IntegerT, DoubleT, CeilingFunction>);
+                             {DataType(LogicalType::kInteger)},
+                             &ScalarFunction::UnaryFunction<IntegerT, IntegerT, CeilingFunctionInt>);
     function_set_ptr->AddFunction(ceiling_int32);
 
     ScalarFunction ceiling_int64(func_name,
                              {DataType(LogicalType::kBigInt)},
-                             {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<BigIntT, DoubleT, CeilingFunction>);
+                             {DataType(LogicalType::kBigInt)},
+                             &ScalarFunction::UnaryFunction<BigIntT, BigIntT, CeilingFunctionInt>);
     function_set_ptr->AddFunction(ceiling_int64);
 
     ScalarFunction ceiling_float(func_name,
                              {DataType(LogicalType::kFloat)},
                              {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<FloatT, DoubleT, CeilingFunction>);
+                             &ScalarFunction::UnaryFunctionWithFailure<FloatT, DoubleT, CeilingFunctionFloat>);
     function_set_ptr->AddFunction(ceiling_float);
 
     ScalarFunction ceiling_double(func_name,
                               {DataType(LogicalType::kDouble)},
                               {DataType(LogicalType::kDouble)},
-                              &ScalarFunction::UnaryFunctionWithFailure<DoubleT, DoubleT, CeilingFunction>);
+                              &ScalarFunction::UnaryFunctionWithFailure<DoubleT, DoubleT, CeilingFunctionFloat>);
     function_set_ptr->AddFunction(ceiling_double);
 
     ScalarFunction ceiling_float16(func_name,
                                {DataType(LogicalType::kFloat16)},
                                {DataType(LogicalType::kDouble)},
-                               &ScalarFunction::UnaryFunctionWithFailure<Float16T, DoubleT, CeilingFunction>);
+                               &ScalarFunction::UnaryFunctionWithFailure<Float16T, DoubleT, CeilingFunctionFloat>);
     function_set_ptr->AddFunction(ceiling_float16);
 
     ScalarFunction ceiling_bfloat16(func_name,
                                 {DataType(LogicalType::kBFloat16)},
                                 {DataType(LogicalType::kDouble)},
-                                &ScalarFunction::UnaryFunctionWithFailure<BFloat16T, DoubleT, CeilingFunction>);
+                                &ScalarFunction::UnaryFunctionWithFailure<BFloat16T, DoubleT, CeilingFunctionFloat>);
     function_set_ptr->AddFunction(ceiling_bfloat16);
 
     Catalog::AddFunctionSet(catalog_ptr.get(), function_set_ptr);
--- src/function/scalar/floor.cpp
+++ src/function/scalar/floor.cpp
@@ -17,7 +17,14 @@ import data_type;
 
 namespace infinity {
 
-struct FloorFunction {
+struct FloorFunctionInt {
+    template <typename SourceType, typename TargetType>
+    static inline void Run(SourceType value, TargetType &result) {
+        result = value;
+    }
+};
+
+struct FloorFunctionFloat {
     template <typename SourceType, typename TargetType>
     static inline bool Run(SourceType value, TargetType &result) {
         result = floor(static_cast<double>(value));
@@ -35,50 +42,50 @@ void RegisterFloorFunction(const UniquePtr<Catalog> &catalog_ptr) {
 
     ScalarFunction floor_int8(func_name,
                             {DataType(LogicalType::kTinyInt)},
-                            DataType(LogicalType::kDouble),
-                            &ScalarFunction::UnaryFunctionWithFailure<TinyIntT, DoubleT, FloorFunction>);
+                            DataType(LogicalType::kTinyInt),
+                            &ScalarFunction::UnaryFunction<TinyIntT, TinyIntT, FloorFunctionInt>);
     function_set_ptr->AddFunction(floor_int8);
 
     ScalarFunction floor_int16(func_name,
                              {DataType(LogicalType::kSmallInt)},
-                             {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<SmallIntT, DoubleT, FloorFunction>);
+                             {DataType(LogicalType::kSmallInt)},
+                             &ScalarFunction::UnaryFunction<SmallIntT, SmallIntT, FloorFunctionInt>);
     function_set_ptr->AddFunction(floor_int16);
 
     ScalarFunction floor_int32(func_name,
                              {DataType(LogicalType::kInteger)},
-                             {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<IntegerT, DoubleT, FloorFunction>);
+                             {DataType(LogicalType::kInteger)},
+                             &ScalarFunction::UnaryFunction<IntegerT, IntegerT, FloorFunctionInt>);
     function_set_ptr->AddFunction(floor_int32);
 
     ScalarFunction floor_int64(func_name,
                              {DataType(LogicalType::kBigInt)},
-                             {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<BigIntT, DoubleT, FloorFunction>);
+                             {DataType(LogicalType::kBigInt)},
+                             &ScalarFunction::UnaryFunction<BigIntT, BigIntT, FloorFunctionInt>);
     function_set_ptr->AddFunction(floor_int64);
 
     ScalarFunction floor_float(func_name,
                              {DataType(LogicalType::kFloat)},
                              {DataType(LogicalType::kDouble)},
-                             &ScalarFunction::UnaryFunctionWithFailure<FloatT, DoubleT, FloorFunction>);
+                             &ScalarFunction::UnaryFunctionWithFailure<FloatT, DoubleT, FloorFunctionFloat>);
     function_set_ptr->AddFunction(floor_float);
 
     ScalarFunction floor_double(func_name,
                               {DataType(LogicalType::kDouble)},
                               {DataType(LogicalType::kDouble)},
-                              &ScalarFunction::UnaryFunctionWithFailure<DoubleT, DoubleT, FloorFunction>);
+                              &ScalarFunction::UnaryFunctionWithFailure<DoubleT, DoubleT, FloorFunctionFloat>);
     function_set_ptr->AddFunction(floor_double);
 
     ScalarFunction floor_float16(func_name,
                                {DataType(LogicalType::kFloat16)},
                                {DataType(LogicalType::kDouble)},
-                               &ScalarFunction::UnaryFunctionWithFailure<Float16T, DoubleT, FloorFunction>);
+                               &ScalarFunction::UnaryFunctionWithFailure<Float16T, DoubleT, FloorFunctionFloat>);
     function_set_ptr->AddFunction(floor_float16);
 
     ScalarFunction floor_bfloat16(func_name,
                                 {DataType(LogicalType::kBFloat16)},
                                 {DataType(LogicalType::kDouble)},
-                                &ScalarFunction::UnaryFunctionWithFailure<BFloat16T, DoubleT, FloorFunction>);
+                                &ScalarFunction::UnaryFunctionWithFailure<BFloat16T, DoubleT, FloorFunctionFloat>);
     function_set_ptr->AddFunction(floor_bfloat16);
 
     Catalog::AddFunctionSet(catalog_ptr.get(), function_set_ptr);
