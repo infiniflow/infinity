8208459c56ef04c5875fd5f45e5f3b0aa7877e73
shenyushi <shenyushi99@qq.com>
2024-10-24 13:48:11 +0800

    Fix.

--- python/test_cluster/test_basic.py
+++ python/test_cluster/test_basic.py
@@ -1,5 +1,7 @@
 import time
-from infinity_cluster import InfinityCluster, MockedInfinityRunner
+
+import pytest
+from infinity_cluster import InfinityCluster, MockInfinityCluster
 
 
 def test_0(infinity_path: str):
@@ -12,12 +14,13 @@ def test_0(infinity_path: str):
 
     time.sleep(1)
 
-# def test_mock(infinity_path: str):
-#     cluster = MockedInfinityRunner(infinity_path)
-#     cluster.add_node("node1", "conf/leader.toml")
-#     cluster.add_node("node2", "conf/follower.toml")
+@pytest.mark.skip(reason="bug")
+def test_mock(infinity_path: str):
+    cluster = MockInfinityCluster(infinity_path)
+    cluster.add_node("node1", "conf/leader.toml")
+    cluster.add_node("node2", "conf/follower.toml")
 
-#     cluster.init_leader("node1")
-#     cluster.init_follower("node2")
+    cluster.init_leader("node1")
+    cluster.init_follower("node2")
 
-#     time.sleep(1)
+    time.sleep(1)
