3b209fc8d246fa8543c1a4c6bc5f56e8137784d6
shenyushi <shenyushi99@qq.com>
2024-10-30 14:58:18 +0800

    fix slow test.

--- src/storage/invertedindex/index_segment_reader.cpp
+++ src/storage/invertedindex/index_segment_reader.cpp
@@ -22,5 +22,5 @@ import segment_posting;
 import index_defines;
 
 namespace infinity {
-IndexSegmentReader::IndexSegmentReader() {}
+IndexSegmentReader::IndexSegmentReader(SegmentID segment_id, ChunkID chunk_id) : segment_id_(segment_id), chunk_id_(chunk_id) {}
 } // namespace infinity
