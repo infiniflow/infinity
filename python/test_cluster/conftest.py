1013b7d204a45308f93e1d2729815865c26e5e34
shenyushi <shenyushi99@qq.com>
2024-10-31 18:41:09 +0800

    Fix.

--- python/test_cluster/conftest.py
+++ python/test_cluster/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from infinity_cluster import InfinityCluster, MinioParams
 from docker_infinity_cluster import DockerInfinityCluster
@@ -37,6 +38,11 @@ def pytest_addoption(parser):
         action="store",
         default=9001,
     )
+    parser.addoption(
+        "--infinity_dir",
+        action="store",
+        default="",
+    )
 
 
 def pytest_generate_tests(metafunc):
@@ -44,18 +50,23 @@ def pytest_generate_tests(metafunc):
     minio_dir = metafunc.config.getoption("minio_dir")
     minio_port = metafunc.config.getoption("minio_port")
     minio_params = MinioParams(minio_dir, minio_port)
+
+    infinity_dir = metafunc.config.getoption("infinity_dir")
+    if len(infinity_dir) == 0:
+        infinity_dir = os.getcwd()
+    print("infinity_dir: ", infinity_dir)
     # print(metafunc.fixturenames)
 
     if metafunc.config.getoption("--docker"):
         print("Docker argument is provided")
         if "docker_cluster" in metafunc.fixturenames:
-            docker_infinity_cluster = DockerInfinityCluster(infinity_path, minio_params=minio_params)
+            docker_infinity_cluster = DockerInfinityCluster(infinity_path, minio_params=minio_params, infinity_dir=infinity_dir)
             metafunc.parametrize("docker_cluster", [docker_infinity_cluster])
     else:
         print("Docker argument is not provided")
         if "cluster" in metafunc.fixturenames:
-            infinity_cluster = InfinityCluster(infinity_path, minio_params=minio_params)
+            infinity_cluster = InfinityCluster(infinity_path, minio_params=minio_params, infinity_dir=infinity_dir)
             metafunc.parametrize("cluster", [infinity_cluster])
         elif "mock_cluster" in metafunc.fixturenames:
-            mock_infinity_cluster = MockInfinityCluster(infinity_path, minio_params=minio_params)
+            mock_infinity_cluster = MockInfinityCluster(infinity_path, minio_params=minio_params, infinity_dir=infinity_dir)
             metafunc.parametrize("mock_cluster", [mock_infinity_cluster])
