859c219a77c9422418b8cbdff2e41327c6f56798
shenyushi <shenyushi99@qq.com>
2024-11-14 10:10:54 +0800

    Add cluster test thread sanitizer to slow test.

--- scripts/collect_thread_sanitizer_log.py
+++ scripts/collect_thread_sanitizer_log.py
@@ -5,9 +5,15 @@ import string
 
 
 def collect_log(stderror_path, output_dir):
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
     random_name = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
     print(f"Random log file name: {random_name}")
     if not os.path.isfile(stderror_path):
         print("Error: stderror file not found")
     else:
-        shutil.copy(stderror_path, f"{output_dir}/{random_name}_sanitizer_stderror.log")
+        target_path = f"{output_dir}/{random_name}_sanitizer_stderror.log"
+        print(f"stderror path: {stderror_path}")
+        print(f"target path: {target_path}")
+        shutil.copy(stderror_path, target_path)
