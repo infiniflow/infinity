607cab1a0e3ce7f6ae5db2df093f9171485844c1
Jin Hai <haijin.chn@gmail.com>
2024-11-17 12:17:26 +0800

    Update version info to 0.5.0.dev3
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- benchmark/remote_infinity/remote_query_benchmark.cpp
+++ benchmark/remote_infinity/remote_query_benchmark.cpp
@@ -51,7 +51,7 @@ struct InfinityClient {
         transport->open();
         CommonResponse response;
         ConnectRequest request;
-        request.__set_client_version(24); // 0.5.0.dev2
+        request.__set_client_version(25); // 0.5.0.dev3
         client->Connect(response, request);
         session_id = response.session_id;
     }
