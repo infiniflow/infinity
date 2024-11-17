65bf2084f48c33abb7c8fa34b94731f1a1bbdee1
Jin Hai <haijin.chn@gmail.com>
2024-11-17 14:36:43 +0800

    Update version info to 0.5.0.dev3 (#2255)
    ### What problem does this PR solve?
    
    Update from 0.5.0-dev2 to 0.5.0-dev3.
    
    ### Type of change
    
    - [x] Documentation Update
    - [x] Refactoring
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- python/infinity_sdk/infinity/remote_thrift/client.py
+++ python/infinity_sdk/infinity/remote_thrift/client.py
@@ -71,7 +71,8 @@ class ThriftInfinityClient:
         # version: 0.4.0.dev5 and 0.4.0, client_version: 22
         # version: 0.5.0.dev1, client_version: 23
         # version: 0.5.0.dev2, client_version: 24
-        res = self.client.Connect(ConnectRequest(client_version=24))  # 0.5.0.dev2
+        # version: 0.5.0.dev3, client_version: 25
+        res = self.client.Connect(ConnectRequest(client_version=25))  # 0.5.0.dev3
         if res.error_code != 0:
             raise InfinityException(res.error_code, res.error_msg)
         self.session_id = res.session_id
