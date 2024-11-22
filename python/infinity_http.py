4ed460b7fe47a06cd23b12043acba5340d1f800d
Jin Hai <haijin.chn@gmail.com>
2024-11-22 14:19:54 +0800

    More cluster test cases (#2278)
    ### What problem does this PR solve?
    
    1. test admin cases
    
    ### Type of change
    
    - [x] Test cases
    
    ---------
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- python/infinity_http.py
+++ python/infinity_http.py
@@ -270,7 +270,7 @@ class infinity_http:
         return database_result(nodes=r.json()["nodes"])
 
     def remove_node(self, node_name):
-        url = f"admin/node/{node_name}" 
+        url = f"admin/node/{node_name}"
         h = self.net.set_up_header(["accept"])
         r = self.net.request(url, "delete", h, {})
         self.net.raise_exception(r)
@@ -291,7 +291,7 @@ class infinity_http:
         r = self.net.request(url, "get", h, {})
         self.net.raise_exception(r)
         # print(r.json())
-        return database_result(node_role=r.json()["node"]["role"])
+        return database_result(node_role=r.json()["node"]["role"], node_status=r.json()["node"]["status"])
 
     def show_global_variables(self):
         url = "variables/global"
@@ -315,13 +315,40 @@ class infinity_http:
         self.net.raise_exception(r)
         return database_result()
 
-    def show_current_node(self):
-        url = f"admin/node/current"
+    def show_admin_variables(self):
+        url = f"admin/variables"
         h = self.net.set_up_header(["accept"])
         r = self.net.request(url, "get", h, {})
         self.net.raise_exception(r)
-        # print(r.json())
-        return database_result(node_role=r.json()["node"]["role"])
+        return database_result(data=r.json()["role"])
+
+    def show_admin_configs(self):
+        url = f"admin/configs"
+        h = self.net.set_up_header(["accept"])
+        r = self.net.request(url, "get", h, {})
+        self.net.raise_exception(r)
+        return database_result(data=r.json())
+
+    def show_admin_catalogs(self):
+        url = f"admin/catalogs"
+        h = self.net.set_up_header(["accept"])
+        r = self.net.request(url, "get", h, {})
+        self.net.raise_exception(r)
+        return database_result(data=r.json())
+
+    def show_admin_logs(self):
+        url = f"admin/logs"
+        h = self.net.set_up_header(["accept"])
+        r = self.net.request(url, "get", h, {})
+        self.net.raise_exception(r)
+        return database_result(data=r.json())
+
+    def list_nodes(self):
+        url = f"admin/nodes"
+        h = self.net.set_up_header(["accept"])
+        r = self.net.request(url, "get", h, {})
+        self.net.raise_exception(r)
+        return database_result(data=r.json()["nodes"])
 
 
 ####################3####################3####################3####################3####################3####################3####################3####################3
@@ -927,10 +954,11 @@ class table_http_result:
     def to_arrow(self):
         return pa.Table.from_pandas(self.to_df())
 
+
 @dataclass
 class database_result():
     def __init__(self, list=[], database_name: str = "", error_code=ErrorCode.OK, columns=[], index_list=[],
-                 node_name="", node_role="", node_status="", index_comment=None, deleted_rows=0, nodes=[]):
+                 node_name="", node_role="", node_status="", index_comment=None, deleted_rows=0, data={}, nodes=[]):
         self.db_names = list
         self.database_name = database_name  # get database
         self.error_code = error_code
@@ -941,6 +969,7 @@ class database_result():
         self.node_status = node_status
         self.index_comment = index_comment
         self.deleted_rows = deleted_rows
+        self.data = data
         self.nodes = nodes
 
 
