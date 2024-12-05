0c75b5685a96dcf4f9c28672a699b18c27c4c960
shen yushi <shenyushi99@qq.com>
2024-12-05 10:27:26 +0800

    Fix bmp. (#2325)
    ### What problem does this PR solve?
    
    Fix bmp index algorithm.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    - [x] Test cases

--- src/unit_test/storage/knnindex/knn_sparse/test_bmp_index.cpp
+++ src/unit_test/storage/knnindex/knn_sparse/test_bmp_index.cpp
@@ -58,7 +58,7 @@ protected:
             {
                 SparseMatrixIter iter(query_set);
                 SparseVecRef vec = iter.val();
-                auto [indices, scores] = index.SearchKnn(vec, 0/*topk*/, options);
+                auto [indices, scores] = index.SearchKnn(vec, 0 /*topk*/, options);
                 EXPECT_EQ(indices.size(), 0);
                 EXPECT_EQ(scores.size(), 0);
             }
@@ -80,7 +80,7 @@ protected:
                 // SparseTestUtil<DataType, IdxType>::PrintQuery(query_id, gt_indices, gt_scores, gt_size, indices, scores);
                 // std::cout << fmt::format("accuracy: {}\n", (f32)hit / total);
             }
-//            std::cout << fmt::format("hit: {}, total: {}\n", hit_all, total_all);
+            //            std::cout << fmt::format("hit: {}, total: {}\n", hit_all, total_all);
             if (hit_all < total_all * accuracy_all) {
                 EXPECT_TRUE(false);
             }
@@ -133,4 +133,54 @@ TEST_F(BMPIndexTest, test1) {
         u32 block_size = 8;
         TestFunc<f64, i32, BMPCompressType::kCompressed>(block_size);
     }
+}
+
+TEST_F(BMPIndexTest, test2) {
+    using BMPAlg = BMPAlg<f32, i32, BMPCompressType::kCompressed>;
+
+    u32 ncol = 2;
+    u32 block_size = 2;
+
+    u32 topk = 5;
+
+    BmpSearchOptions options;
+    options.use_lock_ = false;
+
+    Vector<i32> query_idx = {0, 1};
+    Vector<f32> query_data = {1.0, 1.0};
+    SparseVecRef query(query_idx.size(), query_idx.data(), query_data.data());
+
+    Vector<i32> vec1_idx = {0};
+    Vector<f32> vec1_data = {3.0};
+    SparseVecRef vec1(vec1_idx.size(), vec1_idx.data(), vec1_data.data());
+
+    Vector<i32> vec2_idx = {1};
+    Vector<f32> vec2_data = {1.0};
+    SparseVecRef vec2(vec2_idx.size(), vec2_idx.data(), vec2_data.data());
+
+    Vector<i32> vec3_idx = {0, 1};
+    Vector<f32> vec3_data = {1.0, 1.0};
+    SparseVecRef vec3(vec3_idx.size(), vec3_idx.data(), vec3_data.data());
+
+    BMPAlg index(ncol, block_size);
+    index.AddDoc(vec1, 0);
+    index.AddDoc(vec2, 1);
+    index.AddDoc(vec3, 2);
+    index.AddDoc(vec3, 3);
+    index.AddDoc(vec3, 4);
+    index.AddDoc(vec3, 5);
+
+    [[maybe_unused]] auto [indices, scores] = index.SearchKnn(query, topk, options);
+    ASSERT_EQ(indices.size(), topk);
+    ASSERT_EQ(indices[0], 0);
+    ASSERT_EQ(indices[1], 2);
+    ASSERT_EQ(indices[2], 3);
+    ASSERT_EQ(indices[3], 4);
+    ASSERT_EQ(indices[4], 5);
+    ASSERT_EQ(scores.size(), topk);
+    ASSERT_EQ(scores[0], 3.0);
+    ASSERT_EQ(scores[1], 2.0);
+    ASSERT_EQ(scores[2], 2.0);
+    ASSERT_EQ(scores[3], 2.0);
+    ASSERT_EQ(scores[4], 2.0);
 }
