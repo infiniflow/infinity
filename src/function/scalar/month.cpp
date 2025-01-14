2f3b24820a26ea8cb77fe1f02a50e3ddfb9dbc5d
kche0169 <shiratakekanpakuji@gmail.com>
2025-01-14 15:49:25 +0800

    add month function

---
+++ src/function/scalar/month.cpp
@@ -0,0 +1,62 @@
+// Copyright(C) 2025 InfiniFlow, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "type/internal_types.h"
+#include "type/logical_type.h"
+module;
+module month;
+import stl;
+import catalog;
+import status;
+import logical_type;
+import infinity_exception;
+import scalar_function;
+import scalar_function_set;
+import third_party;
+import internal_types;
+import data_type;
+import column_vector;
+
+namespace infinity {
+
+struct MonthFunction {
+    template <typename TA, typename TB>
+    static inline void Run(TA left, TB &result) {
+        Status status = Status::NotSupport("Not implemented");
+        RecoverableError(status);
+    }
+
+};
+
+template <>
+inline void MonthFunction::Run(DateT left, BigIntT &result) {
+    result = DateT::GetDatePart(left, TimeUnit::kMonth);
+}
+
+void RegisterMonthFunction(const UniquePtr<Catalog> &catalog_ptr) {
+    String func_name = "month";
+
+    SharedPtr<ScalarFunctionSet> function_set_ptr = MakeShared<ScalarFunctionSet>(func_name);
+
+    ScalarFunction month_function(func_name,
+                                  {DataType(LogicalType::kDate)},
+                                  {DataType(LogicalType::kBigInt)},
+                                  &ScalarFunction::UnaryFunctionVarlenToVarlen<DateT, BigIntT, MonthFunction>);
+    function_set_ptr->AddFunction(month_function);
+
+
+    Catalog::AddFunctionSet(catalog_ptr.get(), function_set_ptr);
+}
+
+} // namespace infinity
