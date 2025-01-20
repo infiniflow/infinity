f4fb9a07896466601239bad3756b8a25f36981d3
kche0169 <shiratakekanpakuji@gmail.com>
2025-01-20 10:51:22 +0800

    fix header

--- src/function/scalar/day_of_week.cpp
+++ src/function/scalar/day_of_week.cpp
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 module;
-#include <__chrono/weekday.h>
-#include <cstdlib>
+#include <chrono>
 module day_of_week;
 import stl;
 import catalog;
