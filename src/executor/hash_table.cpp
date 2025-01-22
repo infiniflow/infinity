cfec92130287da1fb88db8913a558804c3f1598b
shenyushi <shenyushi99@qq.com>
2025-01-22 10:03:59 +0800

    Fix varchar group by.

--- src/executor/hash_table.cpp
+++ src/executor/hash_table.cpp
@@ -38,88 +38,65 @@ void HashTable::Init(const Vector<SharedPtr<DataType>> &types) {
             case LogicalType::kSmallInt:
             case LogicalType::kInteger:
             case LogicalType::kBigInt:
-            case LogicalType::kHugeInt:
             case LogicalType::kFloat:
             case LogicalType::kDouble:
-            case LogicalType::kDecimal:
-            case LogicalType::kVarchar:
             case LogicalType::kDate:
             case LogicalType::kTime:
-            case LogicalType::kDateTime:
-            case LogicalType::kMixed: {
+            case LogicalType::kDateTime: {
+                SizeT type_size = data_type.Size();
+                key_size_ += type_size;
                 break; // All these type can be hashed.
             }
-            case LogicalType::kTimestamp:
-            case LogicalType::kInterval:
-            case LogicalType::kArray:
-            case LogicalType::kTuple:
-            case LogicalType::kPoint:
-            case LogicalType::kLine:
-            case LogicalType::kLineSeg:
-            case LogicalType::kBox:
-                //            case kPath:
-                //            case kPolygon:
-            case LogicalType::kCircle:
-                //            case kBitmap:
-            case LogicalType::kUuid:
-                //            case kBlob:
-            case LogicalType::kEmbedding:
-            case LogicalType::kRowID:
-            case LogicalType::kNull:
-            case LogicalType::kMissing:
-            case LogicalType::kInvalid:
+            case LogicalType::kVarchar: {
+                key_size_ = 0;
+                break; // Varchar can be hashed.
+            }
             default: {
                 RecoverableError(Status::NotSupport(fmt::format("Attempt to construct hash key for type: {}", data_type.ToString())));
             }
         }
-
-        SizeT type_size = data_type.Size();
-        key_size_ += type_size;
+        if (key_size_ == 0) {
+            break;
+        }
     }
 
-    // Key layout: col1\0col2\0col3\0.
-    key_size_ += type_count;
+    if (key_size_) {
+        // Key layout: col1\0col2\0col3\0.
+        key_size_ += type_count;
+    }
 }
 
 void HashTable::Append(const Vector<SharedPtr<ColumnVector>> &columns, SizeT block_id, SizeT row_count) {
-    UniquePtr<char[]> hash_key = MakeUnique<char[]>(key_size_);
     SizeT column_count = columns.size();
+    String hash_key;
+    if (key_size_) {
+        hash_key.reserve(key_size_);
+    }
     for (SizeT row_id = 0; row_id < row_count; ++row_id) {
-        std::memset(hash_key.get(), 0, key_size_);
-        SizeT offset = 0;
-
+        hash_key.clear();
         for (SizeT column_id = 0; column_id < column_count; ++column_id) {
-            char *target_ptr = hash_key.get() + offset;
             if (!columns[column_id]->nulls_ptr_->IsTrue(row_id)) {
-                *(target_ptr) = '\0';
-                offset += 2;
+                hash_key += "\0\0";
                 continue;
             }
 
             const DataType &data_type = *types_[column_id];
-            if (data_type.type() == LogicalType::kMixed) {
-                // Only float/boolean/integer/string can be built as hash key. Array/Tuple will be treated as null
-                RecoverableError(Status::NotSupport("Attempt to construct hash key for heterogeneous type"));
-            }
 
             if (data_type.type() == LogicalType::kVarchar) {
-                VarcharT *vchar_ptr = &((VarcharT *)(columns[column_id]->data()))[row_id];
-                if (vchar_ptr->IsInlined()) {
-                    std::memcpy(target_ptr, vchar_ptr->short_.data_, vchar_ptr->length_);
-                    offset += (vchar_ptr->length_ + 1);
-                } else {
-                    std::memcpy(target_ptr, vchar_ptr->vector_.prefix_, VARCHAR_PREFIX_LEN);
-                    offset += (VARCHAR_PREFIX_LEN + 1);
-                }
+                Span<const char> text = columns[column_id]->GetVarchar(row_id);
+                hash_key.append(text.begin(), text.end());
             } else {
                 SizeT type_size = types_[column_id]->Size();
-                std::memcpy(target_ptr, columns[column_id]->data() + type_size * row_id, type_size);
-                offset += (type_size + 1);
+                Span<const char> binary(reinterpret_cast<const char *>(columns[column_id]->data() + type_size * row_id), type_size);
+                hash_key.append(binary.begin(), binary.end());
             }
+            hash_key += '\0';
+        }
+        if (key_size_ && hash_key.size() != key_size_) {
+            UnrecoverableError(fmt::format("Hash key size mismatch: {} vs {}", hash_key.size(), key_size_));
         }
 
-        String key(hash_key.get(), key_size_);
-        hash_table_[key][block_id].emplace_back(row_id);
+        hash_table_[std::move(hash_key)][block_id].emplace_back(row_id);
     }
 }
 
