6ee365224a7b5106c03ec3e8ed0d8fd0a623f3f2
joy-joy-joy-joy-joy-joy-joy <97589642+joy-joy-joy-joy-joy-joy-joy@users.noreply.github.com>
2025-04-15 16:18:50 +0800

    add table_index

--- src/unit_test/storage/new_catalog/transform_meta.cpp
+++ src/unit_test/storage/new_catalog/transform_meta.cpp
@@ -30,6 +30,7 @@ import catalog;
 import new_txn;
 import extra_ddl_info;
 import table_meeta;
+import table_index_meeta;
 
 using namespace infinity;
 
@@ -228,6 +229,54 @@ TEST_P(TransformMeta, table_meta_transform_00) {
         //                     Optional<TableMeeta> &table_meta,
         //                     String *table_key = nullptr);
         EXPECT_TRUE(status.ok());
+    }
+
+    status = new_txn_mgr->CommitTxn(txn);
+    EXPECT_TRUE(status.ok());
+
+    UnInit();
+}
+
+TEST_P(TransformMeta, table_meta_transform_01) {
+    UniquePtr<Config> config_ptr = MakeUnique<Config>();
+    Status status = config_ptr->Init(config_path, nullptr);
+    EXPECT_TRUE(status.ok());
+    UniquePtr<KVStore> kv_store_ptr = MakeUnique<KVStore>();
+    status = kv_store_ptr->Init(config_ptr->CatalogDir());
+    EXPECT_TRUE(status.ok());
+    UniquePtr<NewCatalog> new_catalog_ptr = MakeUnique<NewCatalog>(kv_store_ptr.get());
+
+    String full_ckp_path = String(test_data_path()) + "/json/table_meta_01.json";
+    Vector<String> delta_ckp_path_array;
+    new_catalog_ptr->TransformCatalog(config_ptr.get(), full_ckp_path, delta_ckp_path_array);
+
+    status = kv_store_ptr->Flush();
+    EXPECT_TRUE(status.ok());
+    kv_store_ptr->Uninit();
+    kv_store_ptr.reset();
+    new_catalog_ptr.reset();
+
+    Init();
+
+    NewTxnManager *new_txn_mgr = InfinityContext::instance().storage()->new_txn_manager();
+    auto *txn = new_txn_mgr->BeginTxn(MakeUnique<String>("check db"), TransactionType::kNormal);
+    {
+        Optional<DBMeeta> db_meta;
+        Optional<TableMeeta> table_meta;
+        // status = txn->GetTableMeta("db1", "test_table", db_meta, table_meta);
+        // // Status GetTableMeta(const String &db_name,
+        // //                     const String &table_name,
+        // //                     Optional<DBMeeta> &db_meta,
+        // //                     Optional<TableMeeta> &table_meta,
+        // //                     String *table_key = nullptr);
+        // EXPECT_TRUE(status.ok());
+        Optional<TableIndexMeeta> table_index_meta;
+        if (table_meta.has_value()) {
+            status = txn->GetTableIndexMeta("idx_test_table_age", table_meta.value(), table_index_meta);
+            EXPECT_TRUE(status.ok());
+        }
+
+
     }
 
     status = new_txn_mgr->CommitTxn(txn);
