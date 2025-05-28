098aec2ad5e6e5d3a7875f65252ec83030f60139
Zhichang Yu <yuzhichang@gmail.com>
2025-05-28 15:40:28 +0800

    Fixed MemoryIndexer::Dump

--- src/storage/invertedindex/memory_indexer.cpp
+++ src/storage/invertedindex/memory_indexer.cpp
@@ -396,6 +396,7 @@ void MemoryIndexer::Dump(bool offline, bool spill) {
 
     Vector<u32> &column_length_array = column_lengths_.UnsafeVec();
     file_handle->Append(&column_length_array[0], sizeof(column_length_array[0]) * column_length_array.size());
+    file_handle->Sync();
     if (use_object_cache) {
         PersistResultHandler handler(pm);
         PersistWriteResult result1 = pm->Persist(posting_file, tmp_posting_file, false);
