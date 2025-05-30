21a13e5ab9b2b8f789534fd7ddf3a75859c68f30
Ling Qin <qinling0210@163.com>
2025-05-30 20:48:11 +0800

    Restore test_alter.py

--- python/restart_test/test_alter.py
+++ python/restart_test/test_alter.py
@@ -12,6 +12,71 @@ import infinity.index as index
 
 
 class TestAlter:
+    def test_alter_simple(self, infinity_runner: InfinityRunner):
+        table_name = "test_alter1"
+        config = "test/data/config/restart_test/test_alter/1.toml"
+
+        infinity_runner.clear()
+        uri = common_values.TEST_LOCAL_HOST
+
+        decorator = infinity_runner_decorator_factory(config, uri, infinity_runner)
+
+        @decorator
+        def part1(infinity_obj):
+            db_obj = infinity_obj.get_database("default_db")
+            db_obj.drop_table(table_name, ConflictType.Ignore)
+            table_obj = db_obj.create_table(
+                table_name,
+                {
+                    "c1": {"type": "int"},
+                    "c2": {"type": "int"},
+                    "c3": {"type": "varchar"},
+                },
+            )
+            res = table_obj.create_index(
+                "index1", index.IndexInfo("c3", index.IndexType.FullText)
+            )
+            assert res.error_code == ErrorCode.OK
+
+            table_obj.insert([{"c1": 1, "c2": 2, "c3": "test"}])
+
+            res = table_obj.add_columns(
+                {"c4": {"type": "varchar", "default": "default"}}
+            )
+            assert res.error_code == ErrorCode.OK
+
+            res = table_obj.drop_columns(["c2"])
+            assert res.error_code == ErrorCode.OK
+
+            table_obj.insert([{"c1": 2, "c3": "test", "c4": "test2"}])
+
+        @decorator
+        def part2(infinity_obj):
+            db_obj = infinity_obj.get_database("default_db")
+            table_obj = db_obj.get_table(table_name)
+
+            res, extra_result = table_obj.output(["*"]).to_df()
+            pd.testing.assert_frame_equal(
+                res,
+                pd.DataFrame(
+                    {
+                        "c1": [1, 2],
+                        "c3": ["test", "test"],
+                        "c4": ["default", "test2"],
+                    }
+                ).astype(
+                    {
+                        "c1": dtype("int32"),
+                        "c3": dtype("object"),
+                        "c4": dtype("object"),
+                    }
+                ),
+            )
+
+            db_obj.drop_table(table_name)
+
+        part1()
+        part2()
 
     @pytest.mark.parametrize(
         "flush, flush_mid",
@@ -77,6 +142,39 @@ class TestAlter:
 
             db_obj = infinity_obj.get_database("default_db")
             table_obj = db_obj.get_table(table_name)
+            res, extra_result = table_obj.output(["*"]).to_df()
+            pd.testing.assert_frame_equal(
+                res,
+                pd.DataFrame(
+                    {
+                        "c1": [1, 2],
+                        "c3": ["test", "test2"],
+                        "c4": ["tttt", "tttt2"],
+                        "c2": [0.0, 1.0],
+                        "c5": [0, 0],
+                    }
+                ).astype(
+                    {
+                        "c1": dtype("int32"),
+                        "c3": dtype("object"),
+                        "c4": dtype("object"),
+                        "c2": dtype("float32"),
+                        "c5": dtype("int32"),
+                    }
+                ),
+            )
+
+            infinity_obj.cleanup()
+
+            dropped_column_dirs = pathlib.Path("/var/infinity/data").rglob("1.col")
+            assert len(list(dropped_column_dirs)) == 0
+
+            res = table_obj.list_indexes()
+            assert len(res.index_names) == 1
+
+            data, _, _ = table_obj.output(["*"]).to_result()
+            assert len(data) == 5
+            assert len(data["c1"]) == 2
 
             data_dict, _, _ = (
                 table_obj.output(["c1"])
