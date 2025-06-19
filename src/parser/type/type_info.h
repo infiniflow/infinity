504113b883932b3859ed7666a05f5a045e31168c
Jin Hai <haijin.chn@gmail.com>
2025-06-19 10:03:45 +0800

    Add rapidjson
    Signed-off-by: Long <2262328655@qq.com>

--- src/parser/type/type_info.h
+++ src/parser/type/type_info.h
@@ -16,6 +16,9 @@
 
 #include "json.hpp"
 #include "simdjson.h"
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 #include <cstdint>
 #include <string>
 
