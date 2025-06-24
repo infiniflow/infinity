0e9de21ed2b7032ed464b867aca3148b9d158f87
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2025-06-24 12:27:41 +0800

    Fix compile under Mac/X86

--- src/unit_test/storage/knnindex/knn_hnsw/test_hnsw_handler.cpp
+++ src/unit_test/storage/knnindex/knn_hnsw/test_hnsw_handler.cpp
@@ -146,8 +146,8 @@ TEST_F(HnswHandlerTest, test_memory) {
 
         /// test interface
         auto [mem_usage, vec_num] = hnsw_handler->GetInfo();
-        printf("hnsw_handler::GetInfo() -> %lu, %lu\n", mem_usage, vec_num);
-        printf("hnsw_handler::GetSizeInBytes() -> %lu\n", hnsw_handler->GetSizeInBytes());
+        printf("hnsw_handler::GetInfo() -> %llu, %llu\n", mem_usage, vec_num);
+        printf("hnsw_handler::GetSizeInBytes() -> %llu\n", hnsw_handler->GetSizeInBytes());
         SearchHnswHandler(hnsw_handler.get());
 
         /// test save
