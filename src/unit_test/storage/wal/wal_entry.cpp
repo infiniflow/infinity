866e35a4bde98f628fbcbf701f993a6181c05055
Jin Hai <haijin.chn@gmail.com>
2025-06-27 14:30:32 +0800

    Fix compile
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- src/unit_test/storage/wal/wal_entry.cpp
+++ src/unit_test/storage/wal/wal_entry.cpp
@@ -244,7 +244,7 @@ TEST_F(WalEntryTest, ReadWrite) {
         Vector<RowID> row_ids = {RowID(1, 3)};
         entry->cmds_.push_back(MakeShared<WalCmdDelete>("db1", "tbl1", row_ids));
     }
-    entry->cmds_.push_back(MakeShared<WalCmdCheckpoint>(int64_t(123), true, "catalog", String("META_123.full.json")));
+    entry->cmds_.push_back(MakeShared<WalCmdCheckpoint>(int64_t(123), "catalog", String("META_123.full.json")));
     {
         Vector<WalSegmentInfo> new_segment_infos(3, MakeSegmentInfo(1, 0, 2));
         entry->cmds_.push_back(MakeShared<WalCmdCompact>("db1", "tbl1", std::move(new_segment_infos), Vector<SegmentID>{0, 1, 2}));
