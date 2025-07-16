a1d4ba7865bb7199aa1ba30b8298e8a1e6758321
Jin Hai <haijin.chn@gmail.com>
2025-07-16 14:54:55 +0800

    Update UT
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- src/unit_test/storage/new_catalog/new_txn_replay_with_ckp.cpp
+++ src/unit_test/storage/new_catalog/new_txn_replay_with_ckp.cpp
@@ -452,3 +452,106 @@ TEST_P(TxnReplayExceptionTest, test_replay_compact) {
         EXPECT_TRUE(status.ok());
     }
 }
+
+TEST_P(TxnReplayExceptionTest, test_replay_insert) {
+    using namespace infinity;
+
+    SharedPtr<String> db_name = std::make_shared<String>("db1");
+    auto column_def1 = std::make_shared<ColumnDef>(0, std::make_shared<DataType>(LogicalType::kInteger), "col1", std::set<ConstraintType>());
+    auto column_def2 = std::make_shared<ColumnDef>(1, std::make_shared<DataType>(LogicalType::kVarchar), "col2", std::set<ConstraintType>());
+    auto table_name = std::make_shared<std::string>("tb1");
+    auto new_table_name = std::make_shared<std::string>("tb2");
+    auto table_def = TableDef::Make(db_name, table_name, MakeShared<String>(), {column_def1, column_def2});
+    SharedPtr<String> index_name = MakeShared<String>("index_name");
+    SharedPtr<String> index_comment_ptr = MakeShared<String>("index_comment");
+
+    {
+        auto *txn1 = new_txn_mgr->BeginTxn(MakeUnique<String>("create db"), TransactionType::kNormal);
+        Status status = txn1->CreateDatabase(*db_name, ConflictType::kError, MakeShared<String>());
+        EXPECT_TRUE(status.ok());
+        status = new_txn_mgr->CommitTxn(txn1);
+        EXPECT_TRUE(status.ok());
+    }
+
+    {
+        auto *txn = new_txn_mgr->BeginTxn(MakeUnique<String>("create table"), TransactionType::kNormal);
+        Status status = txn->CreateTable(*db_name, table_def, ConflictType::kError);
+        EXPECT_TRUE(status.ok());
+        status = new_txn_mgr->CommitTxn(txn);
+        EXPECT_TRUE(status.ok());
+    }
+
+    auto make_input_block = [&](const Value &v1, const Value &v2, SizeT row_cnt) {
+        auto make_column = [&](SharedPtr<ColumnDef> &column_def, const Value &v) {
+            auto col = ColumnVector::Make(column_def->type());
+            col->Initialize();
+            for (SizeT i = 0; i < row_cnt; ++i) {
+                col->AppendValue(v);
+            }
+            return col;
+        };
+        auto input_block = MakeShared<DataBlock>();
+        {
+            auto col1 = make_column(column_def1, v1);
+            input_block->InsertVector(col1, 0);
+        }
+        {
+            auto col2 = make_column(column_def2, v2);
+            input_block->InsertVector(col2, 1);
+        }
+        input_block->Finalize();
+        return input_block;
+    };
+
+    auto append = [&] {
+        auto *txn = new_txn_mgr->BeginTxn(MakeUnique<String>("append"), TransactionType::kNormal);
+        auto input_block = make_input_block(Value::MakeInt(1), Value::MakeVarchar("abcdefghijklmnopqrstuvwxyz"), 4);
+        Status status = txn->Append(*db_name, *table_name, input_block);
+        EXPECT_TRUE(status.ok());
+        status = new_txn_mgr->CommitTxn(txn);
+        EXPECT_TRUE(status.ok());
+    };
+    append();
+    append();
+
+    {
+        auto *txn = new_txn_mgr->BeginTxn(MakeUnique<String>("delete"), TransactionType::kNormal);
+        Vector<RowID> row_ids;
+        row_ids.push_back(RowID(0, 1));
+        row_ids.push_back(RowID(0, 3));
+        Status status = txn->Delete(*db_name, *table_name, row_ids);
+        EXPECT_TRUE(status.ok());
+        status = new_txn_mgr->CommitTxn(txn);
+        EXPECT_TRUE(status.ok());
+    }
+
+    {
+        auto *txn = new_txn_mgr->BeginTxn(MakeUnique<String>("drop table"), TransactionType::kNormal);
+        Status status = txn->DropTable(*db_name, *table_name, ConflictType::kError);
+        EXPECT_TRUE(status.ok());
+        status = new_txn_mgr->CommitTxn(txn);
+        EXPECT_TRUE(status.ok());
+    }
+
+    {
+        auto *txn1 = new_txn_mgr->BeginTxn(MakeUnique<String>("drop db"), TransactionType::kNormal);
+        Status status = txn1->DropDatabase(*db_name, ConflictType::kError);
+        EXPECT_TRUE(status.ok());
+        status = new_txn_mgr->CommitTxn(txn1);
+        EXPECT_TRUE(status.ok());
+    }
+
+    new_txn_mgr->kv_store()->Flush();
+    RestartTxnMgr();
+
+    {
+        auto *txn2 = new_txn_mgr->BeginTxn(MakeUnique<String>("check db"), TransactionType::kNormal);
+        Vector<String> db_names;
+        Status status = txn2->ListDatabase(db_names);
+        EXPECT_TRUE(status.ok());
+        EXPECT_EQ(Set<String>(db_names.begin(), db_names.end()), Set<String>({"default_db"}));
+        status = new_txn_mgr->CommitTxn(txn2);
+        EXPECT_TRUE(status.ok());
+    }
+}
+
