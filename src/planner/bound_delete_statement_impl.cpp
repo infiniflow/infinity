a550a44ebf0544afab8b64ddb260f1fb7dbdf24a
noob <yixiao121314@outlook.com>
2025-07-16 15:21:42 +0800

    Replace draft
    Signed-off-by: noob <yixiao121314@outlook.com>

---
+++ src/planner/bound_delete_statement_impl.cpp
@@ -0,0 +1,155 @@
+// Copyright(C) 2023 InfiniFlow, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+module;
+
+#include <vector>
+
+module infinity_core:bound_delete_statement.impl;
+
+import :bound_delete_statement;
+
+import :bound_statement;
+import :table_ref;
+import :base_expression;
+import :bind_context;
+import :logical_node;
+import :query_context;
+import :stl;
+import :infinity_exception;
+import :status;
+import :base_table_ref;
+import :subquery_table_ref;
+import :expression_transformer;
+import :expression_type;
+import :subquery_expression;
+import :query_context;
+import :logical_table_scan;
+import :logical_filter;
+import :logical_delete;
+import :subquery_unnest;
+
+import :conjunction_expression;
+import table_reference;
+import :logger;
+
+namespace infinity {
+
+SharedPtr<LogicalNode> BoundDeleteStatement::BuildPlan(QueryContext *query_context) {
+    const SharedPtr<BindContext> &bind_context = this->bind_context_;
+    SharedPtr<LogicalNode> table_scan_node = BuildFrom(table_ref_ptr_, query_context, bind_context);
+
+    auto base_table_ref = std::static_pointer_cast<BaseTableRef>(table_ref_ptr_);
+    SharedPtr<LogicalNode> delete_node = MakeShared<LogicalDelete>(bind_context->GetNewLogicalNodeId(), base_table_ref->table_info_);
+
+    if (!where_conditions_.empty()) {
+        SharedPtr<LogicalNode> filter_node = BuildFilter(table_scan_node, where_conditions_, query_context, bind_context);
+        filter_node->set_left_node(table_scan_node);
+        delete_node->set_left_node(filter_node);
+    } else {
+        delete_node->set_left_node(table_scan_node);
+    }
+    return delete_node;
+}
+
+SharedPtr<LogicalNode>
+BoundDeleteStatement::BuildFrom(SharedPtr<TableRef> &table_ref, QueryContext *query_context, const SharedPtr<BindContext> &bind_context) {
+    if (table_ref.get() == nullptr || table_ref->type_ != TableRefType::kTable) {
+        String error_message = "Unsupported!";
+        UnrecoverableError(error_message);
+    }
+    return BuildBaseTable(table_ref, query_context, bind_context);
+}
+
+SharedPtr<LogicalNode>
+BoundDeleteStatement::BuildBaseTable(SharedPtr<TableRef> &table_ref, QueryContext *, const SharedPtr<BindContext> &bind_context) {
+    // SharedPtr<BaseTableRef> base_table_ref
+    auto base_table_ref = std::static_pointer_cast<BaseTableRef>(table_ref);
+
+    SharedPtr<LogicalTableScan> table_scan_node = MakeShared<LogicalTableScan>(bind_context->GetNewLogicalNodeId(), base_table_ref, true);
+    return table_scan_node;
+}
+
+SharedPtr<LogicalNode> BoundDeleteStatement::BuildFilter(SharedPtr<LogicalNode> &root,
+                                                         Vector<SharedPtr<BaseExpression>> &conditions,
+                                                         QueryContext *query_context,
+                                                         const SharedPtr<BindContext> &bind_context) {
+    for (auto &cond : conditions) {
+        // 1. Go through all the expression to find subquery
+        //        VisitExpression(cond,
+        //                        [&](SharedPtr<BaseExpression> &expr) {
+        //                            SubqueryUnnest::UnnestSubqueries(expr, root, bind_context);
+        //                        });
+        BuildSubquery(root, cond, query_context, bind_context);
+    }
+
+    // SharedPtr<BaseExpression> filter_expr
+    auto filter_expr = ComposeExpressionWithDelimiter(conditions, ConjunctionType::kAnd);
+
+    // SharedPtr<LogicalFilter> filter
+    auto filter = MakeShared<LogicalFilter>(bind_context->GetNewLogicalNodeId(), filter_expr);
+
+    return filter;
+}
+
+void BoundDeleteStatement::BuildSubquery(SharedPtr<LogicalNode> &root,
+                                         SharedPtr<BaseExpression> &condition,
+                                         QueryContext *query_context,
+                                         const SharedPtr<BindContext> &bind_context) {
+    if (condition.get() == nullptr) {
+        return;
+    }
+
+    VisitExpression(condition, [&](SharedPtr<BaseExpression> &expr) { BuildSubquery(root, expr, query_context, bind_context); });
+
+    if (condition->type() == ExpressionType::kSubQuery) {
+        if (building_subquery_) {
+            // nested subquery
+            Status status = Status::SyntaxError("Nested subquery detected");
+            RecoverableError(status);
+        }
+        condition = UnnestSubquery(root, condition, query_context, bind_context);
+    }
+}
+
+SharedPtr<BaseExpression> BoundDeleteStatement::UnnestSubquery(SharedPtr<LogicalNode> &root,
+                                                               SharedPtr<BaseExpression> &condition,
+                                                               QueryContext *query_context,
+                                                               const SharedPtr<BindContext> &) {
+    building_subquery_ = true;
+    //    UniquePtr<QueryBinder> query_binder_ptr = MakeUnique<QueryBinder>(query_context,
+    //                                                                      bind_context);
+    SubqueryExpression *subquery_expr_ptr = (SubqueryExpression *)condition.get();
+    SharedPtr<LogicalNode> subquery_plan = subquery_expr_ptr->bound_select_statement_ptr_->BuildPlan(query_context);
+    SharedPtr<BaseExpression> return_expr = nullptr;
+    if (subquery_expr_ptr->bound_select_statement_ptr_->bind_context_->HasCorrelatedColumn()) {
+        // If correlated subquery
+        return_expr = SubqueryUnnest::UnnestCorrelated(subquery_expr_ptr,
+                                                       root,
+                                                       subquery_plan,
+                                                       query_context,
+                                                       subquery_expr_ptr->bound_select_statement_ptr_->bind_context_);
+    } else {
+        // If uncorrelated subquery
+        return_expr = SubqueryUnnest::UnnestUncorrelated(subquery_expr_ptr,
+                                                         root,
+                                                         subquery_plan,
+                                                         query_context,
+                                                         subquery_expr_ptr->bound_select_statement_ptr_->bind_context_);
+    }
+    building_subquery_ = false;
+    return return_expr;
+}
+
+} // namespace infinity
