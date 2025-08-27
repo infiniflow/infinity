a62711032f3576f8a7934aae174ef714eef173b9
Jin Hai <haijin.chn@gmail.com>
2025-08-27 17:01:31 +0800

    Fix failed test when snapshot (#2948)
    ### What problem does this PR solve?
    
    By disable checkpoint to avoid checkpoint conflicts.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    
    ---------
    
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- src/planner/query_binder_impl.cpp
+++ src/planner/query_binder_impl.cpp
@@ -428,20 +428,12 @@ std::shared_ptr<BaseTableRef> QueryBinder::BuildBaseTable(QueryContext *query_co
     std::shared_ptr<TableInfo> table_info;
     Status status;
     NewTxn *new_txn = query_context->GetNewTxn();
-    MetaCache *meta_cache = query_context->storage()->meta_cache();
     std::shared_ptr<DBMeeta> db_meta;
-    std::shared_ptr<TableMeeta> tmp_table_meta;
-    status = new_txn->GetTableMeta(db_name, table_name, db_meta, tmp_table_meta);
+    std::shared_ptr<TableMeeta> table_meta;
+    status = new_txn->GetTableMeta(db_name, table_name, db_meta, table_meta);
     if (!status.ok()) {
         RecoverableError(status);
     }
-    auto table_meta = std::make_shared<TableMeeta>(tmp_table_meta->db_id_str(),
-                                                   tmp_table_meta->table_id_str(),
-                                                   tmp_table_meta->table_name(),
-                                                   tmp_table_meta->kv_instance(),
-                                                   tmp_table_meta->begin_ts(),
-                                                   tmp_table_meta->commit_ts(),
-                                                   meta_cache);
     table_info = std::make_shared<TableInfo>();
     status = table_meta->GetTableInfo(*table_info);
     if (!status.ok()) {
