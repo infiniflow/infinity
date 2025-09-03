a6ae9b3de2013fabce50e3775e1ee7156a3d1138
Ling Qin <qinling0210@163.com>
2025-09-03 19:54:01 +0800

    Minor change

--- src/storage/new_txn/new_txn_impl.cpp
+++ src/storage/new_txn/new_txn_impl.cpp
@@ -6325,9 +6325,6 @@ void NewTxn::ResetMetaCacheAndCacheInfo() {
 }
 
 void NewTxn::SaveMetaCacheAndCacheInfo() {
-    if (meta_cache_items_.empty() && cache_infos_.empty()) {
-        return;
-    }
     MetaCache *meta_cache_ptr = txn_mgr_->storage()->meta_cache();
     meta_cache_ptr->Put(meta_cache_items_, cache_infos_, this->BeginTS());
 }
