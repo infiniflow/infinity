4a49d05f48849a68ebcb10e319d566a5b5def522
zpf121 <1219290549@qq.com>
2025-12-17 10:37:45 +0800

    modify

--- src/storage/column_vector/column_vector_impl.cpp
+++ src/storage/column_vector/column_vector_impl.cpp
@@ -870,8 +870,7 @@ std::string ColumnVector::ToString(size_t row_index) const {
         case LogicalType::kJson: {
             const auto &json = reinterpret_cast<const JsonT *>(data_ptr_)[row_index];
             auto data = buffer_->GetVarchar(json.file_offset_, json.length_);
-            std::vector<uint8_t> bson(json.length_);
-            memcpy(bson.data(), data, json.length_);
+            std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(data), reinterpret_cast<const uint8_t *>(data) + json.length_);
             auto res = JsonManager::from_bson(bson);
             return res.dump();
         }
