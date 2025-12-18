4f7a2c65c247e8cb697157101c84e999bdb725f7
zpf121 <1219290549@qq.com>
2025-12-18 19:03:59 +0800

    modify

--- src/function/scalar/extract_json_impl.cpp
+++ src/function/scalar/extract_json_impl.cpp
@@ -30,134 +30,53 @@ namespace infinity {
 
 void JsonExtractBase(const DataBlock &, std::shared_ptr<ColumnVector> &) {}
 
-void JsonExtract(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
-    auto extra_ptr = reinterpret_cast<const JsonExtraInfo *>(extract_ptr.get());
-    auto column = input.column_vectors[0];
-    auto input_data = reinterpret_cast<const JsonT *>(column->data());
-    auto row_count = input.row_count();
+template <typename T, std::tuple<bool, T> (*ExtractFunc)(const JsonTypeDef &, const std::vector<std::string> &), Value (*MakeValueFunc)(T)>
+class JsonExtractor {
+public:
+    static void Execute(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
+        auto extra_ptr = reinterpret_cast<const JsonExtraInfo *>(extract_ptr.get());
+        auto column = input.column_vectors[0];
+        auto input_data = reinterpret_cast<const JsonT *>(column->data());
+        auto row_count = input.row_count();
+        const std::shared_ptr<Bitmask> &output_null = output->nulls_ptr_;
 
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &input_null = column->nulls_ptr_;
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &output_null = output->nulls_ptr_;
-
-    if (column->vector_type() == ColumnVectorType::kFlat) {
-        for (size_t row_index = 0; row_index < row_count; row_index++) {
-            const auto json_info = input_data[row_index];
-            auto json_data = column->buffer_->GetVarchar(json_info.file_offset_, json_info.length_);
-            std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(json_data), reinterpret_cast<const uint8_t *>(json_data) + json_info.length_);
-            auto json = JsonManager::from_bson(bson);
-            auto [is_null, json_string] = JsonManager::json_extract(json, extra_ptr->json_tokens_);
-            output_null->Set(row_index, !is_null);
-            Value v = Value::MakeVarchar(json_string.c_str());
-            output->AppendValue(v);
+        if (column->vector_type() == ColumnVectorType::kFlat) {
+            for (size_t row_index = 0; row_index < row_count; row_index++) {
+                const auto json_info = input_data[row_index];
+                auto json_data = column->buffer_->GetVarchar(json_info.file_offset_, json_info.length_);
+                std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(json_data),
+                                          reinterpret_cast<const uint8_t *>(json_data) + json_info.length_);
+                auto json = JsonManager::from_bson(bson);
+                auto [is_null, extracted_value] = ExtractFunc(json, extra_ptr->json_tokens_);
+                output_null->Set(row_index, !is_null);
+                Value v = MakeValueFunc(extracted_value);
+                output->AppendValue(v);
+            }
+            output->Finalize(row_count);
+        } else {
+            RecoverableError(Status::SyntaxError("JsonExtract: Invalid column type."));
         }
-        output->Finalize(row_count);
-    } else {
-        RecoverableError(Status::SyntaxError("JsonExtract: Invalid column type."));
     }
-}
+};
 
 void JsonExtractString(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
-    JsonExtract(extract_ptr, input, output);
+    JsonExtractor<std::string, JsonManager::json_extract, Value::MakeVarchar>::Execute(extract_ptr, input, output);
 }
 
 void JsonExtractInt(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
-    auto extra_ptr = reinterpret_cast<const JsonExtraInfo *>(extract_ptr.get());
-    auto column = input.column_vectors[0];
-    auto input_data = reinterpret_cast<const JsonT *>(column->data());
-    auto row_count = input.row_count();
-
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &input_null = column->nulls_ptr_;
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &output_null = output->nulls_ptr_;
-
-    if (column->vector_type() == ColumnVectorType::kFlat) {
-        for (size_t row_index = 0; row_index < row_count; row_index++) {
-            const auto json_info = input_data[row_index];
-            auto json_data = column->buffer_->GetVarchar(json_info.file_offset_, json_info.length_);
-            std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(json_data), reinterpret_cast<const uint8_t *>(json_data) + json_info.length_);
-            auto json = JsonManager::from_bson(bson);
-            auto [is_null, json_int] = JsonManager::json_extract_int(json, extra_ptr->json_tokens_);
-            output_null->Set(row_index, !is_null);
-            Value v = Value::MakeInt(json_int);
-            output->AppendValue(v);
-        }
-        output->Finalize(row_count);
-    } else {
-        RecoverableError(Status::SyntaxError("JsonExtract: Invalid column type."));
-    }
+    JsonExtractor<int, JsonManager::json_extract_int, Value::MakeInt>::Execute(extract_ptr, input, output);
 }
 
 void JsonExtractDouble(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
-    auto extra_ptr = reinterpret_cast<const JsonExtraInfo *>(extract_ptr.get());
-    auto column = input.column_vectors[0];
-    auto input_data = reinterpret_cast<const JsonT *>(column->data());
-    auto row_count = input.row_count();
-
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &input_null = column->nulls_ptr_;
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &output_null = output->nulls_ptr_;
-
-    if (column->vector_type() == ColumnVectorType::kFlat) {
-        for (size_t row_index = 0; row_index < row_count; row_index++) {
-            const auto json_info = input_data[row_index];
-            auto json_data = column->buffer_->GetVarchar(json_info.file_offset_, json_info.length_);
-            std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(json_data), reinterpret_cast<const uint8_t *>(json_data) + json_info.length_);
-            auto json = JsonManager::from_bson(bson);
-            auto [is_null, json_double] = JsonManager::json_extract_double(json, extra_ptr->json_tokens_);
-            output_null->Set(row_index, !is_null);
-            Value v = Value::MakeDouble(json_double);
-            output->AppendValue(v);
-        }
-        output->Finalize(row_count);
-    } else {
-        RecoverableError(Status::SyntaxError("JsonExtract: Invalid column type."));
-    }
+    JsonExtractor<double, JsonManager::json_extract_double, Value::MakeDouble>::Execute(extract_ptr, input, output);
 }
 
 void JsonExtractBool(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
-    auto extra_ptr = reinterpret_cast<const JsonExtraInfo *>(extract_ptr.get());
-    auto column = input.column_vectors[0];
-    auto input_data = reinterpret_cast<const JsonT *>(column->data());
-    auto row_count = input.row_count();
-
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &input_null = column->nulls_ptr_;
-    [[maybe_unused]] const std::shared_ptr<Bitmask> &output_null = output->nulls_ptr_;
-
-    if (column->vector_type() == ColumnVectorType::kFlat) {
-        for (size_t row_index = 0; row_index < row_count; row_index++) {
-            const auto json_info = input_data[row_index];
-            auto json_data = column->buffer_->GetVarchar(json_info.file_offset_, json_info.length_);
-            std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(json_data), reinterpret_cast<const uint8_t *>(json_data) + json_info.length_);
-            auto json = JsonManager::from_bson(bson);
-            auto [is_null, json_bool] = JsonManager::json_extract_bool(json, extra_ptr->json_tokens_);
-            output_null->Set(row_index, !is_null);
-            Value v = Value::MakeBool(json_bool);
-            output->AppendValue(v);
-        }
-        output->Finalize(row_count);
-    } else {
-        RecoverableError(Status::SyntaxError("JsonExtract: Invalid column type."));
-    }
+    JsonExtractor<bool, JsonManager::json_extract_bool, Value::MakeBool>::Execute(extract_ptr, input, output);
 }
 
 void JsonExtractIsNull(std::shared_ptr<BaseExtraInfo> extract_ptr, const DataBlock &input, std::shared_ptr<ColumnVector> &output) {
-    auto extra_ptr = reinterpret_cast<const JsonExtraInfo *>(extract_ptr.get());
-    auto column = input.column_vectors[0];
-    auto input_data = reinterpret_cast<const JsonT *>(column->data());
-    auto row_count = input.row_count();
-
-    if (column->vector_type() == ColumnVectorType::kFlat) {
-        for (size_t row_index = 0; row_index < row_count; row_index++) {
-            const auto json_info = input_data[row_index];
-            auto json_data = column->buffer_->GetVarchar(json_info.file_offset_, json_info.length_);
-            std::vector<uint8_t> bson(reinterpret_cast<const uint8_t *>(json_data), reinterpret_cast<const uint8_t *>(json_data) + json_info.length_);
-            auto json = JsonManager::from_bson(bson);
-            auto [is_null, json_isnull] = JsonManager::json_extract_int(json, extra_ptr->json_tokens_);
-            Value v = Value::MakeBool(json_isnull);
-            output->AppendValue(v);
-        }
-        output->Finalize(row_count);
-    } else {
-        RecoverableError(Status::SyntaxError("JsonExtract: Invalid column type."));
-    }
+    JsonExtractor<bool, JsonManager::json_extract_is_null, Value::MakeBool>::Execute(extract_ptr, input, output);
 }
 
 void RegisterJsonFunction(NewCatalog *catalog_ptr) {
