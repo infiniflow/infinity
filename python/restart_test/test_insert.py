29302135d1fc10bcdf25c35ed380321e77896ba1
euvre <93761161+euvre@users.noreply.github.com>
2025-12-31 05:49:17 +0000

    Fix restart test in slow test (#3198)
    ### What problem does this PR solve?
    
    Replace `idx.column_name` to `idx.target_name`.
    
    ### Type of change
    
    - [x] Bug Fix (non-breaking change which fixes an issue)
    - [x] Test cases
    
    ---------
    
    Signed-off-by: noob <yixiao121314@outlook.com>

--- python/restart_test/test_insert.py
+++ python/restart_test/test_insert.py
@@ -224,7 +224,7 @@ class TestInsert:
             db_obj = infinity_obj.get_database("default_db")
             table_obj = db_obj.create_table("test_insert", columns, ConflictType.Error)
             for idx in indexes:
-                table_obj.create_index(f"idx_{idx.column_name}", idx)
+                table_obj.create_index(f"idx_{idx.target_name}", idx)
         part1()
 
         self.insert_inner(insert_n, infinity_runner, config, columns, data_gen_factory)
