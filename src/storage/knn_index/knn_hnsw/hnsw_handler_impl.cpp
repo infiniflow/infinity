1b624e5e4fe80044288b8ac2452e61c0ef4122e1
noob <yixiao121314@outlook.com>
2026-01-05 10:14:29 +0800

    remove usless code
    Signed-off-by: noob <yixiao121314@outlook.com>

--- src/storage/knn_index/knn_hnsw/hnsw_handler_impl.cpp
+++ src/storage/knn_index/knn_hnsw/hnsw_handler_impl.cpp
@@ -629,7 +629,18 @@ void HnswIndexInMem::Dump(FileWorker *index_file_worker, size_t *dump_size_ptr) 
     index_file_worker_ = std::move(index_file_worker);
     auto hnsw_handler = std::shared_ptr<HnswHandler>(hnsw_handler_);
     index_file_worker_->Write(std::move(hnsw_handler));
-    // delete hnsw_handler_;
+    // if (own_memory_ && hnsw_handler_ != nullptr) {
+    //     size_t mem_usage = hnsw_handler_->MemUsage();
+    //     delete hnsw_handler_;
+    //     auto *storage = InfinityContext::instance().storage();
+    //     if (storage == nullptr) {
+    //         return;
+    //     }
+    //     auto *memindex_tracer = storage->memindex_tracer();
+    //     if (memindex_tracer != nullptr) {
+    //         memindex_tracer->DecreaseMemUsed(mem_usage);
+    //     }
+    // }
 }
 
 size_t
