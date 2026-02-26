73e48c945d985acd1f70f04d2610753ba46568dc
Jin Hai <haijin.chn@gmail.com>
2026-02-26 16:42:10 +0800

    Update version info
    Signed-off-by: Jin Hai <haijin.chn@gmail.com>

--- go/infinity.go
+++ go/infinity.go
@@ -24,7 +24,7 @@ import (
 
 // ClientVersion is the version of this Go SDK client
 // This should match the protocol version expected by the server
-const ClientVersion = 36 // version: 0.7.0.dev2
+const ClientVersion = 36 // version: 0.7.0.dev3
 
 // Connect creates a new connection to Infinity
 // This is a factory function that creates the appropriate connection type based on the URI
