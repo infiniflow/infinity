78fae401ba8fb329914cfcd7650f26470af0fa60
zpf121 <1219290549@qq.com>
2026-03-17 14:25:16 +0800

    modify

--- python/restart_test/test_multiple_index_types_import.py
+++ python/restart_test/test_multiple_index_types_import.py
@@ -15,9 +15,9 @@ class TestMultipleIndexTypesImport:
         "config, generator, kImportRepeat, kBatchCount, kRunTime",
         [
             pytest.param("test/data/config/restart_test/test_insert/5.toml", MultiIndexTypesGenerator, 25, 50, 120, marks=pytest.mark.slow),
-            # pytest.param("test/data/config/restart_test/test_insert/4.toml", MultiIndexTypesGenerator, 2, 4, 60, marks=[pytest.mark.slow, pytest.mark.ubsan]),
-            # pytest.param("test/data/config/restart_test/test_insert/5.toml", MultiIndexTypesGenerator, 2, 4, 60, marks=[pytest.mark.slow, pytest.mark.ubsan]),
-            # pytest.param("test/data/config/restart_test/test_insert/6.toml", MultiIndexTypesGenerator, 2, 4, 60, marks=[pytest.mark.slow, pytest.mark.ubsan]),
+            # pytest.param("test/data/config/restart_test/test_insert/4.toml", MultiIndexTypesGenerator, 2, 4, 60, marks=pytest.mark.ubsan),
+            # pytest.param("test/data/config/restart_test/test_insert/5.toml", MultiIndexTypesGenerator, 2, 4, 60, marks=pytest.mark.ubsan),
+            # pytest.param("test/data/config/restart_test/test_insert/6.toml", MultiIndexTypesGenerator, 2, 4, 60, marks=pytest.mark.ubsan),
         ],
     )
     def test_multiple_index_types_import_restart(
