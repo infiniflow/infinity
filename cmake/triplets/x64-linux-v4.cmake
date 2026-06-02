6995f907300570870058b5879d1a6f9af08b8681
noob <yixiao121314@outlook.com>
2026-06-02 20:37:45 +0800

    fix some
    Signed-off-by: noob <yixiao121314@outlook.com>

--- cmake/triplets/x64-linux-v4.cmake
+++ cmake/triplets/x64-linux-v4.cmake
@@ -8,3 +8,6 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/x86_64-linux-v4.cmake")
 
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-gnu")
+
+set(VCPKG_MAKE_OPTIONS "CC='clang-20 --target=aarch64-linux-gnu --sysroot=$ENV{SYS_ROOT} --gcc-toolchain=$ENV{GCC_TOOLCHAIN}'"
+    "CXX='clang++-20 --target=aarch64-linux-gnu --sysroot=$ENV{SYS_ROOT} --gcc-toolchain=$ENV{GCC_TOOLCHAIN}'")
